abf0610e5250ed69cc542e19f19c8a6812f4914e
Sammy <r62914@freescale.com>
2009-07-13 15:05:16 +0800

    ENGR00114178 Support MMC/SD boot and enable dhcp in uboot on MX25
    1. Support MMC/SD boot in uboot, however, it is disabled default and
     user needs to enable manually by changing mx25_3stack.h;
    2. Enable dhcp for network.
    
    Signed-off-by: Sammy He <r62914@freescale.com>

---
+++ include/asm-arm/arch-mx25/mmc.h
@@ -0,0 +1,16 @@
+/*
+ *  (C) Copyright 2008-2009 Freescale Semiconductor, Inc.
+ *
+ *  linux/drivers/mmc/mmc.h
+ *
+ *  Author: Vladimir Shebordaev, Igor Oblakov
+ *  Copyright:  MontaVista Software Inc.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  published by the Free Software Foundation.
+ */
+#ifndef __MMC_MX25_3STACK_H__
+#define __MMC_MX25_3STACK_H__
+
+#endif /* __MMC_MX51_3STACK_H__ */
