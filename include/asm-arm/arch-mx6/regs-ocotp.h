9aa74e691a1de2e471af837d72a5a3acc274c540
Mahesh Mahadevan <r9aadq@freescale.com>
2011-11-02 06:10:18 -0500

    ENGR00161277 Add fuse access capability for MX6 Sabre-lite
    Add support to read and program fuses in the MX6 Sabre-lite
    
    Signed-off-by: Mahesh Mahadevan <r9aadq@freescale.com>

--- include/asm-arm/arch-mx6/regs-ocotp.h
+++ include/asm-arm/arch-mx6/regs-ocotp.h
@@ -1,7 +1,7 @@
 /*
  * Freescale OCOTP Register Definitions
  *
- * Copyright 2008-2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright (C) 2011 Freescale Semiconductor, Inc. All Rights Reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
