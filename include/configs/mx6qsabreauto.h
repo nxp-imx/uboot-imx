9c344337485908eb7d8c97e4df1cd0d3c3b1ec8c
Ye.Li <B37916@freescale.com>
2014-01-14 13:47:47 +0800

    ENGR00294905 ARM:imx6:sabreauto Add USB HOST BSP support
    Sabreauto board has pin conflict (pin EIM_D18) between NOR flash
    and i2c3. To enable the USB host, the i2c3 must be used to operate
    the max7310 IO expander to output the VBUS power.
    
    As SPINOR is enabled at default, it is impossible to use USB host
    at same time. Thus, remove the SYS_USE_SPINOR from sabreauto
    configurations to disable SPINOR.
    
    Signed-off-by: Ye.Li <B37916@freescale.com>
    (cherry picked from commit ea211a104343ec386b5b78c1e213552af35349ca)

--- include/configs/mx6qsabreauto.h
+++ include/configs/mx6qsabreauto.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012-2013 Freescale Semiconductor, Inc.
+ * Copyright (C) 2012-2014 Freescale Semiconductor, Inc.
  *
  * Configuration settings for the Freescale i.MX6Q SabreAuto board.
  *
@@ -17,22 +17,33 @@
 #define CONFIG_CONSOLE_DEV		"ttymxc3"
 #define CONFIG_MMCROOT			"/dev/mmcblk0p2"
 
+#define CONFIG_SYS_USE_NAND
+
+#include "mx6qsabre_common.h"
+#include <asm/imx-common/gpio.h>
+
+/*Since the pin conflicts on EIM D18, disable the USB host if the NOR flash is enabled */
+#if !defined(CONFIG_SYS_USE_SPINOR) && !defined(CONFIG_SYS_USE_EIMNOR)
 /* USB Configs */
 #define CONFIG_CMD_USB
 #define CONFIG_USB_EHCI
 #define CONFIG_USB_EHCI_MX6
 #define CONFIG_USB_STORAGE
+#define CONFIG_EHCI_HCD_INIT_AFTER_RESET
 #define CONFIG_USB_HOST_ETHER
 #define CONFIG_USB_ETHER_ASIX
-#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
-#define CONFIG_EHCI_HCD_INIT_AFTER_RESET	/* For OTG port */
-#define CONFIG_MXC_USB_PORTSC	(PORT_PTS_UTMI | PORT_PTS_PTW)
-#define CONFIG_MXC_USB_FLAGS	0
+#define CONFIG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
+#define CONFIG_MXC_USB_FLAGS   0
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2 /* Enabled USB controller number */
 
-#define CONFIG_SYS_USE_NAND
+/* Enable multiple I2C bus support*/
+#define CONFIG_I2C_MULTI_BUS
 
-#include "mx6qsabre_common.h"
-#include <asm/imx-common/gpio.h>
+/* MAX7310 configs*/
+#define CONFIG_MAX7310_IOEXP
+#define CONFIG_IOEXP_DEVICES_NUM 3
+#define CONFIG_IOEXP_DEV_PINS_NUM 8
+#endif
 
 #define CONFIG_SYS_FSL_USDHC_NUM	2
 #define CONFIG_SYS_MMC_ENV_DEV		0
