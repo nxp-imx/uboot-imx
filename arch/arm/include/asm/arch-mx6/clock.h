c2be538be123b3fd5de1c32f321e9b094892d1c5
Ye.Li <B37916@freescale.com>
2014-06-12 19:33:05 +0800

    ENGR00315894-76 mx6 clock: Add vadc clock enable function
    Add vadc clock enable function.
    
    Signed-off-by: Sandor Yu <R01008@freescale.com>
    Signed-off-by: Ye.Li <B37916@freescale.com>
    (cherry picked from commit 721c7a1448c5b7265b597b83d18f8338a27ea213)
    Signed-off-by: Peng Fan <Peng.Fan@freescale.com>
    (cherry picked from commit 903a59ef941f39b6d7f693dd7c60528e166de079)
    (cherry picked from commit dc767fb7d5c155f2a6ef01c4dee808b9c1944fc2)
    (cherry picked from commit 0a48932f375a969e6f7e72d171522146981b2135)
    (cherry picked from commit b6f962db57c988c31d35e204f638ba3b0c53eb4a)
    (cherry picked from commit b28c20cc8d9032a0521274f6bc21d4da414a8a37)
    (cherry picked from commit 909929b5b8a6ae7d7f8d8b12e60e0882634550c8)
    (cherry picked from commit 99177cd10c0177c1f92abc553ab514be8f3778f6)
    (cherry picked from commit 80c9584b23ee3bb0d28bb44c27bc680d43128477)

--- arch/arm/include/asm/arch-mx6/clock.h
+++ arch/arm/include/asm/arch-mx6/clock.h
@@ -84,6 +84,7 @@ void enable_epdc_clock(void);
 void mxs_set_lcdclk(u32 base_addr, u32 freq);
 void select_ldb_di_clock_source(enum ldb_di_clock clk);
 void enable_eim_clk(unsigned char enable);
+void mxs_set_vadcclk(void);
 int do_mx6_showclocks(struct cmd_tbl *cmdtp, int flag, int argc,
 		      char *const argv[]);
 #endif /* __ASM_ARCH_CLOCK_H */
