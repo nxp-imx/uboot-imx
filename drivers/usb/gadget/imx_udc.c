8937308dce9c6fa907aafeae3a11bee2ada1db2a
Ye.Li <B37916@freescale.com>
2014-06-12 17:00:56 +0800

    ENGR00315894-68 iMX6SX:SABRESD: Add Android features support
    Add BSP codes to mx6sxsabresd to support android uboot features:
    fastboot, booti and recovery
    
    Signed-off-by: Ye.Li <B37916@freescale.com>

--- drivers/usb/gadget/imx_udc.c
+++ drivers/usb/gadget/imx_udc.c
@@ -583,7 +583,7 @@ static void usb_set_mode_device(void)
 		;
 	DBG("DOORE RESET END\n");
 
-#if defined(CONFIG_MX6Q) || defined(CONFIG_MX6DL) || defined(CONFIG_MX6SL)
+#if defined(CONFIG_MX6)
 	reset_usb_phy1();
 #endif
 	DBG("init core to device mode\n");
@@ -983,7 +983,7 @@ int mxc_udc_init(void)
     udc_pins_setting();
 	set_usb_phy1_clk();
 	enable_usboh3_clk(1);
-#if defined(CONFIG_MX6Q) || defined(CONFIG_MX6DL) || defined(CONFIG_MX6SL)
+#if defined(CONFIG_MX6)
 	udc_disable_over_current();
 #endif
 	enable_usb_phy1_clk(1);
