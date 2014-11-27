9af06ef99b07a6ae2f6709bc45fa1b416678bc7e
Allen Xu <b45815@freescale.com>
2014-11-28 04:54:59 +0800

    MLK-9927 - change the kernel and dtb location for nand read
    kernel and dtb file location should be changed from 0x1000000 and
    0x2000000 to 0x4000000 and 0x5000000, since the uboot partition expanded
    to 64M.
    
    Signed-off-by: Allen Xu <b45815@freescale.com>
    (cherry picked from commit eb4e6a6e65fe9074095869ecd5ccfe0a1559917d)

--- include/configs/mx6sxsabreauto.h
+++ include/configs/mx6sxsabreauto.h
@@ -187,8 +187,8 @@
 	"bootargs=console=ttymxc0,115200 ubi.mtd=5 "  \
 		"root=ubi0:rootfs rootfstype=ubifs "		     \
 		"mtdparts=gpmi-nand:64m(boot),16m(kernel),16m(dtb),-(rootfs)\0"\
-	"bootcmd=nand read ${loadaddr} 0x1000000 0x800000;"\
-		"nand read ${fdt_addr} 0x2000000 0x100000;"\
+	"bootcmd=nand read ${loadaddr} 0x4000000 0x800000;"\
+		"nand read ${fdt_addr} 0x5000000 0x100000;"\
 		"bootz ${loadaddr} - ${fdt_addr}\0"
 
 #else
