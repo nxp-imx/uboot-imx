3df5d604ff12ed45897f14cf018cbbcdc66b532a
Ye.Li <B37916@freescale.com>
2015-02-02 12:51:30 +0800

    MLK-10191-1 imx: mx7: Add i.MX7 platform common head file
    Add common head file mx7_common.h for all i.MX7 platform
    
    Signed-off-by: Ye.Li <B37916@freescale.com>
    (cherry picked from commit e38c9a7a543890e0879f89627a3f404d8068d602)
    Signed-off-by: Peng Fan <Peng.Fan@freescale.com>

---
+++ include/configs/mx7_common.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2014-2015 Freescale Semiconductor, Inc.
+ *
+ * Configuration settings for the Freescale i.MX7.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+
+#ifndef __MX7_COMMON_H
+#define __MX7_COMMON_H
+
+#define CONFIG_BOARD_POSTCLK_INIT
+#define CONFIG_MXC_GPT_HCLK
+
+#define CONFIG_SC_TIMER_CLK 8000000 /* 8Mhz */
+
+#define CONFIG_IOMUX_LPSR
+#define CONFIG_IMX_FIXED_IVT_OFFSET
+#endif
