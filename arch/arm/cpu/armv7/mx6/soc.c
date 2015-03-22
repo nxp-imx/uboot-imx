ccf3b130d71cf3dd9a97d3bb424931bf6bd7e8c0
Ye.Li <B37916@freescale.com>
2015-03-23 04:49:51 +0800

    MLK-10448-1 mx6: Add MX6DQP CPU rev type
    Add new cpu type for i.MX6DQP and providing a dynamical
    detecting function.
    
    Signed-off-by: Peng Fan <Peng.Fan@freescale.com>
    Signed-off-by: Ye.Li <B37916@freescale.com>

--- arch/arm/cpu/armv7/mx6/soc.c
+++ arch/arm/cpu/armv7/mx6/soc.c
@@ -62,6 +62,7 @@ u32 get_cpu_rev(void)
 	struct mxc_ccm_reg *ccm_regs = (struct mxc_ccm_reg *)CCM_BASE_ADDR;
 	u32 reg = readl(&ccm_regs->digprog_sololite);
 	u32 type = ((reg >> 16) & 0xff);
+	u32 major;
 
 	if (type != MXC_CPU_MX6SL) {
 		reg = readl(&ccm_regs->digprog);
@@ -79,8 +80,9 @@ u32 get_cpu_rev(void)
 		}
 
 	}
+	major = ((reg >> 8) & 0xff);
 	reg &= 0xff;		/* mx6 silicon revision */
-	return (type << 12) | (reg + 0x10);
+	return (type << 12) | (reg + (0x10 * (major + 1)));
 }
 
 #ifdef CONFIG_REVISION_TAG
