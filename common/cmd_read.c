998d8cc04add25c0fb4b11351097ca00de6de8f7
fang hui <b31070@freescale.com>
2015-12-21 10:30:23 +0800

    MA-7373 - [brillo]: store slot meta data in a raw partition, no file system.
    Partition name change from slotmeta to misc.
    Read/write raw data on partition misc, not use ext4 file system.
    
    Store meta in bootloader_message.slot_suffix, as defined in
    bootable/recovery/bootloader.h
    
    The first 4 bytes of boot_ctl are defined as magic number.
    
    Also, modify code to remove warning in drivers/usb/gadget/bootctrl.c
    warning: implicit declaration of function 'do_read'
    
    Signed-off-by: fang hui <b31070@freescale.com>

--- common/cmd_read.c
+++ common/cmd_read.c
@@ -12,7 +12,7 @@
 #include <command.h>
 #include <part.h>
 
-int do_read(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
+int do_raw_read(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	char *ep;
 	block_dev_desc_t *dev_desc = NULL;
@@ -75,7 +75,7 @@ int do_read(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 }
 
 U_BOOT_CMD(
-	read,	6,	0,	do_read,
+	read,	6,	0,	do_raw_read,
 	"Load binary data from a partition",
 	"<interface> <dev[:part]> addr blk# cnt"
 );
