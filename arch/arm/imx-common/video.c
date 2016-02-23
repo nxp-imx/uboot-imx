d6d0265b09e66c576ab4c01841166415f834a1ff
Ye Li <ye.li@nxp.com>
2016-02-23 15:19:39 +0800

    MLK-12437-2 Video: Update the common board_video_skip to support MXS LCD
    Update the board_video_skip to use CONFIG_VIDEO_MXS for LCD display support.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- arch/arm/imx-common/video.c
+++ arch/arm/imx-common/video.c
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ *
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
@@ -34,8 +36,14 @@ int board_video_skip(void)
 	}
 
 	if (i < display_count) {
+#if defined(CONFIG_VIDEO_IPUV3)
 		ret = ipuv3_fb_init(&displays[i].mode, 0,
 				    displays[i].pixfmt);
+#elif defined(CONFIG_VIDEO_MXS)
+		ret = mxs_lcd_panel_setup(displays[i].mode,
+					displays[i].pixfmt,
+				    displays[i].bus);
+#endif
 		if (!ret) {
 			if (displays[i].enable)
 				displays[i].enable(displays + i);
