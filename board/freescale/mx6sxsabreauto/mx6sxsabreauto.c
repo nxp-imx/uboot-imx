b5c6277890e12b5fab86d1f49681fa6efe2f40ac
Ye Li <ye.li@nxp.com>
2016-03-14 16:49:04 +0800

    MLK-12555 mx6sxsabreauto: Enable the Watchdog WDOG_B signal output
    When using watchdog timeout in kernel, the reset does not output the
    WDOG_B signal, so the power supply won't be reset. To solve the problem,
    we enable it in u-boot.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 7b53ee014c9f02f6ead0b60d5295d07205247a7c)

--- board/freescale/mx6sxsabreauto/mx6sxsabreauto.c
+++ board/freescale/mx6sxsabreauto/mx6sxsabreauto.c
@@ -526,6 +526,9 @@ int board_late_init(void)
 	board_late_mmc_env_init();
 #endif
 
+	/* set WDOG_B to reset whole system */
+	set_wdog_reset((struct wdog_regs *)WDOG1_BASE_ADDR);
+
 	return 0;
 }
 
