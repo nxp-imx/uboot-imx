8aef13ebd92f709f4e6b80cf46c7cfe0f69bb211
Ye Li <ye.li@nxp.com>
2016-03-15 22:12:46 +0800

    MLK-12483-3 mx6: Add a module fuse checking
    Implement a functionality to read the soc fuses and check if any module
    is fused. For fused module, we have to disable it in u-boot dynamically,
    and change the its node in FDT to "disabled" status before starting the kernel.
    
    In this patch, we implement the ft_system_setup for FDT fixup. This function will
    be called during boot process or by "fdt systemsetup" command.
    
    To enable the module fuse checking, two configurations must be defined:
    CONFIG_MODULE_FUSE
    CONFIG_OF_SYSTEM_SETUP
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 7236051526b73a5a25cc8330a79f5c08b7d70726)

--- arch/arm/include/asm/arch-mx6/sys_proto.h
+++ arch/arm/include/asm/arch-mx6/sys_proto.h
@@ -2,7 +2,10 @@
  * (C) Copyright 2009
  * Stefano Babic, DENX Software Engineering, sbabic@denx.de.
  *
+ * Copyright (C) 2015-2016 Freescale Semiconductor, Inc.
+ *
  * SPDX-License-Identifier:	GPL-2.0+
  */
 
 #include <asm/imx-common/sys_proto.h>
+#include <asm/arch/module_fuse.h>
