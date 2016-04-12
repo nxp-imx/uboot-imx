ca0df9c20a3b0e67bf95a077115cd43ef61df1b6
Ye Li <ye.li@nxp.com>
2016-04-12 12:24:02 +0800

    MLK-12616-5 GPT: Update GPT driver for MX6ULL
    The MX6ULL has GPT with supporting OSC clock source, update the driver
    accordingly.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit b2740fd7a0f8d80b416d4de1f57b6542407d23bd)

--- arch/arm/imx-common/timer.c
+++ arch/arm/imx-common/timer.c
@@ -46,7 +46,7 @@ static inline int gpt_has_clk_source_osc(void)
 	if (((is_cpu_type(MXC_CPU_MX6Q) || is_cpu_type(MXC_CPU_MX6D)) &&
 	    (soc_rev() > CHIP_REV_1_0)) || is_cpu_type(MXC_CPU_MX6DL) ||
 	     is_cpu_type(MXC_CPU_MX6SOLO) || is_cpu_type(MXC_CPU_MX6SX) ||
-	     is_cpu_type(MXC_CPU_MX6UL))
+	     is_cpu_type(MXC_CPU_MX6UL) || is_cpu_type(MXC_CPU_MX6ULL))
 		return 1;
 
 	return 0;
@@ -91,12 +91,13 @@ int timer_init(void)
 	if (gpt_has_clk_source_osc()) {
 		i |= GPTCR_CLKSOURCE_OSC | GPTCR_TEN;
 
-		/* For DL/S, SX, UL, set 24Mhz OSC Enable bit and prescaler */
+		/* For DL/S, SX, UL, ULL set 24Mhz OSC Enable bit and prescaler */
 		if (is_cpu_type(MXC_CPU_MX6DL) ||
 		    is_cpu_type(MXC_CPU_MX6SOLO) ||
 		    is_cpu_type(MXC_CPU_MX6SX) ||
 		    is_cpu_type(MXC_CPU_MX7D) ||
-		    is_cpu_type(MXC_CPU_MX6UL)) {
+		    is_cpu_type(MXC_CPU_MX6UL) ||
+		    is_cpu_type(MXC_CPU_MX6ULL)) {
 			i |= GPTCR_24MEN;
 
 			/* Produce 3Mhz clock */
