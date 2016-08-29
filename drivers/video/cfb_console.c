2f3d9b03246f95a38ace10cd375239e47671a06e
Ye Li <ye.li@nxp.com>
2016-08-29 13:51:01 +0800

    MLK-15324-1 video: cfb_console: Fix 64 bits support problem
    There are some integer to pointer convert. To support 64 bits system,
    change the unsigned int to unsigned long.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit c3b5c2df452eac82a970ba304e94550be8e27335)
    (cherry picked from commit 8a2330424ce8dff9ba9e51ccf74fa600ea93aeb1)
    (cherry picked from commit a39800dd920f7196a960cb7fbe51edaf0d753b47)

--- drivers/video/cfb_console.c
+++ drivers/video/cfb_console.c
@@ -1179,7 +1179,7 @@ static int display_rle8_bitmap(struct bmp_image *img, int xoff, int yoff,
 	y = __le32_to_cpu(img->header.height) - 1;
 	ncolors = __le32_to_cpu(img->header.colors_used);
 	bpp = VIDEO_PIXEL_SIZE;
-	fbp = (unsigned char *) ((unsigned int) video_fb_address +
+	fbp = (unsigned char *) ((unsigned long) video_fb_address +
 				 (y + yoff) * VIDEO_LINE_LEN +
 				 xoff * bpp);
 
@@ -1234,7 +1234,7 @@ static int display_rle8_bitmap(struct bmp_image *img, int xoff, int yoff,
 				x = 0;
 				y--;
 				fbp = (unsigned char *)
-					((unsigned int) video_fb_address +
+					((unsigned long) video_fb_address +
 					 (y + yoff) * VIDEO_LINE_LEN +
 					 xoff * bpp);
 				continue;
@@ -1247,7 +1247,7 @@ static int display_rle8_bitmap(struct bmp_image *img, int xoff, int yoff,
 				x += bm[2];
 				y -= bm[3];
 				fbp = (unsigned char *)
-					((unsigned int) video_fb_address +
+					((unsigned long) video_fb_address +
 					 (y + yoff) * VIDEO_LINE_LEN +
 					 xoff * bpp);
 				bm += 4;
@@ -2026,7 +2026,7 @@ static int cfg_video_init(void)
 	if (pGD == NULL)
 		return -1;
 
-	video_fb_address = (void *) VIDEO_FB_ADRS;
+	video_fb_address = (void *)(unsigned long) VIDEO_FB_ADRS;
 
 	cfb_do_flush_cache = cfb_fb_is_in_dram() && dcache_status();
 
