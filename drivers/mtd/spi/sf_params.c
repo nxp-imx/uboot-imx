049a20d1c74b1517abe8c7ea818748ca651a0f39
Ye Li <ye.li@nxp.com>
2016-10-26 17:00:23 +0800

    MLK-13450-17 sf: Add Macronix MX25R6435F SPI NOR flash to flash parameters array
    On mx7ulp EVK board, we use MX25R6435F NOR flash, add its parameters
    and IDs to flash parameter array. Otherwise, the flash probe will fails.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- drivers/mtd/spi/sf_params.c
+++ drivers/mtd/spi/sf_params.c
@@ -51,6 +51,7 @@ const struct spi_flash_params spi_flash_params_table[] = {
 	{"MX25L25635F",	   0xc22019, 0x0,	64 * 1024,   512, RD_FULL,		     WR_QPP},
 	{"MX25L51235F",	   0xc2201a, 0x0,	64 * 1024,  1024, RD_FULL,		     WR_QPP},
 	{"MX25L12855E",	   0xc22618, 0x0,	64 * 1024,   256, RD_FULL,		     WR_QPP},
+	{"MX25R6435F",	   0xc22817, 0x0,	64 * 1024,   128, RD_FULL,			 SECT_4K},
 #endif
 #ifdef CONFIG_SPI_FLASH_SPANSION	/* SPANSION */
 	{"S25FL008A",	   0x010213, 0x0,	64 * 1024,    16, RD_NORM,			  0},
