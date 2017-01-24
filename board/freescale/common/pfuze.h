65ed2f0cb6895ea5a1590d5a21e5633ef67a73ab
Ye Li <ye.li@nxp.com>
2017-01-24 16:19:21 +0800

    MLK-14421 imx: Add pfuze pmic common init for DM pmic driver
    Since the DM pmic driver has changed the driver API, add a new file
    pfuze_dm.c for common pfuze100 init.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- board/freescale/common/pfuze.h
+++ board/freescale/common/pfuze.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2014 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -7,7 +8,13 @@
 #ifndef __PFUZE_BOARD_HELPER__
 #define __PFUZE_BOARD_HELPER__
 
+#ifdef CONFIG_DM_PMIC_PFUZE100
+struct udevice *pfuze_common_init(void);
+int pfuze_mode_init(struct udevice *dev, u32 mode);
+
+#elif defined(CONFIG_POWER_PFUZE100)
 struct pmic *pfuze_common_init(unsigned char i2cbus);
 int pfuze_mode_init(struct pmic *p, u32 mode);
+#endif
 
 #endif
--- board/freescale/common/pfuze_dm.c
+++ board/freescale/common/pfuze_dm.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -9,13 +9,13 @@
 #include <power/pmic.h>
 #include <power/pfuze100_pmic.h>
 
-int pfuze_mode_init(struct pmic *p, u32 mode)
+int pfuze_mode_init(struct udevice *dev, u32 mode)
 {
 	unsigned char offset, i, switch_num;
 	u32 id;
 	int ret;
 
-	pmic_reg_read(p, PFUZE100_DEVICEID, &id);
+	id = pmic_reg_read(dev, PFUZE100_DEVICEID);
 	id = id & 0xf;
 
 	if (id == 0) {
@@ -29,14 +29,14 @@ int pfuze_mode_init(struct pmic *p, u32 mode)
 		return -EINVAL;
 	}
 
-	ret = pmic_reg_write(p, PFUZE100_SW1ABMODE, mode);
+	ret = pmic_reg_write(dev, PFUZE100_SW1ABMODE, mode);
 	if (ret < 0) {
 		printf("Set SW1AB mode error!\n");
 		return ret;
 	}
 
 	for (i = 0; i < switch_num - 1; i++) {
-		ret = pmic_reg_write(p, offset + i * SWITCH_SIZE, mode);
+		ret = pmic_reg_write(dev, offset + i * SWITCH_SIZE, mode);
 		if (ret < 0) {
 			printf("Set switch 0x%x mode error!\n",
 			       offset + i * SWITCH_SIZE);
@@ -47,47 +47,43 @@ int pfuze_mode_init(struct pmic *p, u32 mode)
 	return ret;
 }
 
-struct pmic *pfuze_common_init(unsigned char i2cbus)
+struct udevice *pfuze_common_init(void)
 {
-	struct pmic *p;
+	struct udevice *dev;
 	int ret;
-	unsigned int reg;
+	unsigned int reg, dev_id, rev_id;
 
-	ret = power_pfuze100_init(i2cbus);
-	if (ret)
+	ret = pmic_get("pfuze100", &dev);
+	if (ret == -ENODEV)
 		return NULL;
 
-	p = pmic_get("PFUZE100");
-	ret = pmic_probe(p);
-	if (ret)
-		return NULL;
-
-	pmic_reg_read(p, PFUZE100_DEVICEID, &reg);
-	printf("PMIC:  PFUZE100 ID=0x%02x\n", reg);
+	dev_id = pmic_reg_read(dev, PFUZE100_DEVICEID);
+	rev_id = pmic_reg_read(dev, PFUZE100_REVID);
+	printf("PMIC: PFUZE100! DEV_ID=0x%x REV_ID=0x%x\n", dev_id, rev_id);
 
 	/* Set SW1AB stanby volage to 0.975V */
-	pmic_reg_read(p, PFUZE100_SW1ABSTBY, &reg);
+	reg = pmic_reg_read(dev, PFUZE100_SW1ABSTBY);
 	reg &= ~SW1x_STBY_MASK;
 	reg |= SW1x_0_975V;
-	pmic_reg_write(p, PFUZE100_SW1ABSTBY, reg);
+	pmic_reg_write(dev, PFUZE100_SW1ABSTBY, reg);
 
 	/* Set SW1AB/VDDARM step ramp up time from 16us to 4us/25mV */
-	pmic_reg_read(p, PFUZE100_SW1ABCONF, &reg);
+	reg = pmic_reg_read(dev, PFUZE100_SW1ABCONF);
 	reg &= ~SW1xCONF_DVSSPEED_MASK;
 	reg |= SW1xCONF_DVSSPEED_4US;
-	pmic_reg_write(p, PFUZE100_SW1ABCONF, reg);
+	pmic_reg_write(dev, PFUZE100_SW1ABCONF, reg);
 
 	/* Set SW1C standby voltage to 0.975V */
-	pmic_reg_read(p, PFUZE100_SW1CSTBY, &reg);
+	reg = pmic_reg_read(dev, PFUZE100_SW1CSTBY);
 	reg &= ~SW1x_STBY_MASK;
 	reg |= SW1x_0_975V;
-	pmic_reg_write(p, PFUZE100_SW1CSTBY, reg);
+	pmic_reg_write(dev, PFUZE100_SW1CSTBY, reg);
 
 	/* Set SW1C/VDDSOC step ramp up time from 16us to 4us/25mV */
-	pmic_reg_read(p, PFUZE100_SW1CCONF, &reg);
+	reg = pmic_reg_read(dev, PFUZE100_SW1CCONF);
 	reg &= ~SW1xCONF_DVSSPEED_MASK;
 	reg |= SW1xCONF_DVSSPEED_4US;
-	pmic_reg_write(p, PFUZE100_SW1CCONF, reg);
+	pmic_reg_write(dev, PFUZE100_SW1CCONF, reg);
 
-	return p;
+	return dev;
 }
