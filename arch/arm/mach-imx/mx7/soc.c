fd33de063a9c8a233ef3790d0b066df9b48fb516
Ye Li <ye.li@nxp.com>
2017-02-07 22:48:01 +0800

    MLK-14422 imx7d: wdog: Overwrite the reset_cpu to turn off internal reset signal
    Set wdog WCR register SRS bit to turn off internal reset signal WDOG_RESET_B_DEB
    for mx7d. So that the warm reset is disabled.
    The WDA is cleared to output WDOG_B immediately to reset the board.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 6d09863801695d975060fbc240147a9067dbafbf)
    (cherry picked from commit cde504077e1ce94d9ad34ece0f3e882972404297)
    (cherry picked from commit b0d49a10b23f0b9a7d8cc9299e6eac6b39ce1434)

--- arch/arm/mach-imx/mx7/soc.c
+++ arch/arm/mach-imx/mx7/soc.c
@@ -18,6 +18,7 @@
 #include <imx_thermal.h>
 #include <fsl_sec.h>
 #include <asm/setup.h>
+#include <fsl_wdog.h>
 
 #define IOMUXC_GPR1		0x4
 #define BM_IOMUXC_GPR1_IRQ	0x1000
@@ -417,3 +418,16 @@ void reset_misc(void)
 #endif
 }
 
+void reset_cpu(ulong addr)
+{
+	struct watchdog_regs *wdog = (struct watchdog_regs *)WDOG1_BASE_ADDR;
+
+	/* Clear WDA to trigger WDOG_B immediately */
+	writew((WCR_WDE | WCR_SRS), &wdog->wcr);
+
+	while (1) {
+		/*
+		 * spin for .5 seconds before reset
+		 */
+	}
+}
