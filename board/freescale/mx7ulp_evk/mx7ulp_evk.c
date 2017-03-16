e8795e4dc6eb5f3c9583e1d787368ff1e1ffb48f
Ye Li <ye.li@nxp.com>
2017-03-16 11:25:35 +0800

    MLK-14445-2 mx7ulp_evk: Add QSPI flash support
    Porting the QSPI flash board support from v2016.03, and convert to use
    DM QSPI driver.
    Since we need to support QSPI at default in u-boot, change the default
    DTS file to qspi enabled DTS.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 41895cd598be6c4a64fc4fec521120e4962abc28)
    (cherry picked from commit b4698ce0e5b6952a88702075ce905a059da277d9)
    (cherry picked from commit 2fa81543fa49924a92a470cce62fbca7544ecd56)

--- board/freescale/mx7ulp_evk/mx7ulp_evk.c
+++ board/freescale/mx7ulp_evk/mx7ulp_evk.c
@@ -14,6 +14,7 @@
 DECLARE_GLOBAL_DATA_PTR;
 
 #define UART_PAD_CTRL	(PAD_CTL_PUS_UP)
+#define QSPI_PAD_CTRL1	(PAD_CTL_PUS_UP | PAD_CTL_DSE)
 
 int dram_init(void)
 {
@@ -33,6 +34,36 @@ static void setup_iomux_uart(void)
 					 ARRAY_SIZE(lpuart4_pads));
 }
 
+#ifdef CONFIG_FSL_QSPI
+#ifndef CONFIG_DM_SPI
+static iomux_cfg_t const quadspi_pads[] = {
+	MX7ULP_PAD_PTB8__QSPIA_SS0_B | MUX_PAD_CTRL(QSPI_PAD_CTRL1),
+	MX7ULP_PAD_PTB15__QSPIA_SCLK  | MUX_PAD_CTRL(QSPI_PAD_CTRL1),
+	MX7ULP_PAD_PTB16__QSPIA_DATA3 | MUX_PAD_CTRL(QSPI_PAD_CTRL1),
+	MX7ULP_PAD_PTB17__QSPIA_DATA2 | MUX_PAD_CTRL(QSPI_PAD_CTRL1),
+	MX7ULP_PAD_PTB18__QSPIA_DATA1 | MUX_PAD_CTRL(QSPI_PAD_CTRL1),
+	MX7ULP_PAD_PTB19__QSPIA_DATA0 | MUX_PAD_CTRL(QSPI_PAD_CTRL1),
+};
+#endif
+
+int board_qspi_init(void)
+{
+	u32 val;
+#ifndef CONFIG_DM_SPI
+	mx7ulp_iomux_setup_multiple_pads(quadspi_pads, ARRAY_SIZE(quadspi_pads));
+#endif
+
+	/* enable clock */
+	val = readl(PCC1_RBASE + 0x94);
+
+	if (!(val & 0x20000000)) {
+		writel(0x03000003, (PCC1_RBASE + 0x94));
+		writel(0x43000003, (PCC1_RBASE + 0x94));
+	}
+	return 0;
+}
+#endif
+
 int board_early_init_f(void)
 {
 	setup_iomux_uart();
@@ -44,6 +75,10 @@ int board_init(void)
 {
 	/* address of boot parameters */
 	gd->bd->bi_boot_params = PHYS_SDRAM + 0x100;
+
+#ifdef CONFIG_FSL_QSPI
+	board_qspi_init();
+#endif
 
 	return 0;
 }
