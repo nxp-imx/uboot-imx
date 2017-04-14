867499766e0f2003be2ba162561f5da15ae82356
Gao Pan <pandy.gao@nxp.com>
2017-04-14 15:51:44 +0800

    MLK-14698 imx: lpi2c: fix clock issue when NACK detected
    For LPI2C IP, NACK is detected by the rising edge of the ninth clock.
    In current uboot driver, once NACK is detected, it will reset and then
    disable LPI2C master. As a result, we can never see the falling edge
    of the ninth clock.
    
    Signed-off-by: Gao Pan <pandy.gao@nxp.com>
    (cherry picked from commit dd139ee52b709c95af3e0c968bcbc3cf42cca408)
    (cherry picked from commit 59643c7b34415c6a23e0d73a8aed9145b0220a47)

--- drivers/i2c/imx_lpi2c.c
+++ drivers/i2c/imx_lpi2c.c
@@ -17,6 +17,7 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 #define LPI2C_FIFO_SIZE 4
+#define LPI2C_NACK_TOUT_MS 1
 #define LPI2C_TIMEOUT_MS 100
 
 /* Weak linked function for overridden by some SoC power function */
@@ -186,6 +187,7 @@ static int bus_i2c_stop(struct imx_lpi2c_reg *regs)
 {
 	lpi2c_status_t result = LPI2C_SUCESS;
 	u32 status;
+	ulong start_time;
 
 	result = bus_i2c_wait_for_tx_ready(regs);
 	if (result) {
@@ -196,7 +198,8 @@ static int bus_i2c_stop(struct imx_lpi2c_reg *regs)
 	/* send stop command */
 	writel(LPI2C_MTDR_CMD(0x2), &regs->mtdr);
 
-	while (result == LPI2C_SUCESS) {
+	start_time = get_timer(0);
+	while (1) {
 		status = readl(&regs->msr);
 		result = imx_lpci2c_check_clear_error(regs);
 		/* stop detect flag */
@@ -205,6 +208,11 @@ static int bus_i2c_stop(struct imx_lpi2c_reg *regs)
 			status &= LPI2C_MSR_SDF_MASK;
 			writel(status, &regs->msr);
 			break;
+		}
+
+		if (get_timer(start_time) > LPI2C_NACK_TOUT_MS) {
+			debug("stop timeout\n");
+			return -ETIMEDOUT;
 		}
 	}
 
@@ -365,10 +373,8 @@ static int imx_lpi2c_probe_chip(struct udevice *bus, u32 chip,
 	}
 
 	result = bus_i2c_stop(regs);
-	if (result) {
+	if (result)
 		bus_i2c_init(bus, 100000);
-		return -result;
-	}
 
 	return result;
 }
