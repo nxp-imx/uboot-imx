2480a0160c7fe4d38e2d718e0cd6aa01bd0b3f05
Stefan-Gabriel Mirea <stefan-gabriel.mirea@nxp.com>
2017-05-08 16:21:53 +0100

    s32v234bbmini: Enable PHY auto-negociation
    Override strap-in for RGMII to advertise all capabilities.
    
    Issue: ALB-1371
    Signed-off-by: Stefan-Gabriel Mirea <stefan-gabriel.mirea@nxp.com>

--- include/configs/s32v234bbmini.h
+++ include/configs/s32v234bbmini.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
  * (C) Copyright 2015-2016 Freescale Semiconductor, Inc.
- * (C) Copyright 2016,2020 NXP
+ * (C) Copyright 2016-2017,2020 NXP
  */
 
 /*
@@ -18,6 +18,10 @@
 #define	FDT_FILE s32v234-bbmini.dtb
 
 #define CONFIG_PCIE_EP_MODE
+
+#define CONFIG_BOARD_EXTRA_ENV_SETTINGS \
+	"setphy=mii write 3 d 2; mii write 3 e 2; mii write 3 d 4002; " \
+	"mii write 3 e 8000; mii write 3 0 8000;\0"
 
 /* we include this file here because it depends on the above definitions */
 #include <configs/s32v234evb_2016q4.h>
