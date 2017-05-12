cd9db7ced900ac17436ee82fa84189099ae52e0a
Jean-Jacques Hiblot <jjhiblot@ti.com>
2017-05-12 20:16:33 +0200

    mmc: add a new mmc parameter to disable mmc clock
    mmc clock has to be disabled in certain cases like during
    the voltage switch sequence. Modify mmc_set_clock function
    to take disable as an argument that signifies if the
    clock has to be enabled or disabled.
    
    Signed-off-by: Kishon Vijay Abraham I <kishon@ti.com>
    Signed-off-by: Jean-Jacques Hiblot <jjhiblot@ti.com>

--- drivers/mmc/fsl_esdhc.c
+++ drivers/mmc/fsl_esdhc.c
@@ -677,7 +677,7 @@ static int esdhc_init(struct mmc *mmc)
 #endif
 
 	/* Set the initial clock speed */
-	mmc_set_clock(mmc, 400000);
+	mmc_set_clock(mmc, 400000, false);
 
 	/* Disable the BRR and BWR bits in IRQSTAT */
 	esdhc_clrbits32(&regs->irqstaten, IRQSTATEN_BRR | IRQSTATEN_BWR);
