954f9ede48540e15202bace81e6384ef153c0485
Ye Li <ye.li@nxp.com>
2017-05-17 00:23:06 -0500

    MLK-14938-1 imx: Add SCFW-API and IPC functions
    Using the latest SCFW-API based on commit from SCFW master branch:
    "commit 0d7528ab8d243d8de60ac33a473a4988c3135b28
     Author: Ranjani Vaidyanathan <ranjani.vaidyanathan@nxp.com>
     Date:   Wed May 10 13:13:12 2017 -0500
    
     Add support to change the parent of clock slices in a SS.
    "
    
    Signed-off-by: Ranjani Vaidyanathan <Ranjani.Vaidyanathan@nxp.com>
    Signed-off-by: Ye Li <ye.li@nxp.com>

---
+++ arch/arm/include/asm/imx-common/sci/sci.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+/*==========================================================================*/
+/*!
+ * @file sci.h
+ *
+ * Header file containing the public System Controller Interface (SCI)
+ * definitions.
+ *
+ *
+ * @{
+ */
+/*==========================================================================*/
+
+#ifndef _SC_SCI_H
+#define _SC_SCI_H
+
+/* Defines */
+
+/* Includes */
+
+#include <asm/imx-common/sci/types.h>
+#include <asm/imx-common/sci/ipc.h>
+#include <asm/imx-common/sci/svc/misc/api.h>
+#include <asm/imx-common/sci/svc/otp/api.h>
+#include <asm/imx-common/sci/svc/pad/api.h>
+#include <asm/imx-common/sci/svc/pm/api.h>
+#include <asm/imx-common/sci/svc/rm/api.h>
+#include <asm/imx-common/sci/svc/timer/api.h>
+
+#define SC_IPC_AP_CH0       	(MU_BASE_ADDR(0))
+#define SC_IPC_AP_CH1       	(MU_BASE_ADDR(1))
+#define SC_IPC_AP_CH2       	(MU_BASE_ADDR(2))
+#define SC_IPC_AP_CH3       	(MU_BASE_ADDR(3))
+#define SC_IPC_AP_CH4       	(MU_BASE_ADDR(4))
+
+#define SC_IPC_CH		SC_IPC_AP_CH0
+
+/* Types */
+
+/* Functions */
+
+#endif /* _SC_SCI_H */
+
+/**@}*/
+
