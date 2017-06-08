facfd0a1682600d7d1c2fbc65a918c00bc1ec73b
sanshan zhang <sanshan.zhang@nxp.com>
2017-06-08 10:43:42 +0800

    MA-9702-3 [Android] arm2-8qm: bring up android in v2017.
    add configs for arm2-8qm.
    
    Change-Id: Ief47e1587f6dbc4f16b243c29fa9bc9c8c80c044
    Signed-off-by: sanshan zhang <sanshan.zhang@nxp.com>

---
+++ include/configs/imx8qm_arm2_android.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013-2016 Freescale Semiconductor, Inc. All Rights Reserved.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef IMX8QM_ARM2_ANDROID_H
+#define IMX8QM_ARM2_ANDROID_H
+
+#define CONFIG_USBD_HS
+#define CONFIG_BOOTLOADER_OFFSET_33K
+
+#define CONFIG_BCB_SUPPORT
+#define CONFIG_CMD_READ
+#define CONFIG_USB_FUNCTION_MASS_STORAGE
+#define CONFIG_USB_GADGET_VBUS_DRAW	2
+
+#define CONFIG_USB_FUNCTION_FASTBOOT
+#define CONFIG_CMD_FASTBOOT
+#define CONFIG_ANDROID_BOOT_IMAGE
+#define CONFIG_FASTBOOT_FLASH
+
+#define CONFIG_FSL_FASTBOOT
+#define CONFIG_ANDROID_RECOVERY
+
+#if defined CONFIG_NAND_BOOT
+#define CONFIG_FASTBOOT_STORAGE_NAND
+#elif defined CONFIG_SYS_BOOT_SATA
+#define CONFIG_FASTBOOT_STORAGE_SATA
+#define CONFIG_FASTBOOT_SATA_NO 0
+#else
+#define CONFIG_FASTBOOT_STORAGE_MMC
+#endif
+
+#if defined(CONFIG_FASTBOOT_STORAGE_NAND)
+#define ANDROID_FASTBOOT_NAND_PARTS "16m@64m(boot) 16m@80m(recovery) 1m@96m(misc) 810m@97m(android_root)ubifs"
+#endif
+
+#define CONFIG_CMD_BOOTA
+#define CONFIG_SUPPORT_RAW_INITRD
+#define CONFIG_SERIAL_TAG
+
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#undef CONFIG_BOOTCOMMAND
+
+#define CONFIG_EXTRA_ENV_SETTINGS					\
+	"splashpos=m,m\0"	  \
+	"fdt_high=0xffffffffffffffff\0"	  \
+	"initrd_high=0xffffffffffffffff\0" \
+
+#define CONFIG_FASTBOOT_BUF_ADDR   CONFIG_SYS_LOAD_ADDR
+#define CONFIG_FASTBOOT_BUF_SIZE   0x19000000
+
+#endif /* MX6_SABRE_ANDROID_COMMON_H */
