b5aeddb57b622e794663035f6ec6b144c7da6f87
Zhang Bo <bo.zhang@nxp.com>
2017-06-29 16:38:45 +0800

    MA-9795[Android] imx8mq-evk: bring up android in u-boot v2017
    add configs for imx8mq-evk board.
    
    Change-Id: I9686bc8700fa6ad1355892176ebcf45d758331ea
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>

---
+++ include/configs/imx8mq_evk_android.h
@@ -0,0 +1,36 @@
+/*
+ * opyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef IMX8MQ_EVK_ANDROID_H
+#define IMX8MQ_EVK_ANDROID_H
+
+#define CONFIG_BOOTLOADER_OFFSET_33K
+#define CONFIG_BCB_SUPPORT
+#define CONFIG_CMD_READ
+
+#define CONFIG_USB_FUNCTION_FASTBOOT
+#define CONFIG_ANDROID_BOOT_IMAGE
+#define CONFIG_FASTBOOT_STORAGE_MMC
+
+#define CONFIG_FSL_FASTBOOT
+#define CONFIG_ANDROID_RECOVERY
+
+#define CONFIG_FASTBOOT_BUF_ADDR   CONFIG_SYS_LOAD_ADDR
+#define CONFIG_FASTBOOT_BUF_SIZE   0x19000000
+
+#define CONFIG_CMD_BOOTA
+#define CONFIG_SUPPORT_RAW_INITRD
+#define CONFIG_SERIAL_TAG
+
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#undef CONFIG_BOOTCOMMAND
+
+#define CONFIG_EXTRA_ENV_SETTINGS		\
+	"splashpos=m,m\0"			\
+	"fdt_high=0xffffffffffffffff\0"		\
+	"initrd_high=0xffffffffffffffff\0"	\
+
+#endif /* IMX8MQ_EVK_ANDROID_H */
