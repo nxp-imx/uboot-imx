c85a0f78da67354cce56b6c1c1019b64d3646757
Ye Li <ye.li@nxp.com>
2017-08-01 05:06:25 -0500

    MLK-16118-6 net: fec_mxc: Add the init_clk_fec function for non-DM driver
    When the power domain driver is enabled, we need to enable clocks after power
    domain on. So the clock settings can't set in board_init, needs to set them
    when the device is probed. Add this weak function in driver, that SoC codes
    can implement the clock settings.
    
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit c0e4ac66196b20f363f711fb18e40b70e3be9240)
    (cherry picked from commit 187ea376980be12e69c45bd6e62c7ca1559046f6)
    (cherry picked from commit 9c0be2929558498429b31868d67e1d6695546ac9)
    (cherry picked from commit 91b0a78c99f939077e9cf33ac8b3a22cebc311b2)
    (cherry picked from commit 485d10ec1442cb9148fae9afd11933423636b426)

--- drivers/net/fec_mxc.c
+++ drivers/net/fec_mxc.c
@@ -1104,6 +1104,10 @@ struct mii_dev *fec_get_miibus(ulong base_addr, int dev_id)
 	return bus;
 }
 
+__weak void init_clk_fec(int index)
+{
+}
+
 #ifndef CONFIG_DM_ETH
 #ifdef CONFIG_PHYLIB
 int fec_probe(struct bd_info *bd, int dev_id, uint32_t base_addr,
@@ -1229,6 +1233,7 @@ int fecmxc_initialize_multi(struct bd_info *bd, int dev_id, int phy_id,
 #else
 	base_mii = addr;
 #endif
+	init_clk_fec(dev_id);
 	debug("eth_init: fec_probe(bd, %i, %i) @ %08x\n", dev_id, phy_id, addr);
 	bus = fec_get_miibus(base_mii, dev_id);
 	if (!bus)
