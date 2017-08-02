19c5632dc481d0fe983a4699d263bc9fc14edfcf
Ye Li <ye.li@nxp.com>
2017-08-01 20:55:03 -0500

    MLK-16118-1 power: Add power domain driver for i.MX8QM/QXP
    Add the power domain DM driver for i.MX8, that it depends on the DTB
    power domain trees to generate the power domain provider devices. Users
    needs add power domain trees with property "compatible = "nxp,imx8-pd";"
    
    - When power on one PD device, the driver will power on its ancestor PD devices
    in power domain tree.
    
    - When power off on PD device, the driver will check its child PD devices first,
    only all child PD devices are off, then power off the current PD device. Then the
    driver checks sibling PD devices. If sibling PD devices are off, then it will power
    off parent PD device.
    
    - There is no counter maintained in this driver, but a state to hold current on/off
    state. So the request and free functions are empty.
    
    - The power domain implemetation in i.MX8 DTB set the "#power-domain-cells" to 0, so
    there is no ID binding with each PD device. We don't use "id" variable in
    struct power_domain. At same time, we have to set of_xlate to empty to bypass standard
    of_xlate in uclass driver.
    
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Ye Li <ye.li@nxp.com>

---
+++ arch/arm/include/asm/arch-imx8/power-domain.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef _ASM_ARCH_POWER_DOMAIN_H
+#define _ASM_ARCH_POWER_DOMAIN_H
+
+#include <asm/imx-common/sci/types.h>
+
+struct imx8_power_domain_platdata{
+	sc_rsrc_t resource_id;
+};
+
+#endif
