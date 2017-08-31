7b790cf7c89f118e4fe0d74b426de16623ca1899
Ye Li <ye.li@nxp.com>
2017-08-30 21:58:03 -0500

    MLK-16333 imx8qm/qxp: Change memory region 0x0-0x1c000000 to strongly order
    This memory region is for LSIO subsystem, including OCRAM, AP ROM,
    flexspi0 mapped memory and flexspi1 buffer. If we set it to cachable,
    the AHB read in flexspi driver will have coherence problem.
    
    This patch set this memory region to strongly order to avoid any issue
    in driver.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/cpu/armv8/imx8/cpu.c
+++ arch/arm/cpu/armv8/imx8/cpu.c
@@ -992,8 +992,7 @@ void dram_init_banksize(void)
 static u64 get_block_attrs(sc_faddr_t addr_start)
 {
 	if ((addr_start >= PHYS_SDRAM_1 && addr_start <= ((sc_faddr_t)PHYS_SDRAM_1 + PHYS_SDRAM_1_SIZE))
-		|| (addr_start >= PHYS_SDRAM_2 && addr_start <= ((sc_faddr_t)PHYS_SDRAM_2 + PHYS_SDRAM_2_SIZE))
-		|| (addr_start >= 0x0 && addr_start <= ((sc_faddr_t)0x20000000)))
+		|| (addr_start >= PHYS_SDRAM_2 && addr_start <= ((sc_faddr_t)PHYS_SDRAM_2 + PHYS_SDRAM_2_SIZE)))
 		return (PTE_BLOCK_MEMTYPE(MT_NORMAL) | PTE_BLOCK_OUTER_SHARE);
 
 	return (PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) | PTE_BLOCK_NON_SHARE | PTE_BLOCK_PXN | PTE_BLOCK_UXN);
