67bb87f16e3742d6fab0b7837ffdb8bbed1245d1
Ye Li <ye.li@nxp.com>
2017-09-01 05:26:58 -0500

    MLK-16361-1 imx8qm/qxp: Send boot status to SCFW if booted by SC ROM
    We met u-boot hang when booting from eMMC fastboot on QM/QXP boards.
    The hang happens on u-boot accessing USB2 PHY for checking USB boot.
    The root cause is when putting AP image (u-boot-atf.bin) in first container,
    the USB2 PHY reset bit won’t be set in CONN SS by SCFW, because this SS has been
    powered on by SC ROM.
    
    In normal boot case, we won't meet such issue. Because we put u-boot-atf.bin in second
    container and AP ROM will boot up for loading this binary. When AP ROM completes the
    loading, it calls “misc_boot_status” API to power off the boot device and also power
    off the CONN SS. Then when u-boot enables any the module in CONN, the CONN SS will power
    on again by SCFW and set the USB2 PHY reset bit.
    
    Since the clock settings are different in SC ROM and SCFW, so it is suggested to power off
    CONN SS when booting is completed. In this patch, we check the g_ap_mu field in pass over
    structure which is used to pass into from SC ROM to AP ROM. This field is set only when AP
    image is included in the second container. If this field is not set, we suppose the booting
    only uses SC ROM, then u-boot calls "misc_boot_status" at early stage to power off
    boot device.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/include/asm/arch-imx8/sys_proto.h
+++ arch/arm/include/asm/arch-imx8/sys_proto.h
@@ -38,6 +38,15 @@ struct rom_sw_info_t {
 	uint32_t rsvd[3];
 };
 
+struct pass_over_info_t {
+	uint16_t barker;
+	uint16_t len;
+	uint32_t g_bt_cfg_shadow;
+	uint32_t card_address_mode;
+	uint32_t bad_block_count_met;
+	uint32_t g_ap_mu;
+};
+
 int print_bootinfo(void);
 int init_otg_power(void);
 void power_off_pd_devices(const char* permanent_on_devices[], int size);
