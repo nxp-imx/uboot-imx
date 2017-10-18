f8d69b49c95312d54ec41d0f102e5c8f7acce552
Zhang Bo <bo.zhang@nxp.com>
2017-10-18 14:23:20 +0800

    MA-10477[Android] Remove NAND partition definitions and operations in bcb
    Remove NAND partitions definitions in .h file.
    Remove NAND related bcb operations.
    
    Change-Id: I38774732e80f0402d63ea6d3f862e385f3e01e31
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>

--- include/configs/mx6sabreandroid_common.h
+++ include/configs/mx6sabreandroid_common.h
@@ -8,8 +8,7 @@
 #ifndef MX6_SABRE_ANDROID_COMMON_H
 #define MX6_SABRE_ANDROID_COMMON_H
 #include "mx_android_common.h"
-/* For NAND we don't support lock/unlock */
-#ifndef CONFIG_NAND_BOOT
+
 #define CONFIG_FASTBOOT_LOCK
 #define FSL_FASTBOOT_FB_DEV "mmc"
 #define FASTBOOT_ENCRYPT_LOCK
@@ -17,10 +16,5 @@
 #define CONFIG_CMD_FSL_CAAM_KB
 #define CONFIG_SHA1
 #define CONFIG_SHA256
-#endif
-
-#if defined(CONFIG_NAND_BOOT)
-#define ANDROID_FASTBOOT_NAND_PARTS "16m@64m(boot) 16m@80m(recovery) 1m@96m(misc) 810m@97m(android_root)ubifs"
-#endif
 
 #endif /* MX6_SABRE_ANDROID_COMMON_H */
--- include/configs/mx6sxsabreautoandroid.h
+++ include/configs/mx6sxsabreautoandroid.h
@@ -10,8 +10,6 @@
 #define __MX6SX_SABREAUTO_ANDROID_H
 #include "mx_android_common.h"
 
-/* For NAND we don't support lock/unlock */
-#ifndef CONFIG_NAND_BOOT
 #define CONFIG_FASTBOOT_LOCK
 #define FSL_FASTBOOT_FB_DEV "mmc"
 #define FASTBOOT_ENCRYPT_LOCK
@@ -19,11 +17,5 @@
 #define CONFIG_CMD_FSL_CAAM_KB
 #define CONFIG_SHA1
 #define CONFIG_SHA256
-#endif
-
-
-#if defined(CONFIG_FASTBOOT_STORAGE_NAND)
-#define ANDROID_FASTBOOT_NAND_PARTS "16m@64m(boot) 16m@80m(recovery) 1m@96m(misc) 810m@97m(android_root)ubifs"
-#endif
 
 #endif
