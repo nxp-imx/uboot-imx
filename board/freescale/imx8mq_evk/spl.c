a934533f26bdf2f5a0d9afe41f0a3a0e21e1c3d5
Peng Fan <peng.fan@nxp.com>
2017-10-18 09:56:36 +0800

    MLK-16599 imx8m: support SPL FIT
    Support SPL FIT. Uboot text base are also modified, because spl
    will load fit to address before text base of uboot.
    
    According to new ATF request, modified the SPL stack/bss to OCRAM_S
    space.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- board/freescale/imx8mq_evk/spl.c
+++ board/freescale/imx8mq_evk/spl.c
@@ -198,6 +198,16 @@ void spl_board_init(void)
 	puts("Normal Boot\n");
 }
 
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	/* Just empty function now - can't decide what to choose */
+	debug("%s: %s\n", __func__, name);
+
+	return 0;
+}
+#endif
+
 void board_init_f(ulong dummy)
 {
 	/* Clear global data */
