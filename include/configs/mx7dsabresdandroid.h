c17b87fafce7faf46c0c9b6024bc189793e67265
ji.luo <ji.luo@nxp.com>
2017-11-06 15:36:30 +0800

    MA-10580 Open lock/unlock feature for evk_6sl and sabresd_7d
    Add configs to open lock/unlock feature for evk_6sl and sabresd_7d.
    
    Change-Id: I18f8917df06290efb553b10c9bbdaeb145f8a423
    Signed-off-by: ji.luo <ji.luo@nxp.com>

--- include/configs/mx7dsabresdandroid.h
+++ include/configs/mx7dsabresdandroid.h
@@ -8,4 +8,12 @@
 #ifndef __MX7D_SABRESD_ANDROID_H
 #define __MX7D_SABRESD_ANDROID_H
 #include "mx_android_common.h"
-#endif
+
+/* lock/unlock stuff */
+#define FASTBOOT_ENCRYPT_LOCK
+#define CONFIG_FASTBOOT_LOCK
+#define FSL_FASTBOOT_FB_DEV "mmc"
+#define CONFIG_FSL_CAAM_KB
+#define CONFIG_SHA1
+
+#endif /* __MX7D_SABRESD_ANDROID_H */
