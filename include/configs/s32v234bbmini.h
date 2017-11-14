e107cd4bf9ee3ba2fd6be7badc643b0b37505109
Iustin Dumitrescu <iustin.dumitrescu@nxp.com>
2017-11-14 09:56:25 +0000

    s32v234bbmini: Enable ramdisk support for nfs boot
    Enable CONFIG_BOARD_USE_RAMFS_IN_NFSBOOT.
    
    Issue: ALB-1849
    Signed-off-by: Iustin Dumitrescu <iustin.dumitrescu@nxp.com>

--- include/configs/s32v234bbmini.h
+++ include/configs/s32v234bbmini.h
@@ -19,6 +19,7 @@
 
 #define CONFIG_PCIE_EP_MODE
 
+#define CONFIG_BOARD_USE_RAMFS_IN_NFSBOOT
 #define CONFIG_BOARD_EXTRA_ENV_SETTINGS \
 	"setphy=mii write 3 d 2; mii write 3 e 2; mii write 3 d 4002; " \
 	"mii write 3 e 8000; mii write 3 0 8000;\0"
