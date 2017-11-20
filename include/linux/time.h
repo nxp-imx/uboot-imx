1b0b4efcb94ae94a47d8a172e6c7a3e769a0041f
Iustin Dumitrescu <iustin.dumitrescu@nxp.com>
2017-11-20 10:46:42 +0000

    time: Add PER_SEC defines from Linux
    Add all PER_SEC defines to time.h, imported from Linux.
    
    Issue: ALB-1120
    Signed-off-by: Iustin Dumitrescu <iustin.dumitrescu@nxp.com>

--- include/linux/time.h
+++ include/linux/time.h
@@ -25,6 +25,14 @@
 
 #define isleap(y) ((((y) % 4) == 0 && ((y) % 100) != 0) || ((y) % 400) == 0)
 
+/* Parameters used to convert the timespec values: */
+#define MSEC_PER_SEC	1000L
+#define USEC_PER_MSEC	1000L
+#define NSEC_PER_USEC	1000L
+#define NSEC_PER_MSEC	1000000L
+#define USEC_PER_SEC	1000000L
+#define NSEC_PER_SEC	1000000000L
+#define FSEC_PER_SEC	1000000000000000LL
 
 /* Used by other time functions.  */
 struct tm {
