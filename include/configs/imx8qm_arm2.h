nxp-imx/uboot-imx

commit 432e5b4347841095c3b5a8a0d106f35deadd006e

MLK-17205-3 video: imx: hdp: Adding configs for HDP firmware loading
Added default environment for hdp loading.
Added hdp loading as default for iMX8QM ARM2 and MEK boards.

Signed-off-by: Oliver Brown <oliver.brown@nxp.com>

include/configs/imx8qm_arm2.h
@@ -174,13 +174,16 @@
 	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
 	"mmcautodetect=yes\0" \
 	"mmcargs=setenv bootargs console=${console},${baudrate} root=${mmcroot} " \
-	"video=imxdpufb5:off video=imxdpufb6:off video=imxdpufb7:off\0" \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
 	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
 	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+	"hdp_addr=0x84000000\0" \
+	"hdp_file=dpfw.bin\0" \
+	"loadhdp=fatload mmc ${mmcdev}:${mmcpart} ${hdp_addr} ${hdp_file}\0" \
 	"mmcboot=echo Booting from mmc ...; " \
+		"run loadhdp; hdp load ${hdp_addr}; " \
 		"run mmcargs; " \
 		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
 			"if run loadfdt; then " \
@@ -202,6 +205,7 @@
 		"else " \
 			"setenv get_cmd tftp; " \
 		"fi; " \
+		"${get_cmd} ${hdp_addr} ${hdp_file}; hdp load ${hdp_addr}; " \
 		"${get_cmd} ${loadaddr} ${image}; " \
 		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
 			"if ${get_cmd} ${fdt_addr} ${fdt_file}; then " \

include/configs/imx8qm_mek.h
@@ -142,13 +142,16 @@
 	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
 	"mmcautodetect=yes\0" \
 	"mmcargs=setenv bootargs console=${console},${baudrate} root=${mmcroot} " \
-	"video=imxdpufb5:off video=imxdpufb6:off video=imxdpufb7:off\0" \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
 	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
 	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}\0" \
+	"hdp_addr=0x84000000\0" \
+	"hdp_file=hdmitxfw.bin\0" \
+	"loadhdp=fatload mmc ${mmcdev}:${mmcpart} ${hdp_addr} ${hdp_file}\0" \
 	"mmcboot=echo Booting from mmc ...; " \
+		"run loadhdp; hdp load ${hdp_addr}; " \
 		"run mmcargs; " \
 		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
 			"if run loadfdt; then " \
@@ -170,6 +173,7 @@
 		"else " \
 			"setenv get_cmd tftp; " \
 		"fi; " \
+		"${get_cmd} ${hdp_addr} ${hdp_file}; hdp load ${hdp_addr}; " \
 		"${get_cmd} ${loadaddr} ${image}; " \
 		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
 			"if ${get_cmd} ${fdt_addr} ${fdt_file}; then " \

configs/imx8qm_ddr4_arm2_defconfig
@@ -62,6 +62,7 @@ CONFIG_DM_REGULATOR_FIXED=y
 CONFIG_DM_REGULATOR_GPIO=y
 
 CONFIG_VIDEO=y
+CONFIG_VIDEO_IMX_HDP_LOAD=y
 
 CONFIG_PINCTRL=y
 CONFIG_PINCTRL_IMX8=y

configs/imx8qm_lpddr4_arm2_defconfig
@@ -62,6 +62,7 @@ CONFIG_DM_REGULATOR_FIXED=y
 CONFIG_DM_REGULATOR_GPIO=y
 
 CONFIG_VIDEO=y
+CONFIG_VIDEO_IMX_HDP_LOAD=y
 
 CONFIG_PINCTRL=y
 CONFIG_PINCTRL_IMX8=y

configs/imx8qm_lpddr4_arm2_fspi_defconfig
@@ -63,6 +63,7 @@ CONFIG_DM_REGULATOR_FIXED=y
 CONFIG_DM_REGULATOR_GPIO=y
 
 CONFIG_VIDEO=y
+CONFIG_VIDEO_IMX_HDP_LOAD=y
 
 CONFIG_PINCTRL=y
 CONFIG_PINCTRL_IMX8=y

configs/imx8qm_mek_defconfig
@@ -62,6 +62,7 @@ CONFIG_DM_REGULATOR_FIXED=y
 CONFIG_DM_REGULATOR_GPIO=y
 
 CONFIG_VIDEO=y
+CONFIG_VIDEO_IMX_HDP_LOAD=y
 
 CONFIG_PINCTRL=y
 CONFIG_PINCTRL_IMX8=y

configs/imx8qm_mek_fspi_defconfig
@@ -63,6 +63,7 @@ CONFIG_DM_REGULATOR_FIXED=y
 CONFIG_DM_REGULATOR_GPIO=y
 
 CONFIG_VIDEO=y
+CONFIG_VIDEO_IMX_HDP_LOAD=y
 
 CONFIG_PINCTRL=y
 CONFIG_PINCTRL_IMX8=y

commit 77130fcf86a602a5fea0eb842f22237fdfb46bea

MLK-17493: imx8qm: Fix mmcargs variable missing terminator
Missing the '\0' means the value of the mmcargs value includes the
declaration of loadbootscript below.

Fixes: 432e5b434784 ("MLK-17205-3 video: imx: hdp: Adding configs for HDP firmware loading")

Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
Reviewed-by: Ye Li <ye.li@nxp.com>

include/configs/imx8qm_arm2.h
@@ -176,7 +176,7 @@
 	"mmcpart=" __stringify(CONFIG_SYS_MMC_IMG_LOAD_PART) "\0" \
 	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
 	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs console=${console},${baudrate} root=${mmcroot} " \
+	"mmcargs=setenv bootargs console=${console},${baudrate} root=${mmcroot}\0 " \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \

include/configs/imx8qm_mek.h
@@ -137,7 +137,7 @@
 	"mmcpart=" __stringify(CONFIG_SYS_MMC_IMG_LOAD_PART) "\0" \
 	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
 	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs console=${console},${baudrate} root=${mmcroot} " \
+	"mmcargs=setenv bootargs console=${console},${baudrate} root=${mmcroot}\0 " \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \