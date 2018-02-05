aab1dd852e7ab5d3efb6bb4c0809702b1cadfb06
Kay Potthoff <Kay.Potthoff@microsys.de>
2018-02-05 08:24:51 +0100

    mpxs32v234: initial support and configuration
    This adds initial support and configuration for
    the MicroSys SBC-S32 evaluation board.
    
    Signed-off-by: Kay Potthoff <Kay.Potthoff@microsys.de>
    
    - moved SYS_VENDOR config to board/freescale/Kconfig in order to avoid code
    duplication.
    - removed unrelevant configs from board/microsys/mpxs32v234/Kconfig
    - documented magic numbers from /board/microsys/mpxs32v234/mpxs32v234.c
    - documented magic numbers from /board/microsys/mpxs32v234/mpxs32v234.cfg
    - removed BCM switch config
    
    Signed-off-by: Costin Carabas <costin.carabas@nxp.com>
    Signed-off-by: Cosmin Oprea <cosmin.oprea@nxp.com>

---
+++ include/configs/mpxs32v234.h
@@ -0,0 +1,41 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2015-2016 Freescale Semiconductor, Inc.
+ * (C) Copyright 2017 MicroSys Electronics GmbH
+ * Copyright 2020 NXP
+ */
+
+/*
+ * Configuration settings for the MicroSys MPXS32V234 board.
+ */
+
+#ifndef __MPXS32V234_H
+#define __MPXS32V234_H
+
+/* The configurations of this board depend on the definitions in this file and
+ * the ones in the header included at the end, configs/s32v234_common.h
+ */
+
+#define CONFIG_MPXS32V234_R2
+
+/* Config DCU */
+#define CONFIG_FSL_DCU_FB
+
+/*
+ * RTC configuration
+ */
+#define CONFIG_SYS_I2C_MXC_I2C2
+#define CONFIG_SYS_I2C_MXC_I2C3
+
+#define CONFIG_SYS_I2C_RTC_ADDR		0x51
+
+/* Ethernet config */
+#define CONFIG_FEC_XCV_TYPE	RGMII
+#define CONFIG_FEC_MXC_PHYADDR  1
+
+#define	FDT_FILE fsl-s32v234sbc.dtb
+
+/* we include this file here because it depends on the above definitions */
+#include <configs/s32v234_common.h>
+
+#endif
