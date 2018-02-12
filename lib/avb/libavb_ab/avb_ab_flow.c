21b93364d777cbf409139d14eb3e341e2ac1ec33
Luo Ji <ji.luo@nxp.com>
2018-02-12 16:33:49 +0800

    MA-11363 [Android] Don't free memory for boot image in AVB
    We read the boot image to a fixed partition which is defined
    in boot header, this part of memory should not be freed with
    free() otherwise "Synchronous Abort" error may happen.
    
    Change-Id: I0fd9a3e558fda90c4daeee3080e4f9b393e11d98
    Signed-off-by: Luo Ji <ji.luo@nxp.com>

--- lib/avb/libavb_ab/avb_ab_flow.c
+++ lib/avb/libavb_ab/avb_ab_flow.c
@@ -595,7 +595,9 @@ out:
 
   for (n = 0; n < 2; n++) {
     if (slot_data[n] != NULL) {
-      avb_slot_verify_data_free(slot_data[n]);
+      /* the address of bootimage isn't alloced by malloc,
+       * we should not free it. */
+      avb_slot_verify_data_free_fast(slot_data[n]);
     }
   }
 
@@ -603,9 +605,7 @@ out:
     *out_data = data;
   } else {
     if (data != NULL) {
-      /* the address of bootimage isn't alloced by malloc,
-       * we should not free it. */
-      avb_slot_verify_data_free_fast(data);
+      avb_slot_verify_data_free(data);
     }
   }
 
