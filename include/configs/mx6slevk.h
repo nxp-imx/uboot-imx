nxp-imx/uboot-imx

commit 7101be554d231d066cc701f7ae95a01038d2d3dd

MLK-17608: u-boot: config: update dtb filename when OP-TEE is used
When booting with OP-TEE support, a dedicated  dtb file is used.
Allow u-boot to automatically update the fdt filename to use.

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

include/configs/mx6slevk.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2013-2016 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
  *
  * Configuration settings for the Freescale i.MX6SL EVK board.
  *
@@ -88,7 +89,7 @@
 	"console=ttymxc0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx6sl-evk.dtb\0" \
+	"fdt_file=undefined\0" \
 	"fdt_addr=0x83000000\0" \
 	"tee_addr=0x84000000\0" \
 	"tee_file=undefined\0" \
@@ -154,10 +155,18 @@
 				"fi; " \
 			"else " \
 				"bootz; " \
-			"fi; " \
-		"fi;\0"
+			"fi;" \
+		"findfdt="\
+			"if test $fdt_file = undefined; then " \
+				"if test ${tee} = yes; then " \
+					"setenv fdt_file imx6sl-evk-optee.dtb; " \
+				"else " \
+					"setenv fdt_file imx6sl-evk.dtb; " \
+				"fi; " \
+			"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
+	   "run findfdt;" \
 	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \

include/configs/mx6sllevk.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2013-2016 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
  *
  * Configuration settings for the Freescale i.MX6SL EVK board.
  *
@@ -52,7 +53,7 @@
 	"console=ttymxc0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx6sll-evk.dtb\0" \
+	"fdt_file=undefined\0" \
 	"fdt_addr=0x83000000\0" \
 	"tee_addr=0x84000000\0" \
 	"tee_file=undefined\0" \
@@ -119,10 +120,18 @@
 				"fi; " \
 			"else " \
 				"bootz; " \
-			"fi; " \
-		"fi;\0"
+			"fi;" \
+		"findfdt="\
+			"if test $fdt_file = undefined; then " \
+				"if test ${tee} = yes; then " \
+					"setenv fdt_file imx6sll-evk-optee.dtb; " \
+				"else " \
+					"setenv fdt_file imx6sll-evk.dtb; " \
+				"fi; " \
+			"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
+	   "run findfdt;" \
 	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \

include/configs/mx6sxsabreauto.h
@@ -107,7 +107,7 @@
 	"console=ttymxc0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx6sx-sabreauto.dtb\0" \
+	"fdt_file=undefined\0" \
 	"fdt_addr=0x83000000\0" \
 	"tee_addr=0x84000000\0" \
 	"tee_file=uTee-6sxauto\0" \
@@ -175,9 +175,17 @@
 			"else " \
 				"bootz; " \
 			"fi;" \
-		"fi;\0"
+		"findfdt="\
+			"if test $fdt_file = undefined; then " \
+				"if test ${tee} = yes; then " \
+					"setenv fdt_file imx6sx-sabreauto-optee.dtb; " \
+				"else " \
+					"setenv fdt_file imx6sx-sabreauto.dtb; " \
+				"fi; " \
+			"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
+	   "run findfdt;" \
 	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \

include/configs/mx6sxsabresd.h
@@ -1,5 +1,6 @@
 /*
  * Copyright 2014 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
  *
  * Configuration settings for the Freescale i.MX6SX Sabresd board.
  *
@@ -87,7 +88,7 @@
 	"console=ttymxc0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx6sx-sdb.dtb\0" \
+	"fdt_file=undefined\0" \
 	"fdt_addr=0x83000000\0" \
 	"tee_addr=0x84000000\0" \
 	"tee_file=uTee-6sxsdb\0" \
@@ -154,10 +155,18 @@
 				"fi; " \
 			"else " \
 				"bootz; " \
-			"fi; " \
-		"fi;\0"
+			"fi;" \
+		"findfdt="\
+			"if test $fdt_file = undefined; then " \
+				"if test ${tee} = yes; then " \
+					"setenv fdt_file imx6sx-sdb-optee.dtb; " \
+				"else " \
+					"setenv fdt_file imx6sx-sdb.dtb; " \
+				"fi; " \
+			"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
+	   "run findfdt;" \
 	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \

include/configs/mx6sabre_common.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 Freescale Semiconductor, Inc.
+ * Copyright 2018 NXP
  *
  * Configuration settings for the Freescale i.MX6Q SabreSD board.
  *
@@ -239,19 +240,26 @@
 		"findfdt="\
 			"if test $fdt_file = undefined; then " \
 				"if test $board_name = SABREAUTO && test $board_rev = MX6QP; then " \
-					"setenv fdt_file imx6qp-sabreauto.dtb; fi; " \
+					"setenv fdt_file imx6qp-sabreauto; fi; " \
 				"if test $board_name = SABREAUTO && test $board_rev = MX6Q; then " \
-					"setenv fdt_file imx6q-sabreauto.dtb; fi; " \
+					"setenv fdt_file imx6q-sabreauto; fi; " \
 				"if test $board_name = SABREAUTO && test $board_rev = MX6DL; then " \
-					"setenv fdt_file imx6dl-sabreauto.dtb; fi; " \
+					"setenv fdt_file imx6dl-sabreauto; fi; " \
 				"if test $board_name = SABRESD && test $board_rev = MX6QP; then " \
-					"setenv fdt_file imx6qp-sabresd.dtb; fi; " \
+					"setenv fdt_file imx6qp-sabresd; fi; " \
 				"if test $board_name = SABRESD && test $board_rev = MX6Q; then " \
-					"setenv fdt_file imx6q-sabresd.dtb; fi; " \
+					"setenv fdt_file imx6q-sabresd; fi; " \
 				"if test $board_name = SABRESD && test $board_rev = MX6DL; then " \
-					"setenv fdt_file imx6dl-sabresd.dtb; fi; " \
+					"setenv fdt_file imx6dl-sabresd; fi; " \
 				"if test $fdt_file = undefined; then " \
-					"echo WARNING: Could not determine dtb to use; fi; " \
+					"echo WARNING: Could not determine dtb to use; " \
+				"else " \
+					"if test ${tee} = yes; then " \
+						"setenv fdt_file $fdt_file-optee.dtb; " \
+					"else " \
+						"setenv fdt_file $fdt_file.dtb; " \
+					"fi; " \
+				"fi; " \
 			"fi;\0" \
 		"findtee="\
 			"if test $tee_file = undefined; then " \

include/configs/mx6ul_14x14_evk.h
@@ -214,15 +214,23 @@
 		"findfdt="\
 			"if test $fdt_file = undefined; then " \
 				"if test $board_name = EVK && test $board_rev = 9X9; then " \
-					"setenv fdt_file imx6ul-9x9-evk.dtb; fi; " \
+					"setenv fdt_file imx6ul-9x9-evk; fi; " \
 				"if test $board_name = EVK && test $board_rev = 14X14; then " \
-					"setenv fdt_file imx6ul-14x14-evk.dtb; fi; " \
+					"setenv fdt_file imx6ul-14x14-evk; fi; " \
 				"if test $fdt_file = undefined; then " \
-					"echo WARNING: Could not determine dtb to use; fi; " \
+					"echo WARNING: Could not determine dtb to use; " \
+				"else " \
+					"if test ${tee} = yes; then " \
+						"setenv fdt_file $fdt_file-optee.dtb; " \
+					"else " \
+						"setenv fdt_file $fdt_file.dtb; " \
+					"fi; " \
+				"fi; " \
 			"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
 	   "run findfdt;" \
+	   "run findtee;" \
 	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \

include/configs/mx6ullevk.h
@@ -200,15 +200,23 @@
 		"findfdt="\
 			"if test $fdt_file = undefined; then " \
 				"if test $board_name = EVK && test $board_rev = 9X9; then " \
-					"setenv fdt_file imx6ull-9x9-evk.dtb; fi; " \
+					"setenv fdt_file imx6ull-9x9-evk; fi; " \
 				"if test $board_name = EVK && test $board_rev = 14X14; then " \
-					"setenv fdt_file imx6ull-14x14-evk.dtb; fi; " \
+					"setenv fdt_file imx6ull-14x14-evk; fi; " \
 				"if test $fdt_file = undefined; then " \
-					"echo WARNING: Could not determine dtb to use; fi; " \
+					"echo WARNING: Could not determine dtb to use; " \
+				"else " \
+					"if test ${tee} = yes; then " \
+						"setenv fdt_file $fdt_file-optee.dtb; " \
+					"else " \
+						"setenv fdt_file $fdt_file.dtb; " \
+					"fi; " \
+				"fi; " \
 			"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
 	   "run findfdt;" \
+	   "run findtee;" \
 	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \

include/configs/mx7dsabresd.h
@@ -164,7 +164,7 @@
 	"console=ttymxc0\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
-	"fdt_file=imx7d-sdb.dtb\0" \
+	"fdt_file=undefined\0" \
 	"fdt_addr=0x83000000\0" \
 	"tee_addr=0x84000000\0" \
 	"tee_file=uTee-7dsdb\0" \
@@ -232,9 +232,18 @@
 			"else " \
 				"bootz; " \
 			"fi; " \
-		"fi;\0"
+		"fi;\0" \
+		"findfdt="\
+			"if test $fdt_file = undefined; then " \
+				"if test ${tee} = yes; then " \
+					"setenv fdt_file imx7d-sdb-optee.dtb; " \
+				"else " \
+					"setenv fdt_file imx7d-sdb.dtb; " \
+				"fi; " \
+			"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
+	   "run findfdt;" \
 	   "mmc dev ${mmcdev};" \
 	   "mmc dev ${mmcdev}; if mmc rescan; then " \
 		   "if run loadbootscript; then " \

commit 558ec9aed32d78630b0c231d986f3db0dd1d96d8

MLK-17608: u-boot: fix findfdt environment variable
Fixes: 7101be554d2 (MLK-17608: u-boot: config: update dtb filename when OP-TEE is used)

fix regression with mx6sl, mx6sll, mx6sxsabreauto, mx6sxsabresd
fix missing "fi;\0" strings in the u-boot environment variable that lead
to findfdt command not being taken into account

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Acked-by: Peng Fan <peng.fan@nxp.com>

include/configs/mx6slevk.h
@@ -156,14 +156,15 @@
 			"else " \
 				"bootz; " \
 			"fi;" \
-		"findfdt="\
-			"if test $fdt_file = undefined; then " \
-				"if test ${tee} = yes; then " \
-					"setenv fdt_file imx6sl-evk-optee.dtb; " \
-				"else " \
-					"setenv fdt_file imx6sl-evk.dtb; " \
-				"fi; " \
-			"fi;\0" \
+		"fi;\0" \
+	"findfdt="\
+		"if test $fdt_file = undefined; then " \
+			"if test ${tee} = yes; then " \
+				"setenv fdt_file imx6sl-evk-optee.dtb; " \
+			"else " \
+				"setenv fdt_file imx6sl-evk.dtb; " \
+			"fi; " \
+		"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
 	   "run findfdt;" \

include/configs/mx6sllevk.h
@@ -121,14 +121,15 @@
 			"else " \
 				"bootz; " \
 			"fi;" \
-		"findfdt="\
-			"if test $fdt_file = undefined; then " \
-				"if test ${tee} = yes; then " \
-					"setenv fdt_file imx6sll-evk-optee.dtb; " \
-				"else " \
-					"setenv fdt_file imx6sll-evk.dtb; " \
-				"fi; " \
-			"fi;\0" \
+		"fi;\0" \
+	"findfdt="\
+		"if test $fdt_file = undefined; then " \
+			"if test ${tee} = yes; then " \
+				"setenv fdt_file imx6sll-evk-optee.dtb; " \
+			"else " \
+				"setenv fdt_file imx6sll-evk.dtb; " \
+			"fi; " \
+		"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
 	   "run findfdt;" \

include/configs/mx6sxsabreauto.h
@@ -175,14 +175,15 @@
 			"else " \
 				"bootz; " \
 			"fi;" \
-		"findfdt="\
-			"if test $fdt_file = undefined; then " \
-				"if test ${tee} = yes; then " \
-					"setenv fdt_file imx6sx-sabreauto-optee.dtb; " \
-				"else " \
-					"setenv fdt_file imx6sx-sabreauto.dtb; " \
-				"fi; " \
-			"fi;\0" \
+		"fi;\0" \
+	"findfdt="\
+		"if test $fdt_file = undefined; then " \
+			"if test ${tee} = yes; then " \
+				"setenv fdt_file imx6sx-sdb-optee.dtb; " \
+			"else " \
+				"setenv fdt_file imx6sx-sdb.dtb; " \
+			"fi; " \
+		"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
 	   "run findfdt;" \

include/configs/mx6sxsabresd.h
@@ -156,14 +156,15 @@
 			"else " \
 				"bootz; " \
 			"fi;" \
-		"findfdt="\
-			"if test $fdt_file = undefined; then " \
-				"if test ${tee} = yes; then " \
-					"setenv fdt_file imx6sx-sdb-optee.dtb; " \
-				"else " \
-					"setenv fdt_file imx6sx-sdb.dtb; " \
-				"fi; " \
-			"fi;\0" \
+		"fi;\0" \
+	"findfdt="\
+		"if test $fdt_file = undefined; then " \
+			"if test ${tee} = yes; then " \
+				"setenv fdt_file imx6sx-sdb-optee.dtb; " \
+			"else " \
+				"setenv fdt_file imx6sx-sdb.dtb; " \
+			"fi; " \
+		"fi;\0" \
 
 #define CONFIG_BOOTCOMMAND \
 	   "run findfdt;" \