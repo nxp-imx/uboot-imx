07ef0fab23204684d82f27baf721a72b247f30c5
Haibo Chen <haibo.chen@nxp.com>
2018-03-14 17:15:23 +0800

    MLK-17586-3 i.MX7ULP: change USDHC clock rate
    Change USDHC0 and USDHC1 per clock source from APLL_PFD1,
    and set the APll_PFD1 clock rate to 352.8MHz.
    
    Also gate off APll_PFD1/2/3 before boot OS, otherwise set
    the clock rate of APll_PFD1/2/3 during OS boot up will triger
    some warning message.
    
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>

--- arch/arm/cpu/armv7/mx7ulp/soc.c
+++ arch/arm/cpu/armv7/mx7ulp/soc.c
@@ -288,6 +288,9 @@ void arch_preboot_os(void)
 #if defined(CONFIG_VIDEO_MXS)
 	lcdif_power_down();
 #endif
+	scg_disable_pll_pfd(SCG_APLL_PFD1_CLK);
+	scg_disable_pll_pfd(SCG_APLL_PFD2_CLK);
+	scg_disable_pll_pfd(SCG_APLL_PFD3_CLK);
 }
 
 #ifdef CONFIG_ENV_IS_IN_MMC
