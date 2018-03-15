2f0dc4c503d8ac831fb809ec124e79712defae77
Ye Li <ye.li@nxp.com>
2018-03-15 00:00:49 -0700

    MLK-17821-2 imx8: clock: Add cdns3 clock enable and disable
    Implemented the clock enable and disable interfaces for CDNS3 USB
    driver.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Acked-by: Peter Chen <peter.chen@nxp.com>

--- arch/arm/cpu/armv8/imx8/clock.c
+++ arch/arm/cpu/armv8/imx8/clock.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 NXP
+ * Copyright 2017-2018 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -241,6 +241,36 @@ void init_clk_usb3(int index)
 	return;
 }
 
+int cdns3_enable_clks(int index)
+{
+	init_clk_usb3(index);
+	return 0;
+}
+
+int cdns3_disable_clks(int index)
+{
+	sc_err_t err;
+	sc_ipc_t ipc;
+
+	ipc = gd->arch.ipc_channel_handle;
+
+	err = sc_pm_clock_enable(ipc, SC_R_USB_2, SC_PM_CLK_MISC, false, false);
+	if (err != SC_ERR_NONE)
+		printf("USB3 disable clock failed!, line=%d (error = %d)\n",
+			__LINE__, err);
+
+	err = sc_pm_clock_enable(ipc, SC_R_USB_2, SC_PM_CLK_MST_BUS, false, false);
+	if (err != SC_ERR_NONE)
+		printf("USB3 disable clock failed!, line=%d (error = %d)\n",
+			__LINE__, err);
+
+	err = sc_pm_clock_enable(ipc, SC_R_USB_2, SC_PM_CLK_PER, false, false);
+	if (err != SC_ERR_NONE)
+		printf("USB3 disable clock failed!, line=%d (error = %d)\n",
+			__LINE__, err);
+
+	return 0;
+}
 
 void init_clk_usdhc(u32 index)
 {
