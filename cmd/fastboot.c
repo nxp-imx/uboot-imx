ee909cf54f3db7be1318b0253c96bd103f614b6c
Luo Ji <ji.luo@nxp.com>
2018-03-21 15:46:26 +0800

    MA-11601 [Android] Use default usb controller in fastboot for imx8qm/imx8qxp
    Set default usb controller index to 1 so "fastboot <any-string>" will
    be equal to "fastboot 1", this is to sync the way we get used to to
    enter the fastboot mode.
    
    Change-Id: If8e402eed8dae98def57f032c44af55c6382ce56
    Signed-off-by: Luo Ji <ji.luo@nxp.com>

--- cmd/fastboot.c
+++ cmd/fastboot.c
@@ -21,9 +21,12 @@ static int do_fastboot(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 
 	if (argc < 2)
 		return CMD_RET_USAGE;
-
+#ifdef CONFIG_FASTBOOT_USB_DEV
+	controller_index = CONFIG_FASTBOOT_USB_DEV;
+#else
 	usb_controller = argv[1];
 	controller_index = simple_strtoul(usb_controller, NULL, 0);
+#endif
 
 	ret = board_usb_init(controller_index, USB_INIT_DEVICE);
 	if (ret) {
