183c12aeec8faae933f3222c40d333f1fc6ef89a
Ye Li <ye.li@nxp.com>
2018-03-23 02:46:41 -0700

    MLK-18146-4 mx6: Update mx6 common head file
    Align the common head file with v2017.03, since some configurations
    have been moved to Kconfig, so don't need them in mx6_common.h now.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- include/configs/mx6_common.h
+++ include/configs/mx6_common.h
@@ -1,5 +1,7 @@
 /*
+ * Copyright (C) 2012-2016 Freescale Semiconductor, Inc.
  * Copyright (c) 2013, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright 2018 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0
  */
@@ -43,7 +45,7 @@
 #if defined(CONFIG_MX6SL) || defined(CONFIG_MX6SLL) || \
 	defined(CONFIG_MX6SX) || \
 	defined(CONFIG_MX6UL) || defined(CONFIG_MX6ULL)
-#define CONFIG_LOADADDR		0x82000000
+#define CONFIG_LOADADDR		0x80800000
 #else
 #define CONFIG_LOADADDR		0x12000000
 #endif
@@ -52,24 +54,31 @@
 /* allow to overwrite serial and ethaddr */
 #define CONFIG_ENV_OVERWRITE
 #define CONFIG_CONS_INDEX       1
+#define CONFIG_BAUDRATE         115200
 
 /* Filesystems and image support */
+#define CONFIG_SUPPORT_RAW_INITRD
 
 /* Miscellaneous configurable options */
 #define CONFIG_SYS_CBSIZE	512
 #define CONFIG_SYS_MAXARGS	32
+#define CONFIG_SYS_BARGSIZE	CONFIG_SYS_CBSIZE
+
+#ifndef CONFIG_SYS_DCACHE_OFF
+#endif
 
 /* MMC */
 #define CONFIG_BOUNCE_BUFFER
 #define CONFIG_FSL_ESDHC
 #define CONFIG_FSL_USDHC
+#define CONFIG_SUPPORT_EMMC_BOOT
 
 /* Fuses */
 #define CONFIG_MXC_OCOTP
 
 /* Secure boot (HAB) support */
 #ifdef CONFIG_SECURE_BOOT
-#define CONFIG_CSF_SIZE			0x2000
+#define CONFIG_CSF_SIZE			0x4000
 #ifdef CONFIG_SPL_BUILD
 #define CONFIG_SPL_DRIVERS_MISC_SUPPORT
 #endif
