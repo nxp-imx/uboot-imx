72d5d4373f53a6facca47c65467714230e382acf
Sandor Yu <Sandor.yu@nxp.com>
2018-04-09 11:29:47 +0800

    MLK-17997: video: imx: hdp: Add HDMI RX firmware loading
    Add iMX8QM HDMI RX firmware loading.
    
    Signed-off-by: Sandor Yu <Sandor.yu@nxp.com>
    (cherry picked from commit 969d93660e1b34765627ff9f6f8dcab08dcd1250)

--- drivers/video/imx/hdp/test_base_sw.c
+++ drivers/video/imx/hdp/test_base_sw.c
@@ -64,6 +64,11 @@
 #define HDMI_SEC_BASE 0x56269000
 #define HDMI_OFFSET_ADDR 0x56261008
 #define HDMI_SEC_OFFSET_ADDR 0x5626100c
+
+#define HDMI_RX_BASE 0x58268000
+#define HDMI_RX_SEC_BASE 0x58269000
+#define HDMI_RX_OFFSET_ADDR 0x58261004
+#define HDMI_RX_SEC_OFFSET_ADDR 0x58261008
 #endif
 
 #endif
@@ -163,6 +168,52 @@ int cdn_sapb_write(unsigned int addr, unsigned int value)
 	uint64_t tmp_addr = (addr & 0xfff) + HDMI_SEC_BASE;
 
 	__raw_writel(addr >> 12, HDMI_SEC_OFFSET_ADDR);
+	__raw_writel(value, tmp_addr);
+
+	return 0;
+}
+
+int hdp_rx_apb_read(unsigned int addr, unsigned int *value)
+{
+	unsigned int temp;
+	uint64_t tmp_addr = (addr & 0xfff) + HDMI_RX_BASE;
+
+	__raw_writel(addr >> 12, HDMI_RX_OFFSET_ADDR);
+
+	temp = __raw_readl(tmp_addr);
+
+	*value = temp;
+	return 0;
+}
+
+int hdp_rx_apb_write(unsigned int addr, unsigned int value)
+{
+	uint64_t tmp_addr = (addr & 0xfff) + HDMI_RX_BASE;
+
+	__raw_writel(addr >> 12, HDMI_RX_OFFSET_ADDR);
+
+	__raw_writel(value, tmp_addr);
+
+	return 0;
+}
+
+int hdp_rx_sapb_read(unsigned int addr, unsigned int *value)
+{
+	unsigned int temp;
+	uint64_t tmp_addr = (addr & 0xfff) + HDMI_RX_SEC_BASE;
+
+	__raw_writel(addr >> 12, HDMI_RX_SEC_OFFSET_ADDR);
+
+	temp = __raw_readl(tmp_addr);
+	*value = temp;
+	return 0;
+}
+
+int hdp_rx_sapb_write(unsigned int addr, unsigned int value)
+{
+	uint64_t tmp_addr = (addr & 0xfff) + HDMI_RX_SEC_BASE;
+
+	__raw_writel(addr >> 12, HDMI_RX_SEC_OFFSET_ADDR);
 	__raw_writel(value, tmp_addr);
 
 	return 0;
