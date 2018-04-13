856d92327df8e1fc32ec7eb9d9b3bf866569c68e
Peng Fan <peng.fan@nxp.com>
2018-04-13 15:52:53 +0800

    MLK-18028 imx8qm: mek: fix error script
    After fi, there should be ";", otherwise there will by syntax error
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reported-by: Guoniu.Zhou <guoniu.zhou@nxp.com>

--- include/configs/imx8qm_mek.h
+++ include/configs/imx8qm_mek.h
@@ -99,7 +99,7 @@
                 "${get_cmd} ${loadaddr} xen;" \
                 "${get_cmd} ${fdt_addr} fsl-imx8qm-mek-dom0.dtb;" \
                 "${get_cmd} ${initrd_addr} ${image};" \
-		"if ${get_cmd} ${hdp_addr} ${hdp_file}; then; hdp load ${hdp_addr}; fi" \
+		"if ${get_cmd} ${hdp_addr} ${hdp_file}; then; hdp load ${hdp_addr}; fi;" \
                 "fdt addr ${fdt_addr};" \
                 "fdt resize 256;" \
                 "fdt set /chosen/module@0 reg <0x00000000 ${initrd_addr} 0x00000000 0x${filesize}>; " \
@@ -200,7 +200,7 @@
 		"else " \
 			"setenv get_cmd tftp; " \
 		"fi; " \
-		"if ${get_cmd} ${hdp_addr} ${hdp_file}; then; hdp load ${hdp_addr}; fi" \
+		"if ${get_cmd} ${hdp_addr} ${hdp_file}; then; hdp load ${hdp_addr}; fi;" \
 		"${get_cmd} ${loadaddr} ${image}; " \
 		"if test ${boot_fdt} = yes || test ${boot_fdt} = try; then " \
 			"if ${get_cmd} ${fdt_addr} ${fdt_file}; then " \
