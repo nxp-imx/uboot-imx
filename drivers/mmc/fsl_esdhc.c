642d77fb6d6412095faa6584eeef7bb0132cae57
guoyin.chen <guoyin.chen@nxp.com>
2018-04-22 18:41:20 +0800

    MA-11789 Flash system.img failed due to erasing timeout
    Flash system partition with fastboot will earse the partition firstly
    The 1.2s timeout will fail on some SD Card.
    Enlarge it to 5s to make it works for most of sdcard
    
    Change-Id: I285df411c7a07025251fd19f4c8e8b549bee2421
    Signed-off-by: guoyin.chen <guoyin.chen@nxp.com>

--- drivers/mmc/fsl_esdhc.c
+++ drivers/mmc/fsl_esdhc.c
@@ -517,9 +517,9 @@ esdhc_send_cmd(struct mmc *mmc, struct mmc_cmd *cmd, struct mmc_data *data)
 
 	/* Workaround for ESDHC errata ENGcm03648 */
 	if (!data && (cmd->resp_type & MMC_RSP_BUSY)) {
-		int timeout = 12000;
+		int timeout = 50000;
 
-		/* Poll on DATA0 line for cmd with busy signal for 1200 ms */
+		/* Poll on DATA0 line for cmd with busy signal for 5000 ms */
 		while (timeout > 0 && !(esdhc_read32(&regs->prsstat) &
 					PRSSTAT_DAT0)) {
 			udelay(100);
