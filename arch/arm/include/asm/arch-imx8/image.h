0567aabc4045e78b63cfb2b2abf11b0413dfd716
Ye Li <ye.li@nxp.com>
2018-04-25 02:33:58 -0700

    MLK-18129-1 imx8: ahab: Add secure boot support
    Add function and new command "auth_cntr" for secure boot support.
    When booting with life cycle set to OEM closed, we need to use
    this function to authenticate the OS container and load kernel & FDT
    from OS container to their destination.
    
    Users can set CONFIG_AHAB_BOOT=y to enable the feature. It is not
    set at default.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 3b0496c267a4c09ff5b790ccf8e4f658af460c6c)
    (cherry picked from commit 1c83e14fb8e8ceec99f2c42ee1442596d8ffe517)

---
+++ arch/arm/include/asm/arch-imx8/image.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2018 NXP
+ */
+
+#define IV_MAX_LEN			32
+#define HASH_MAX_LEN			64
+
+#define CONTAINER_HDR_ALIGNMENT 0x400
+#define CONTAINER_HDR_EMMC_OFFSET 0
+#define CONTAINER_HDR_MMCSD_OFFSET SZ_32K
+#define CONTAINER_HDR_QSPI_OFFSET SZ_4K
+#define CONTAINER_HDR_NAND_OFFSET SZ_128M
+
+ struct container_hdr{
+	 uint8_t version;
+	 uint8_t length_lsb;
+	 uint8_t length_msb;
+	 uint8_t tag;
+	 uint32_t flags;
+	 uint16_t sw_version;
+	 uint8_t fuse_version;
+	 uint8_t num_images;
+	 uint16_t sig_blk_offset;
+	 uint16_t reserved;
+ }__attribute__((packed));
+
+ struct boot_img_t{
+	 uint32_t offset;
+	 uint32_t size;
+	 uint64_t dst;
+	 uint64_t entry;
+	 uint32_t hab_flags;
+	 uint32_t meta;
+	 uint8_t hash[HASH_MAX_LEN];
+	 uint8_t iv[IV_MAX_LEN];
+ }__attribute__((packed));
+
+ struct signature_block_hdr{
+	 uint8_t version;
+	 uint8_t length_lsb;
+	 uint8_t length_msb;
+	 uint8_t tag;
+	 uint16_t srk_table_offset;
+	 uint16_t cert_offset;
+	 uint16_t blob_offset;
+	 uint16_t signature_offset;
+	 uint32_t reserved;
+ }__attribute__((packed));
