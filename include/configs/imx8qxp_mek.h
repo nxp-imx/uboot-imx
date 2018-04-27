4490024e142697e28d2576a3475c2dc9e647c902
Leonard Crestez <leonard.crestez@nxp.com>
2018-04-27 14:49:01 +0300

    MLK-18172-2: imx8: Introduce xenlinux_addr for dom0 kernel
    Avoid overloading variable meaning and overlap with hdp_addr.
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>
    (cherry picked from commit 9e4b3e80b4b2f40bb35a2c8f1b5ede9868788a34)
    (cherry picked from commit 7dea0df52f0100f2b363b79d793f271fac5b6fab)
    (cherry picked from commit dfb3c57f9313ed0bee355dd7217135d05c824be6)

--- include/configs/imx8qxp_mek.h
+++ include/configs/imx8qxp_mek.h
@@ -69,13 +69,14 @@
             "xenhyper_bootargs=console=dtuart dtuart=/serial@5a060000 dom0_mem=1024M dom0_max_vcpus=2 dom0_vcpus_pin=true\0" \
             "xenlinux_bootargs= \0" \
             "xenlinux_console=hvc0 earlycon=xen\0" \
+            "xenlinux_addr=0x9e000000\0" \
             "xenboot_common=" \
                 "${get_cmd} ${loadaddr} xen;" \
                 "${get_cmd} ${fdt_addr} fsl-imx8qxp-mek-dom0.dtb;" \
-                "${get_cmd} ${initrd_addr} ${image};" \
+                "${get_cmd} ${xenlinux_addr} ${image};" \
                 "fdt addr ${fdt_addr};" \
                 "fdt resize 256;" \
-                "fdt set /chosen/module@0 reg <0x00000000 ${initrd_addr} 0x00000000 0x${filesize}>; " \
+                "fdt set /chosen/module@0 reg <0x00000000 ${xenlinux_addr} 0x00000000 0x${filesize}>; " \
                 "fdt set /chosen/module@0 bootargs \"${bootargs} ${xenlinux_bootargs}\"; " \
                 "setenv bootargs ${xenhyper_bootargs};" \
                 "booti ${loadaddr} - ${fdt_addr};" \
