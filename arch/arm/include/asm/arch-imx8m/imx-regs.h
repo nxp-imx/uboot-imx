f5c28e63f19ef99e0fe4d01b176789aacc507d79
Aymen Sghaier <aymen.sghaier@nxp.com>
2018-05-01 11:37:22 +0200

    MLK-18044-1: crypto: caam: Add CAAM support to i.MX8M platforms
      This patch enable CAAM support for i.MX8M platforms.
    
    Signed-off-by: Aymen Sghaier <aymen.sghaier@nxp.com>
    (cherry picked from commit 1fc92e6e34b06bdee81240ce06326aca1d9c02d8)
    (cherry picked from commit b0f889b77b367b69aa0778b1d03a2ec30fdee243)

--- arch/arm/include/asm/arch-imx8m/imx-regs.h
+++ arch/arm/include/asm/arch-imx8m/imx-regs.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0+ */
 /*
- * Copyright 2017 NXP
+ * Copyright 2017-2018 NXP
  */
 
 #ifndef __ASM_ARCH_IMX8M_REGS_H__
@@ -128,6 +128,16 @@
 #define DDRC_IPS_BASE_ADDR(X)	(0x3d400000 + ((X) * 0x2000000))
 #define DDR_CSD1_BASE_ADDR	0x40000000
 
+#define CAAM_ARB_BASE_ADDR              (0x00100000)
+#define CAAM_ARB_END_ADDR               (0x00107FFF)
+#define CAAM_IPS_BASE_ADDR              (0x30900000)
+#define CONFIG_SYS_FSL_SEC_OFFSET       (0)
+#define CONFIG_SYS_FSL_SEC_ADDR         (CAAM_IPS_BASE_ADDR + \
+					 CONFIG_SYS_FSL_SEC_OFFSET)
+#define CONFIG_SYS_FSL_JR0_OFFSET       (0x1000)
+#define CONFIG_SYS_FSL_JR0_ADDR         (CONFIG_SYS_FSL_SEC_ADDR + \
+					 CONFIG_SYS_FSL_JR0_OFFSET)
+#define CONFIG_SYS_FSL_MAX_NUM_OF_SEC   1
 #if !defined(__ASSEMBLY__)
 #include <asm/types.h>
 #include <linux/bitops.h>
