78a1f0387685042d63b33f45ba6d35d2841d6f2b
Franck LENORMAND <franck.lenormand@nxp.com>
2018-05-02 12:01:56 +0200

    MLK-18043 4/4: imx7ulp: Enable support for cmd blob
    Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>
    (cherry picked from commit 03cb71cd22ff6ab1e2005a4bcb183c65f49b6e4e)
    (cherry picked from commit 5d797db35da7fa3f7971df8811b6a1e800786ced)
    (cherry picked from commit dd2e4b027122aa6724985f23319bebd89dfc1f39)

--- cmd/blob.c
+++ cmd/blob.c
@@ -9,7 +9,8 @@
 #include <malloc.h>
 #include <asm/byteorder.h>
 #include <linux/compiler.h>
-#if defined(CONFIG_ARCH_MX6) || defined(CONFIG_ARCH_MX7)
+#if defined(CONFIG_ARCH_MX6) || defined(CONFIG_ARCH_MX7) || \
+	defined(CONFIG_ARCH_MX7ULP)
 #include <fsl_sec.h>
 #include <asm/arch/clock.h>
 #endif
@@ -77,7 +78,8 @@ static int do_blob(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 	src_ptr = (uint8_t *)(uintptr_t)src_addr;
 	dst_ptr = (uint8_t *)(uintptr_t)dst_addr;
 
-#if defined(CONFIG_ARCH_MX6) || defined(CONFIG_ARCH_MX7)
+#if defined(CONFIG_ARCH_MX6) || defined(CONFIG_ARCH_MX7) || \
+	defined(CONFIG_ARCH_MX7ULP)
 
 	hab_caam_clock_enable(1);
 
