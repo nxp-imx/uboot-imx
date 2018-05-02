5f253097e98e1770a738027b72296907cdf3c825
Franck LENORMAND <franck.lenormand@nxp.com>
2018-05-02 12:16:36 +0200

    MLK-18043 2/4: caam: enable support for iMX7ULP
    Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>
    (cherry picked from commit 5f429339ab5a5f0d1f00a2880ce05cf479f11ba5)
    (cherry picked from commit c26db25a130ef1f0abd43a3133877f74431eb217)
    (cherry picked from commit 62ca8b07d4906ea11380f2638cc4b9d55beae9f6)

--- drivers/crypto/fsl/jobdesc.c
+++ drivers/crypto/fsl/jobdesc.c
@@ -14,7 +14,7 @@
 #include "jobdesc.h"
 #include "rsa_caam.h"
 
-#if defined(CONFIG_MX6) || defined(CONFIG_MX7)
+#if defined(CONFIG_MX6) || defined(CONFIG_MX7) || defined(CONFIG_MX7ULP)
 /*!
  * Secure memory run command
  *
