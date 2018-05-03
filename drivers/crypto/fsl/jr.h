485c020389b8e5ad88e50e0aaea88458756fbc98
Aymen Sghaier <aymen.sghaier@nxp.com>
2018-05-03 12:17:53 +0200

    MLK-18044-4: crypto: fsl: refactor for 32 bit version CAAM support on ARM64
    Since i.MX CAAM are all 32 bits no matter the ARM arch (32 or 64),
    to adapt and not break 64 bits CAAM support,  add a new config
    CONFIG_CAAM_64BIT and new relevant type "caam_dma_addr_t".
    
    This config is default enabled when CONFIG_PHYS_64BIT is set except
    for iMX8M.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Horia Geantă <horia.geanta@nxp.com>
    (cherry picked from commit 043c4ff7524dcf9f8fea1b56eddb8d1a40505d6c)

--- drivers/crypto/fsl/jr.h
+++ drivers/crypto/fsl/jr.h
@@ -8,6 +8,7 @@
 #define __JR_H
 
 #include <linux/compiler.h>
+#include "type.h"
 
 #define JR_SIZE 4
 /* Timeout currently defined as 10 sec */
@@ -41,13 +42,13 @@
 #define RNG4_MAX_HANDLES	2
 
 struct op_ring {
-	phys_addr_t desc;
+	caam_dma_addr_t desc;
 	uint32_t status;
 } __packed;
 
 struct jr_info {
 	void (*callback)(uint32_t status, void *arg);
-	phys_addr_t desc_phys_addr;
+	caam_dma_addr_t desc_phys_addr;
 	uint32_t desc_len;
 	uint32_t op_done;
 	void *arg;
@@ -83,7 +84,7 @@ struct jobring {
 	 * by SEC
 	 */
 	/*Circular  Ring of i/p descriptors */
-	dma_addr_t *input_ring;
+	caam_dma_addr_t *input_ring;
 	/* Circular Ring of o/p descriptors */
 	/* Circula Ring containing info regarding descriptors in i/p
 	 * and o/p ring
