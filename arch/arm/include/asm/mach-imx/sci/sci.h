1aeb9d4e2971d444b707e13d622722988d4fe8b8
Teo Hall <teo.hall@nxp.com>
2018-05-03 15:05:31 -0500

    MLK-18197: Use MU1 for SCFW API calls
    In i.MX8QXP & i.MX8QM MU0 is used as the boot container MU.
    This is taken by ATF so change the MU that is used for SCFW API
    to MU1.
    
    Signed-off-by: Teo Hall <teo.hall@nxp.com>
    (cherry picked from commit d5aa444f3e83550e934d09982c76ea74e53fbedf)

--- arch/arm/include/asm/mach-imx/sci/sci.h
+++ arch/arm/include/asm/mach-imx/sci/sci.h
@@ -38,7 +38,7 @@
 #define SC_IPC_AP_CH3       	(MU_BASE_ADDR(3))
 #define SC_IPC_AP_CH4       	(MU_BASE_ADDR(4))
 
-#define SC_IPC_CH		SC_IPC_AP_CH0
+#define SC_IPC_CH		SC_IPC_AP_CH1
 
 /* Types */
 
