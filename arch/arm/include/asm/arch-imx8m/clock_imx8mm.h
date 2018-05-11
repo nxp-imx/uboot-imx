05c72a41bb4b58e9f8471358b0b7cd4d31486285
Ye Li <ye.li@nxp.com>
2018-05-11 01:30:48 -0700

    MLK-18290-3 imx8mm: Add SOC level support for OTG USB
    Enable the OTG power on, add clock fuction and USB base address.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit d4f12cd92b18283daca35b35339a96c557f5127c)

--- arch/arm/include/asm/arch-imx8m/clock_imx8mm.h
+++ arch/arm/include/asm/arch-imx8m/clock_imx8mm.h
@@ -876,4 +876,5 @@ void enable_ocotp_clk(unsigned char enable);
 int enable_i2c_clk(unsigned char enable, unsigned int i2c_num);
 int set_clk_enet(enum enet_freq type);
 void hab_caam_clock_enable(unsigned char enable);
+void enable_usboh3_clk(unsigned char enable);
 #endif
