9b25467fa70f5506fe30b7d93ded5ece74cb4222
Frank Li <Frank.Li@nxp.com>
2018-05-25 16:56:25 -0500

    MLK-18406 imx8: fastboot support all partition
    uuu can write to any position of mmc
    sdps: boot -f ../mkimage_imx8dv/imx-mkimage/iMX8QX/flash.bin
    
    FB: ucmd setenv fastboot_dev mmc
    FB: ucmd setenv mmcdev ${emmc_dev}
    FB: ucmd mmc dev ${emmc_dev}
    FB: flash -raw2sparse all xx.sdcard
    
    Signed-off-by: Frank Li <Frank.Li@nxp.com>
    (cherry picked from commit ca96e0bd1aea1996904b0a71fb1d74c3f5176929)
    (cherry picked from commit 482d31e412f5b373d143dbdf363667b066762711)

--- arch/arm/mach-imx/imx8/cpu.c
+++ arch/arm/mach-imx/imx8/cpu.c
@@ -473,7 +473,7 @@ int mmc_get_env_dev(void)
 		break;
 	default:
 		/* If not boot from sd/mmc, use default value */
-		return CONFIG_SYS_MMC_ENV_DEV;
+		return env_get_ulong("mmcdev", 10, CONFIG_SYS_MMC_ENV_DEV);
 	}
 
 	return board_mmc_get_env_dev(devno);
