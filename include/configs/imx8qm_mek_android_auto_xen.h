4f9ed8ef50bd1ab4841787b6448dd796af252735
Peng Fan <peng.fan@nxp.com>
2018-06-12 16:18:00 +0800

    MLK-18577-7 imx8qm: mek: support uboot in vm
    We are targeting let uboot loading images from emmc to boot android.
    Currently only emmc1 is enabled, so other devices are disabled.
    
    Also change memory layout according to VM layout. The memory layout
    needs to be samesize as what you configured in as "memory = "3584"".
    
    Add a new dts dedicated for this kind uboot, add a new defconfig
    to select CONFIG_XEN/CONFIG_XEN_DEBUG_SERIAL, deselect CONFIG_DM_SERIAL,
    select CONFIG_ENABLE_ARM_SOC_BOOT0_HOOK, and disable some devices
    compared with imx8qm_mek_androidauto_defconfig.
    
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit a4e932cae433b9d2e1cff6a94b12e4abcac45543)
    (cherry picked from commit a26de6e05563d3e3e6734e0d99e464437aa89db5)
    (cherry picked from commit 74ac579ee195c4a2f6651ccc0f86efb0eebd889e)
    (cherry picked from commit 7258e558b51a7f56d2b895c9c554423e12037ed3)

---
+++ include/configs/imx8qm_mek_android_auto_xen.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef IMX8QM_MEK_ANDROID_AUTO_XEN_H
+#define IMX8QM_MEK_ANDROID_AUTO_XEN_H
+
+#undef CONFIG_SYS_SDRAM_BASE
+#undef CONFIG_NR_DRAM_BANKS
+#undef PHYS_SDRAM_1
+#undef PHYS_SDRAM_2
+#undef PHYS_SDRAM_1_SIZE
+#undef PHYS_SDRAM_2_SIZE
+
+#define CONFIG_SYS_SDRAM_BASE		0x40000000
+#define CONFIG_NR_DRAM_BANKS		2
+#define PHYS_SDRAM_1			0x40000000
+#define PHYS_SDRAM_2			0x200000000
+#define PHYS_SDRAM_1_SIZE		0xC0000000	/* 3 GB */
+#define PHYS_SDRAM_2_SIZE		0x20000000	/* 512 MB */
+
+#undef CONFIG_LOADADDR
+#define CONFIG_LOADADDR			0x40280000
+#undef CONFIG_SYS_INIT_SP_ADDR
+#define CONFIG_SYS_INIT_SP_ADDR		0x40200000
+
+#undef CONFIG_REQUIRE_SERIAL_CONSOLE
+#undef CONFIG_IMX_SMMU
+
+/* This needs to be stay same in iomem in domu.cfg */
+#define SC_IPC_CH			0x15d1d0000
+
+#endif /* IMX8QM_MEK_ANDROID_AUTO_XEN_H */
