2818db49471dfd8739504b9e5723fcd96ca72800
Peng Fan <peng.fan@nxp.com>
2018-06-12 16:18:00 +0800

    MLK-18577-7 imx8qm: mek: support uboot in vm
    We are targeting let uboot loading images from emmc to boot android.
    Currently only emmc1 is enabled, so other devices are disabled.
    
    Also change memory layout according to VM layout. The memory layout
    needs to be samesize as what you configured in as "memory = "3584"".
    
    Add a new dts dedicated for this kind uboot, add a new defconfig
    to select CONFIG_XEN/CONFIG_XEN_DEBUG_SERIAL, deselect CONFIG_DM_SERIAL,
    select CONFIG_ENABLE_ARM_SOC_BOOT0_HOOK, and disable some devices
    compared with imx8qm_mek_androidauto_defconfig.
    
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit a4e932cae433b9d2e1cff6a94b12e4abcac45543)
    (cherry picked from commit a26de6e05563d3e3e6734e0d99e464437aa89db5)
    (cherry picked from commit 74ac579ee195c4a2f6651ccc0f86efb0eebd889e)
    (cherry picked from commit 7258e558b51a7f56d2b895c9c554423e12037ed3)
    (cherry picked from commit 4f9ed8ef50bd1ab4841787b6448dd796af252735)
    (cherry picked from commit af0a15a89e916d737e92a1345fb97e4cb7a241a1)

--- board/freescale/imx8qm_mek/imx8qm_mek.c
+++ board/freescale/imx8qm_mek/imx8qm_mek.c
@@ -383,6 +383,9 @@ int board_usb_cleanup(int index, enum usb_init_type init)
 
 int board_init(void)
 {
+	if (IS_ENABLED(CONFIG_XEN))
+		return 0;
+
 	board_gpio_init();
 
 
@@ -437,6 +440,24 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 	return 0;
 }
 #endif
+
+int board_mmc_get_env_dev(int devno)
+{
+	/* Use EMMC */
+	if (IS_ENABLED(CONFIG_XEN))
+		return 0;
+
+	return devno;
+}
+
+int mmc_map_to_kernel_blk(int dev_no)
+{
+	/* Use EMMC */
+	if (IS_ENABLED(CONFIG_XEN))
+		return 0;
+
+	return dev_no;
+}
 
 int board_late_init(void)
 {
