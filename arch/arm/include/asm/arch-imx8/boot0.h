2f1f8069405bdb74d811bb594cbbea3d552fa31a
Peng Fan <peng.fan@nxp.com>
2018-06-12 16:12:44 +0800

    MLK-18577-6 imx8: boot0: add boot header
    Starting U-Boot in a XEN VM, needs a header, just like Linux Kernel.
    Without it, xen tool will take is as a file not supported.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit 344ddf76c6de808699ab742d3c11728ca62f36ee)
    (cherry picked from commit d260e97be35718bea46019b7ca8666e83d0b9f01)
    (cherry picked from commit fa6d1e299da0f6913564dcb11c171cee69a43f8e)

--- arch/arm/include/asm/arch-imx8/boot0.h
+++ arch/arm/include/asm/arch-imx8/boot0.h
@@ -3,6 +3,23 @@
  * Copyright 2019 NXP
  */
 
+#ifdef CONFIG_XEN
+	add     x13, x18, #0x16
+	b       reset
+	/* start of zImage header */
+	.quad   0x80000		// Image load offset from start of RAM
+	.quad   _end - _start	// Effective size of kernel image
+	.quad   0		// Flags
+	.quad   0		// reserved
+	.quad   0		// reserved
+	.quad   0		// reserved
+	.byte   0x41		// Magic number, "ARM\x64"
+	.byte   0x52
+	.byte   0x4d
+	.byte   0x64
+	/* end of zImage header */
+#endif
+
 #if defined(CONFIG_SPL_BUILD)
 	/*
 	 * We use absolute address not PC relative address to jump.
