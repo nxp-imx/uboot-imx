ea6784ee562708ae9a4c3b44222136227d47eb38
Luo Ji <ji.luo@nxp.com>
2018-06-13 15:33:21 +0800

    MA-11958 Fix userdata partition size overflow
    The userdata partition size for some platforms
    (like imx8/imx8m) could be larger than 4GB, use
    "unsigned long int" to store the partition size
    instead of "unsigned int".
    
    Test: Get correct userdata partition size for imx8m.
    
    Change-Id: Ib626fa2c4c64dde4eba3a93ee0455f45d1286c9f
    Signed-off-by: Luo Ji <ji.luo@nxp.com>
    (cherry picked from commit 5a0a2948b0b2222ae5e261e5f67b3f4b9a5f21f3)

--- drivers/usb/gadget/f_fastboot.c
+++ drivers/usb/gadget/f_fastboot.c
@@ -2787,7 +2787,7 @@ static int get_single_var(char *cmd, char *response)
 			strncat(response, "Wrong partition name.", chars_left);
 			return -1;
 		} else {
-			snprintf(response + strlen(response), chars_left, "0x%x", fb_part->length * get_block_size());
+			snprintf(response + strlen(response), chars_left, "0x%lx", (unsigned long int)fb_part->length * get_block_size());
 		}
 	} else if ((str = strstr(cmd, "partition-type:"))) {
 		str +=strlen("partition-type:");
