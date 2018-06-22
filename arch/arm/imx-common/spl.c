a2ad2dade253d88c784238321b564744afd09cf1
Ye Li <ye.li@nxp.com>
2018-06-21 19:26:47 -0700

    MLK-18654-2 imx: Change USB boot device type
    The SPL SDP is configured as BOOT_DEVICE_BOARD, so when booting from
    USB, change its type to BOOT_DEVICE_BOARD, so we can use SDP.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 433032d7d672d4aa18d1399ffaa9449f00bc7d09)

--- arch/arm/imx-common/spl.c
+++ arch/arm/imx-common/spl.c
@@ -118,7 +118,7 @@ u32 spl_boot_device(void)
 	case NAND_BOOT:
 		return BOOT_DEVICE_NAND;
 	case USB_BOOT:
-		return BOOT_DEVICE_USB;
+		return BOOT_DEVICE_BOARD;
 	case SPI_NOR_BOOT:
 		return BOOT_DEVICE_SPI;
 	case QSPI_BOOT:
