TobleMiner/u-boot-cmm2

usb: gadget: sdp: add missing line breaks
Cosmetic change.

Signed-off-by: Andre Heider <a.heider@gmail.com>
Reviewed-by: Lukasz Majewski <lukma@denx.de>
Acked-by: Stefan Agner <stefan.agner@toradex.com>

cmd/usb_gadget_sdp.c
@@ -28,13 +28,13 @@ static int do_sdp(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 
 	ret = sdp_init(controller_index);
 	if (ret) {
-		pr_err("SDP init failed: %d", ret);
+		pr_err("SDP init failed: %d\n", ret);
 		goto exit;
 	}
 
 	/* This command typically does not return but jumps to an image */
 	sdp_handle(controller_index);
-	pr_err("SDP ended");
+	pr_err("SDP ended\n");
 
 exit:
 	g_dnl_unregister();

common/spl/spl_sdp.c
@@ -24,13 +24,13 @@ static int spl_sdp_load_image(struct spl_image_info *spl_image,
 
 	ret = sdp_init(controller_index);
 	if (ret) {
-		pr_err("SDP init failed: %d", ret);
+		pr_err("SDP init failed: %d\n", ret);
 		return -ENODEV;
 	}
 
 	/* This command typically does not return but jumps to an image */
 	sdp_handle(controller_index);
-	pr_err("SDP ended");
+	pr_err("SDP ended\n");
 
 	return -EINVAL;
 }

drivers/usb/gadget/f_sdp.c
@@ -238,12 +238,12 @@ static void sdp_rx_command_complete(struct usb_ep *ep, struct usb_request *req)
 	u8 report = data[0];
 
 	if (status != 0) {
-		pr_err("Status: %d", status);
+		pr_err("Status: %d\n", status);
 		return;
 	}
 
 	if (report != 1) {
-		pr_err("Unexpected report %d", report);
+		pr_err("Unexpected report %d\n", report);
 		return;
 	}
 
@@ -323,12 +323,12 @@ static void sdp_rx_data_complete(struct usb_ep *ep, struct usb_request *req)
 	int datalen = req->length - 1;
 
 	if (status != 0) {
-		pr_err("Status: %d", status);
+		pr_err("Status: %d\n", status);
 		return;
 	}
 
 	if (report != 2) {
-		pr_err("Unexpected report %d", report);
+		pr_err("Unexpected report %d\n", report);
 		return;
 	}
 
@@ -361,7 +361,7 @@ static void sdp_rx_data_complete(struct usb_ep *ep, struct usb_request *req)
 		sdp->state = SDP_STATE_TX_SEC_CONF;
 		break;
 	default:
-		pr_err("Invalid state: %d", sdp->state);
+		pr_err("Invalid state: %d\n", sdp->state);
 	}
 }
 
@@ -371,7 +371,7 @@ static void sdp_tx_complete(struct usb_ep *ep, struct usb_request *req)
 	int status = req->status;
 
 	if (status != 0) {
-		pr_err("Status: %d", status);
+		pr_err("Status: %d\n", status);
 		return;
 	}
 
@@ -394,7 +394,7 @@ static void sdp_tx_complete(struct usb_ep *ep, struct usb_request *req)
 			sdp->state = SDP_STATE_IDLE;
 		break;
 	default:
-		pr_err("Wrong State: %d", sdp->state);
+		pr_err("Wrong State: %d\n", sdp->state);
 		sdp->state = SDP_STATE_IDLE;
 		break;
 	}

nxp-imx/uboot-imx

(cherry picked from commit 24ccd0c8fd6285636edb94d0ece2cbcf062d64eb)

cmd/usb_gadget_sdp.c
@@ -28,13 +28,13 @@ static int do_sdp(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 
 	ret = sdp_init(controller_index);
 	if (ret) {
-		error("SDP init failed: %d", ret);
+		error("SDP init failed: %d\n", ret);
 		goto exit;
 	}
 
 	/* This command typically does not return but jumps to an image */
 	sdp_handle(controller_index);
-	error("SDP ended");
+	error("SDP ended\n");
 
 exit:
 	g_dnl_unregister();

common/spl/spl_sdp.c
@@ -24,13 +24,13 @@ static int spl_sdp_load_image(struct spl_image_info *spl_image,
 
 	ret = sdp_init(controller_index);
 	if (ret) {
-		error("SDP init failed: %d", ret);
+		error("SDP init failed: %d\n", ret);
 		return -ENODEV;
 	}
 
 	/* This command typically does not return but jumps to an image */
 	sdp_handle(controller_index);
-	error("SDP ended");
+	error("SDP ended\n");
 
 	return -EINVAL;
 }

drivers/usb/gadget/f_sdp.c
@@ -238,12 +238,12 @@ static void sdp_rx_command_complete(struct usb_ep *ep, struct usb_request *req)
 	u8 report = data[0];
 
 	if (status != 0) {
-		error("Status: %d", status);
+		error("Status: %d\n", status);
 		return;
 	}
 
 	if (report != 1) {
-		error("Unexpected report %d", report);
+		error("Unexpected report %d\n", report);
 		return;
 	}
 
@@ -323,12 +323,12 @@ static void sdp_rx_data_complete(struct usb_ep *ep, struct usb_request *req)
 	int datalen = req->length - 1;
 
 	if (status != 0) {
-		error("Status: %d", status);
+		error("Status: %d\n", status);
 		return;
 	}
 
 	if (report != 2) {
-		error("Unexpected report %d", report);
+		error("Unexpected report %d\n", report);
 		return;
 	}
 
@@ -361,7 +361,7 @@ static void sdp_rx_data_complete(struct usb_ep *ep, struct usb_request *req)
 		sdp->state = SDP_STATE_TX_SEC_CONF;
 		break;
 	default:
-		error("Invalid state: %d", sdp->state);
+		error("Invalid state: %d\n", sdp->state);
 	}
 }
 
@@ -371,7 +371,7 @@ static void sdp_tx_complete(struct usb_ep *ep, struct usb_request *req)
 	int status = req->status;
 
 	if (status != 0) {
-		error("Status: %d", status);
+		error("Status: %d\n", status);
 		return;
 	}
 
@@ -394,7 +394,7 @@ static void sdp_tx_complete(struct usb_ep *ep, struct usb_request *req)
 			sdp->state = SDP_STATE_IDLE;
 		break;
 	default:
-		error("Wrong State: %d", sdp->state);
+		error("Wrong State: %d\n", sdp->state);
 		sdp->state = SDP_STATE_IDLE;
 		break;
 	}