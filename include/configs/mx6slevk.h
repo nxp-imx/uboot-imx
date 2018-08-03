7beceb156bc4e9a5b69ab2ce7ef23cdeb3da314b
Xiaoning Wang <xiaoning.wang@nxp.com>
2018-08-03 10:27:17 +0800

    MLK-19059-5 Enable uuu for i.MX6SL in 2018 uboot
    i.MX6SL boards enable fastboot as default.
    Support: mx6slevk_defconfig
             mx6slevk_epdc_defconfig
             mx6slevk_optee_defconfig
             mx6slevk_plugin_defconfig
    uuu will use fastboot command to write emmc.
    
    Signed-off-by: Xiaoning Wang <xiaoning.wang@nxp.com>

--- include/configs/mx6slevk.h
+++ include/configs/mx6slevk.h
@@ -10,6 +10,7 @@
 #define __CONFIG_H
 
 #include "mx6_common.h"
+#include "imx_env.h"
 
 #ifdef CONFIG_SPL
 #include "imx6_spl.h"
@@ -59,22 +60,16 @@
 
 #define CONFIG_PHY_SMSC
 
+#define CONFIG_CMD_READ
+#define CONFIG_SERIAL_TAG
+#define CONFIG_FASTBOOT_USB_DEV 0
+
 #define CONFIG_MFG_ENV_SETTINGS \
-	"mfgtool_args=setenv bootargs console=${console},${baudrate} " \
-		"rdinit=/linuxrc " \
-		"g_mass_storage.stall=0 g_mass_storage.removable=1 " \
-		"g_mass_storage.file=/fat g_mass_storage.ro=1 " \
-		"g_mass_storage.idVendor=0x066F g_mass_storage.idProduct=0x37FF "\
-		"g_mass_storage.iSerialNumber=\"\" "\
-		"\0" \
+	CONFIG_MFG_ENV_SETTINGS_DEFAULT \
 	"initrd_addr=0x83800000\0" \
 	"initrd_high=0xffffffff\0" \
-	"bootcmd_mfg=run mfgtool_args;" \
-		"if test ${tee} = yes; then " \
-			"bootm ${tee_addr} ${initrd_addr} ${fdt_addr}; " \
-		"else " \
-			"bootz ${loadaddr} ${initrd_addr} ${fdt_addr}; " \
-		"fi;\0"
+    "emmc_dev=2\0"\
+    "sd_dev=1\0" \
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	CONFIG_MFG_ENV_SETTINGS \
