3ad2087a127753b0580549f20a4f583284232936
Ye Li <ye.li@nxp.com>
2018-08-16 19:43:27 -0700

    MLK-19249-1 imx8mq_evk/arm2: Force DWC3 gadget to high speed
    Since the u-boot gadget class drivers don't support super speed, if
    we set DWC3 gadget driver to super speed, the device mode will meet problem
    when using USB3.0 cable. So force the DWC3 gadget driver to high speed
    for both SPL and regular u-boot.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- board/freescale/imx8mq_arm2/imx8mq_arm2.c
+++ board/freescale/imx8mq_arm2/imx8mq_arm2.c
@@ -230,7 +230,7 @@ int board_phy_config(struct phy_device *phydev)
 #define USB_PHY_CTRL2_TXENABLEN0	BIT(8)
 
 static struct dwc3_device dwc3_device_data = {
-	.maximum_speed = USB_SPEED_SUPER,
+	.maximum_speed = USB_SPEED_HIGH,
 	.base = USB1_BASE_ADDR,
 	.dr_mode = USB_DR_MODE_PERIPHERAL,
 	.index = 0,
