11b519b7805d1467cb4c35a5fa96f38da7d78497
Ye Li <ye.li@nxp.com>
2018-08-22 22:04:22 -0700

    MLK-19308 mx7d_lpddr3_arm2: Fix plugin boot failed issue
    The QOS relevant registers are not defined in register header file.
    When building plugin, these addresses are set to 0 and cause plugin
    failed.
    
    Move the QOS registers definitions from set_epdc_qos to register
    header file to fix the issue.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- arch/arm/mach-imx/mx7/soc.c
+++ arch/arm/mach-imx/mx7/soc.c
@@ -168,11 +168,6 @@ static void imx_enet_mdio_fixup(void)
 
 static void set_epdc_qos(void)
 {
-#define REGS_QOS_BASE     QOSC_IPS_BASE_ADDR
-#define REGS_QOS_EPDC     (QOSC_IPS_BASE_ADDR + 0x3400)
-#define REGS_QOS_PXP0     (QOSC_IPS_BASE_ADDR + 0x2C00)
-#define REGS_QOS_PXP1     (QOSC_IPS_BASE_ADDR + 0x3C00)
-
 	writel(0, REGS_QOS_BASE);  /*  Disable clkgate & soft_reset */
 	writel(0, REGS_QOS_BASE + 0x60);  /*  Enable all masters */
 	writel(0, REGS_QOS_EPDC);   /*  Disable clkgate & soft_reset */
