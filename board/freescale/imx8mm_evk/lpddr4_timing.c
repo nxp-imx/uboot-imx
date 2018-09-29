891955b1a714c8177e04838d1ef39b50b3a10f83
Bai Ping <ping.bai@nxp.com>
2018-09-29 13:49:43 +0800

    MLK-19777-02 imx8mm: rename lpddr4_timing variable to dram_timing.
    For LPDDR4 and DDR4, we use the same dram_timing struct
    to config parameters. rename the 'lpddr4_timing' to
    'dram_timing' for common use.
    
    Signed-off-by: Bai Ping <ping.bai@nxp.com>
    (cherry picked from commit 4e9048d810eb92c395453a28dea2a44961e08948)

--- board/freescale/imx8mm_evk/lpddr4_timing.c
+++ board/freescale/imx8mm_evk/lpddr4_timing.c
@@ -1967,7 +1967,7 @@ struct dram_fsp_msg lpddr4_dram_fsp_msg[] = {
 };
 
 /* lpddr4 timing config params on EVK board */
-struct dram_timing_info lpddr4_timing = {
+struct dram_timing_info dram_timing = {
 	.ddrc_cfg = lpddr4_ddrc_cfg,
 	.ddrc_cfg_num = ARRAY_SIZE(lpddr4_ddrc_cfg),
 	.ddrphy_cfg = lpddr4_ddrphy_cfg,
