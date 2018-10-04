464771551f9394fa501d6ea368b7baae3606a6f5
Abel Vesa <abel.vesa@nxp.com>
2018-10-04 11:07:18 +0300

    iMX8QM mek SPL: Add FIT image loading support
    Add support for FIT image loading of ATF and uboot proper for iMX8QM mek.
    
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>

--- board/freescale/imx8qm_mek/spl.c
+++ board/freescale/imx8qm_mek/spl.c
@@ -203,6 +203,16 @@ void spl_board_prepare_for_boot(void)
 }
 
 
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	/* Just empty function now - can't decide what to choose */
+	debug("%s: %s\n", __func__, name);
+
+	return 0;
+}
+#endif
+
 void board_init_f(ulong dummy)
 {
         /* Clear global data */
