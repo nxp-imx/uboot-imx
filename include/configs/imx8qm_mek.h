22dd58feeef067b5cb9bf269f3598d4ab1c67101
Peng Fan <peng.fan@nxp.com>
2018-10-11 17:13:25 +0800

    MLK-19890 configs: imx8qm_mek: create partition before loading hdmi firmware
    After loading hdmi firmware, create partition, HDMI power domain will be
    off, this will cause HDMI firmware being lost.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 0681ed943c1a00012a8f15e104048f48f2e1fcbe)
    (cherry picked from commit e2922f4f0e6c659bb444d87ca4a2d103110c2f85)

--- include/configs/imx8qm_mek.h
+++ include/configs/imx8qm_mek.h
@@ -105,6 +105,7 @@
             "xenboot_common=" \
                 "${get_cmd} ${loadaddr} xen;" \
                 "${get_cmd} ${fdt_addr} ${dom0fdt_file};" \
+		"scu_rm dtb ${fdt_addr};" \
                 "if ${get_cmd} ${hdp_addr} ${hdp_file}; then; hdp load ${hdp_addr}; fi;" \
                 "${get_cmd} ${xenlinux_addr} ${image};" \
                 "fdt addr ${fdt_addr};" \
@@ -112,7 +113,6 @@
                 "fdt set /chosen/module@0 reg <0x00000000 ${xenlinux_addr} 0x00000000 0x${filesize}>; " \
                 "fdt set /chosen/module@0 bootargs \"${bootargs} ${xenlinux_bootargs}\"; " \
                 "setenv bootargs ${xenhyper_bootargs};" \
-		"scu_rm dtb ${fdt_addr};" \
                 "booti ${loadaddr} - ${fdt_addr};" \
             "\0" \
             "xennetboot=" \
