fe897bdc895510228b3f168e7bbda979d023b3da
Peng Fan <peng.fan@nxp.com>
2018-10-31 09:14:43 +0800

    MLK-20134 imx8qm: xen: correct DRAM SIZE and loadaddr
    The first DRAM BANK size should be 2GB and the load addr
    are 0x80080000.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit 56c9ac9734a59c68cb85125b7047087d00d141e8)
    (cherry picked from commit fd9689b3a2bcd0de26594afa161c4e4acb4c7f58)
    (cherry picked from commit ac0d7c6d0adb8e92ad8905d6d1037f20d624b1f4)
    (cherry picked from commit 698487d8bc9006f4ad023a72bf5ab584e325969e)
    (cherry picked from commit f3a2e5b1a1b5eddbfbc21dc1aa6c519d7cbab038)

--- include/configs/imx8qm_mek_android_auto_xen.h
+++ include/configs/imx8qm_mek_android_auto_xen.h
@@ -16,7 +16,7 @@
 #define CFG_SYS_SDRAM_BASE		0x80000000
 #define PHYS_SDRAM_1			0x80000000
 #define PHYS_SDRAM_2			0x200000000
-#define PHYS_SDRAM_1_SIZE		0x40000000	/* 2 GB */
+#define PHYS_SDRAM_1_SIZE		0x80000000	/* 2 GB */
 #define PHYS_SDRAM_2_SIZE		0x60000000	/* 1536 MB */
 
 /* This needs to be stay same in iomem in domu.cfg */
