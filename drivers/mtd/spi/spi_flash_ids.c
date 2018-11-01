04b813d4687028ce65c9772029d5da5500ec2e1c
Ye Li <ye.li@nxp.com>
2018-10-31 20:13:13 -0700

    MLK-20154-1 mtd: spi: Add flash id for GD25LQ16
    iMX8MM DDR3L validation board uses GD25LQ16, but its id is not in
    u-boot flash ids table. Add the new id and parameters into the table.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- drivers/mtd/spi/spi_flash_ids.c
+++ drivers/mtd/spi/spi_flash_ids.c
@@ -66,6 +66,7 @@ const struct spi_flash_info spi_flash_ids[] = {
 	{"gd25q16c",	   INFO(0xc84015, 0x0, 64 * 1024,    32, SECT_4K) },
 	{"gd25q64b",	   INFO(0xc84017, 0x0, 64 * 1024,   128, SECT_4K) },
 	{"gd25lq32",	   INFO(0xc86016, 0x0, 64 * 1024,    64, SECT_4K) },
+	{"gd25lq16",	   INFO(0xc86015, 0x0, 64 * 1024,    32, SECT_4K) },
 #endif
 #ifdef CONFIG_SPI_FLASH_ISSI		/* ISSI */
 	{"is25lq040b",	   INFO(0x9d4013, 0x0, 64 * 1024,    8, 0)  },
