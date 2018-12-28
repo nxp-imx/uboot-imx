708d7eac574850985f61e2dbf8d0da2caaa65dce
Ye Li <ye.li@nxp.com>
2018-12-27 18:04:13 -0800

    MLK-20664-3 imx8m: Enable redundant offset for SPL NAND FIT
    The new uuu will change to burn entire boot image to nandfit mtdpart not
    only the FIT image. We enable REDUND offset configuration here, so that SPL will
    try to find FIT header at two locations: the begin of nandfit mtdpart and
    the 0x57c00 offset of nandfit mtdpart. This helps to be compatible with both
    old and new uuu.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 48d58d07b9005c94c62fcdc345e65b0a8236d378)

--- include/configs/imx8mq_arm2.h
+++ include/configs/imx8mq_arm2.h
@@ -68,6 +68,10 @@
 #define CONFIG_SPL_NAND_BASE
 #define CONFIG_SPL_NAND_IDENT
 #define CONFIG_SYS_NAND_U_BOOT_OFFS 	0x4000000 /* Put the FIT out of first 64MB boot area */
+
+/* Set a redundant offset in nand FIT mtdpart. The new uuu will burn full boot image (not only FIT part) to the mtdpart, so we check both two offsets */
+#define CONFIG_SYS_NAND_U_BOOT_OFFS_REDUND \
+	(CONFIG_SYS_NAND_U_BOOT_OFFS + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512 - 0x8400)
 #endif
 #define CONFIG_SPL_DMA_SUPPORT
 
