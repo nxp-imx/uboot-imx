d6e0fdcde5773fed4804cdacf927808bd2da3d7d
Ji Luo <ji.luo@nxp.com>
2019-01-11 09:46:53 +0800

    MA-13937 Enable CAAM for imx8q
    The CAAM driver in u-boot will use JR0 by default, but for
    imx8q, both JR0 and JR1 are assigned to SECO and  A core
    should never access them.
    Power on the JR3 in this patchset and use it to complete
    the CAAM operations for imx8q.
    
    Test: CAAM self test cases pass for imx8q.
    
    below patches are merged to this commit:
    MA-13964 imx8q: Kick the CAAM JR before kicking the SMMU
    MA-13969 Fix CAAM build warnings
    
    Change-Id: Ie3d77d1f2910e7f4c257c797c12b5c8a30ad936a
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

---
+++ arch/arm/include/asm/arch-imx8/crm_regs.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef _ASM_ARCH_MX8_CRM_REGS_H
+#define _ASM_ARCH_MX8_CRM_REGS_H
+/* Dummy header, some imx-common code needs this file */
+#endif
