71f50000235f58848bad9b2a9f6050bf7d88d592
Ye Li <ye.li@nxp.com>
2019-01-21 00:37:36 -0800

    MLK-20783 imx8mm: spl: Change VDD_SOC to typical value 0.85V
    According to the latest datasheet, software is expected to program
    the VDD_SOC voltage to the typical value 0.85V prior to the first
    DRAM memory access. At default the VDD_SOC is 0.8V from PMIC BUCK1,
    so we have to change it to 0.85V in SPL.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Tested-by: Anson Huang <Anson.Huang@nxp.com>

--- board/freescale/imx8mm_evk/spl.c
+++ board/freescale/imx8mm_evk/spl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 NXP
+ * Copyright 2018-2019 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -176,6 +176,9 @@ int power_init_board(void)
 
 	/* unlock the PMIC regs */
 	pmic_reg_write(p, BD71837_REGLOCK, 0x1);
+
+	/* increase VDD_SOC to typical value 0.85v before first DRAM access */
+	pmic_reg_write(p, BD71837_BUCK1_VOLT_RUN, 0x0f);
 
 	/* increase VDD_DRAM to 0.9v for 3Ghz DDR */
 	pmic_reg_write(p, BD71837_BUCK5_VOLT, 0x2);
