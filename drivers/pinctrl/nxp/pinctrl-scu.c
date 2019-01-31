4d4715081cd6401650b0a5b60abd2a36c7a4137d
Ye Li <ye.li@nxp.com>
2019-01-30 22:34:48 -0800

    MLK-20886-9 pinctrl-scu: Check the pad partition before set pinmux
    If a pad is not owned by current partition we should not set its
    pinmux.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 547636514f8ae8b456f2351bc84cb7fb7b5fa3f3)

--- drivers/pinctrl/nxp/pinctrl-scu.c
+++ drivers/pinctrl/nxp/pinctrl-scu.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 NXP
+ * Copyright 2017-2019 NXP
  *
  * SPDX-License-Identifier:	GPL-2.0+
  */
@@ -34,6 +34,11 @@ static int imx_pinconf_scu_set(struct imx_pinctrl_soc_info *info,
 		return -EIO;
 	}
 
+	if (!sc_rm_is_pad_owned(ipcHndl, pin_id)) {
+		debug("Pad[%u] is not owned by curr partition\n", pin_id);
+		return -EPERM;
+	}
+
 	val |= PADRING_IFMUX_EN_MASK;
 	val |= PADRING_GP_EN_MASK;
 	val |= (mux << PADRING_IFMUX_SHIFT) & PADRING_IFMUX_MASK;
@@ -63,7 +68,7 @@ int imx_pinctrl_scu_process_pins(struct imx_pinctrl_soc_info *info, u32 *pin_dat
 		config_val = pin_data[j++];
 
 		ret = imx_pinconf_scu_set(info, pin_id, mux, config_val);
-		if (ret)
+		if (ret && ret != -EPERM)
 			printf("Set pin %d, mux %d, val %d, error\n", pin_id, mux, config_val);
 	}
 
