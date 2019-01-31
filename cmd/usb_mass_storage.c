d903cd890d6e6c0d94fc436c8d9a1ff3ce7e60dd
Ye Li <ye.li@nxp.com>
2019-01-31 02:20:23 -0800

    MLK-20887 usb: gadget: Fix controller index in UMS
    The usb mass storage (f_mass_storage.c) uses fixed usb index 0,
    this causes problem while CDNS3 USB controller index is 1.
    Modify the API of fsg to pass the controller index.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Jun Li <jun.li@nxp.com>

--- cmd/usb_mass_storage.c
+++ cmd/usb_mass_storage.c
@@ -167,7 +167,7 @@ static int do_usb_mass_storage(cmd_tbl_t *cmdtp, int flag,
 		goto cleanup_ums_init;
 	}
 
-	rc = fsg_init(ums, ums_count);
+	rc = fsg_init(ums, ums_count, controller_index);
 	if (rc) {
 		pr_err("fsg_init failed");
 		rc = CMD_RET_FAILURE;
