6e103c5b9279588052346601abc8c10e310f9b5d
Ye Li <ye.li@nxp.com>
2019-02-19 00:07:33 -0800

    MLK-20945-5 imx8: Select boot device dynamically
    For fspi build, we will enable both SPL NOR support and SPL SPI
    support. SPL will dynamically check the resource owner then
    select corresponding boot device.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 675cc6031033fbe5e7d8cfe01ebe1dedfd6c1b96)
    (cherry picked from commit 4758cf21988496ac9ad0eb19045cece970f626ff)

--- arch/arm/mach-imx/imx8/cpu.c
+++ arch/arm/mach-imx/imx8/cpu.c
@@ -27,6 +27,7 @@
 #include <asm/setup.h>
 #include <asm/mach-imx/boot_mode.h>
 #include <asm/mach-imx/imx_vservice.h>
+#include <spl.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -1023,4 +1024,15 @@ int board_imx_lpi2c_bind(struct udevice *dev)
 		return 0;
 
 	return -ENODEV;
+}
+
+void board_boot_order(u32 *spl_boot_list)
+{
+	spl_boot_list[0] = spl_boot_device();
+
+	if (spl_boot_list[0] == BOOT_DEVICE_SPI) {
+		/* Check whether we own the flexspi0, if not, use NOR boot */
+		if (!sc_rm_is_resource_owned(-1, SC_R_FSPI_0))
+			spl_boot_list[0] = BOOT_DEVICE_NOR;
+	}
 }
