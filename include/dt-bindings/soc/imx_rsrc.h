dc80d9976dbe69be1dd7cea87ca1d94b5bc2aa66
Ye Li <ye.li@nxp.com>
2019-03-19 23:51:56 -0700

    MLK-23574-7 misc: scu_api: Update SCFW API
    Add new API denitions for clock change.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 22f0808fbbae08d6ca969133bf9eb141e736e476)

--- include/dt-bindings/soc/imx_rsrc.h
+++ include/dt-bindings/soc/imx_rsrc.h
@@ -555,4 +555,6 @@
 #define SC_R_LAST                       541
 #define SC_R_NONE                       0xFFF0
 
+#define SC_R_NONE                       0xFFF0
+
 #endif /* DT_BINDINGS_RSCRC_IMX_H */
