77b46f349fb78c57cf014a5d7e90c650e565a61a
Peng Fan <peng.fan@nxp.com>
2019-03-29 16:55:39 +0800

    MLK-21291 imx8mm: evk: not restrict uart4 when enabling jailhouse
    When booting dual linux with jailhouse, inmate linux will use
    the 2nd uart, so not restrict access the uart for jailhouse case.
    
    The best solution would be using SIP call to ATF, for simplicity,
    directly modify the RDC register.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit b22b70ff0b5bbacea7383bde9e8893bb504199c3)
    (cherry picked from commit e5768d04b57599ef8d5fcb4664d87b7c3ba50feb)
    (cherry picked from commit 0a7bb85d05f567274d54b4aaf6d061b0b1c03b33)
    (cherry picked from commit fb97a542a5c74cb57ca9dcc10c960fde4f4bc209)
    (cherry picked from commit bbf3ab671385896a736f7643c1fc41c6af0b033e)
    (cherry picked from commit 64c5d8db4f9855d19503d8ce392e8e42ae230394)

--- include/configs/imx8mm_evk.h
+++ include/configs/imx8mm_evk.h
@@ -77,12 +77,12 @@
  */
 #define JAILHOUSE_ENV \
 	"jh_clk= \0 " \
-	"jh_mmcboot=setenv fdtfile imx8mm-evk-root.dtb;" \
+	"jh_mmcboot=mw 0x303d0518 0xff; setenv fdtfile imx8mm-evk-root.dtb;" \
 		"setenv jh_clk clk_ignore_unused; " \
 			   "if run loadimage; then " \
 				   "run mmcboot; " \
 			   "else run jh_netboot; fi; \0" \
-	"jh_netboot=setenv fdtfile imx8mm-evk-root.dtb; setenv jh_clk clk_ignore_unused; run netboot; \0 "
+	"jh_netboot=mw 0x303d0518 0xff; setenv fdtfile imx8mm-evk-root.dtb; setenv jh_clk clk_ignore_unused; run netboot; \0 "
 
 /* Initial environment variables */
 #if defined(CONFIG_NAND_BOOT)
