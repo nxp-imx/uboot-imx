57fa56f3ce6028b3b8cc75a905ba230dbee522dc
Zhang Bo <bo.zhang@nxp.com>
2019-04-02 16:15:06 +0800

    MA-14501[Android] change BOOTAUX_RESERVED_MEM macro to defconfig
    As the M4 use different DDR memory size in normal android/car2 and car
    image, use different defconfig for car2 to decrease DDR memory
    reservation. So memory reserved for each M4 core is 8MB in car2 and
    normal android image. it's 32MB for car image.
    
    Change-Id: Idf608f539cd614a154c78e3a1af28eff1da5c1f2
    Signed-off-by: Zhang Bo <bo.zhang@nxp.com>

--- include/configs/imx8qm_arm2.h
+++ include/configs/imx8qm_arm2.h
@@ -437,7 +437,5 @@
 #endif
 
 #define CONFIG_OF_SYSTEM_SETUP
-#define BOOTAUX_RESERVED_MEM_BASE 0x88000000
-#define BOOTAUX_RESERVED_MEM_SIZE 0x08000000 /* Reserve from second 128MB */
 
 #endif /* __IMX8QM_ARM2_H */
