bd9cbf66f801ac930f7e62da9ed42653fd3dc7f9
Ye Li <ye.li@nxp.com>
2019-04-18 23:34:13 -0700

    MLK-18654-4 imx8mm_evk: Enable SPL SDP for iMX8MM EVK board
    Enable the SPL SDP for iMX8MM EVK. So that when booting from USB serial
    download mode, the SPL will enter SDP to download FIT image from USB host
    (uuu tool) and boot into FIT image.
    
    The SDP driver needs larger malloc pool size, so extend the SPL malloc pool
    to 12KB and reduce the SPL BSS MAX to 4KB because BSS actual size is about 1KB.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- board/freescale/imx8mm_evk/spl.c
+++ board/freescale/imx8mm_evk/spl.c
@@ -199,6 +199,13 @@ int power_init_board(void)
 
 void spl_board_init(void)
 {
+#ifndef CONFIG_SPL_USB_SDP_SUPPORT
+	/* Serial download mode */
+	if (is_usb_boot()) {
+		puts("Back to ROM, SDP\n");
+		restore_boot_params();
+	}
+#endif
 	puts("Normal Boot\n");
 }
 
