54849b1302ce54cb3cc0e4a3e62afbd1af6e1e04
Ye Li <ye.li@nxp.com>
2019-04-28 04:08:43 -0700

    MLK-21850-1 dm: device: Check driver flag before setting default clocks
    Add a new driver FLAG (DM_FLAG_IGNORE_DEFAULT_CLKS). If any driver
    is set with this flag, then when probing the device, we will skip
    setting the default clocks.
    
    This is useful when clock driver can't work with some device driver.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 5dd4ff270dc387111f5feb85e4020adf2d60a32f)
    (cherry picked from commit 7b5950ea2999d3c4e28ab62b5ba363037ae649c3)
    (cherry picked from commit aa9f568864f18cdb59b13917f0b576e28142b9c8)
    (cherry picked from commit 80c48ed9b45714e44fb814775a5c320a9f144e0d)

--- drivers/core/device.c
+++ drivers/core/device.c
@@ -570,7 +570,7 @@ int device_probe(struct udevice *dev)
 	}
 
 	/* Only handle devices that have a valid ofnode */
-	if (dev_has_ofnode(dev)) {
+	if (dev_has_ofnode(dev) && !(dev->driver->flags & DM_FLAG_IGNORE_DEFAULT_CLKS)) {
 		/*
 		 * Process 'assigned-{clocks/clock-parents/clock-rates}'
 		 * properties
