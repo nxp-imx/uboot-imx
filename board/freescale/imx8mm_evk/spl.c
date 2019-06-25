00d87b9dbb0af6269d09ebc82f7c60341752a47a
Ye Li <ye.li@nxp.com>
2019-06-25 02:51:42 -0700

    MLK-22105-3 imx8m: spl: Move bss clean up before arch_cpu_init
    Since rng_init is used arch_cpu_init, we have to clean up BSS section
    before it.
    Also remove the unnecessary memset to global data, because
    board_init_f_init_reserve already memset it. If we memset it in board_init_f,
    the gd->malloc_base is reset to 0 and will cause early malloc problem
    when CONFIG_MALLOC_F_ADDR is not set.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit c36ae6a623b075192b20d93f9e3fe966961d86f4)
    (cherry picked from commit f7e11efcb4689cd7e1068677454b2bd4480708f3)
    (cherry picked from commit 692bb011e8b0302b9e96b54de7a8fc9a17e69b56)
    (cherry picked from commit 5289397ce5ddbf7a02008ac802580988b4c95dd9)

--- board/freescale/imx8mm_evk/spl.c
+++ board/freescale/imx8mm_evk/spl.c
@@ -107,14 +107,14 @@ void board_init_f(ulong dummy)
 	struct udevice *dev;
 	int ret;
 
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
 	arch_cpu_init();
 
 	init_uart_clk(1);
 
 	timer_init();
-
-	/* Clear the BSS. */
-	memset(__bss_start, 0, __bss_end - __bss_start);
 
 	ret = spl_early_init();
 	if (ret) {
