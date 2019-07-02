d4eee224f46e89b9990ea7c1ddd7e487710607bc
Ye Li <ye.li@nxp.com>
2019-07-02 03:28:45 -0700

    MLK-23574-6 imx8: Add lpcg driver for iMX8QM/QXP
    Each module may have one or more lpcg registers for SW/HW enabling its
    clocks. Add lpcg register address and its driver for accessing lpcg.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit d414a8fac97f8793cd75be5e3178eb3b831018d1)
    (cherry picked from commit 06c5b838787206cb921ad1a12b6940918ef74cba)

---
+++ arch/arm/include/asm/arch-imx8/lpcg.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __ASM_ARCH_IMX8_LPCG_H__
+#define __ASM_ARCH_IMX8_LPCG_H__
+
+#if defined(CONFIG_IMX8QM)
+#include "imx8qm_lpcg.h"
+#elif defined(CONFIG_IMX8QXP)
+#include "imx8qxp_lpcg.h"
+#else
+#error "No lpcg header"
+#endif
+
+void lpcg_clock_off(u32 lpcg_addr, u8 clk);
+void lpcg_clock_on(u32 lpcg_addr, u8 clk);
+void lpcg_clock_autogate(u32 lpcg_addr, u8 clk);
+bool lpcg_is_clock_on(u32 lpcg_addr, u8 clk);
+void lpcg_all_clock_off(u32 lpcg_addr);
+void lpcg_all_clock_on(u32 lpcg_addr);
+void lpcg_all_clock_autogate(u32 lpcg_addr);
+
+#endif	/* __ASM_ARCH_IMX8_LPCG_H__ */
