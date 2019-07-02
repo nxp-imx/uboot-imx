f7b1e3cc3f665f244ce62f4716ebfa20f9bd3279
faqiang.zhu <faqiang.zhu@nxp.com>
2019-07-02 13:32:00 +0800

    MA-15062-1 change names of MACROs used to boot MCU on imx8m devices
    new imx8mn chips have Cortex-M7 inside, not like other imx8m devices
    of imx8mm and imx8mq which have Cortex-M4 inside. the names of MACROs
    used to boot MCU on imx8m devices is modified to make them more common
    to cover M4 and M7.
    annotations are also modified based on the differences between M4 and
    M7.
    
    Change-Id: Ida272e6ecdf577eeaadb9f1242f4524bd1014cac
    Signed-off-by: faqiang.zhu <faqiang.zhu@nxp.com>

--- include/imx_sip.h
+++ include/imx_sip.h
@@ -8,7 +8,7 @@
 #define _IMX_SIP_H_
 
 #define IMX_SIP_SRC		0xC2000005
-#define IMX_SIP_SRC_M4_START	0x00
-#define IMX_SIP_SRC_M4_STARTED	0x01
+#define IMX_SIP_SRC_MCU_START	0x00
+#define IMX_SIP_SRC_MCU_STARTED	0x01
 
 #endif
