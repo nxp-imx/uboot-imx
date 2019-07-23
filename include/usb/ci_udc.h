e00632802b5cc5d06b57400696d256394362a148
Sherry Sun <sherry.sun@nxp.com>
2019-07-23 15:21:57 -0400

    MLK-22336-1 usb: ci_udc: Convert driver to DM_USB_GADGET
    Convert the ci_udc driver to driver model by using the uclass
    UCLASS_USB_GADGET_GENERIC. The clk and power of USB controller and USB
    PHY both are initialized by parsing the device tree nodes.
    
    If CONFIG_DM_USB_GADGET is defined, we use the ci_udc driver in DM way,
    if it does not defined, we can use ci_udc driver in its original Non-DM
    way.
    
    Move some USB PHY register definitions from ehci-mx6.c to
    asm/mach-imx/regs-usbphy.h in order to share with DM usb gadget driver.
    
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 5c50d6bd6fda34827a27387e42c9a803da40b1e5)
    (cherry picked from commit 24c896d4d2fab51abd287f1a991c68fc709d2458)
    (cherry picked from commit 455bed086f7f264a9abbb778218c095dff332300)

--- include/usb/ci_udc.h
+++ include/usb/ci_udc.h
@@ -7,7 +7,16 @@
 
 #ifndef __CI_UDC_H__
 #define __CI_UDC_H__
+#include <usb/ehci-ci.h>
 
 #define EP_MAX_PACKET_SIZE	0x200
 #define EP0_MAX_PACKET_SIZE	64
+
+struct ehci_mx6_phy_data {
+	void __iomem *phy_addr;
+	void __iomem *misc_addr;
+	void __iomem *anatop_addr;
+};
+
+void ehci_mx6_phy_init(struct usb_ehci *ehci, struct ehci_mx6_phy_data *phy_data, int index);
 #endif /* __CI_UDC_H__ */
