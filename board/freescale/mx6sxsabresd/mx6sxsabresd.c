b4c7569818e66a4a8fa2158712608330c5dbed3e
Ye Li <ye.li@nxp.com>
2019-08-06 01:02:41 -0700

    MLK-22398-8 mx6sxsdb: Enable PCIE IMX DM driver
    Update the board codes and configs to enable pcie imx DM driver.
    The pcie node in SDB board DTS reply on the alway on regulator
    "reg_pcie". So power up the always on regulators in board_init.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 4271f767a42f2ccb6085aec3abf9197faf60bb9f)

--- board/freescale/mx6sxsabresd/mx6sxsabresd.c
+++ board/freescale/mx6sxsabresd/mx6sxsabresd.c
@@ -32,6 +32,7 @@
 #include <usb.h>
 #include <usb/ehci-ci.h>
 #include <asm/mach-imx/video.h>
+#include <power/regulator.h>
 
 #ifdef CONFIG_IMX_RDC
 #include <asm/mach-imx/rdc-sema.h>
@@ -162,7 +163,7 @@ static iomux_v3_cfg_t const phy_control_pads[] = {
 	MX6_PAD_ENET2_CRS__GPIO2_IO_7 | MUX_PAD_CTRL(NO_PAD_CTRL),
 };
 
-#ifdef CONFIG_PCIE_IMX
+#if defined(CONFIG_PCIE_IMX) && !defined(CONFIG_DM_PCI)
 iomux_v3_cfg_t const pcie_pads[] = {
 	MX6_PAD_ENET1_COL__GPIO2_IO_0 | MUX_PAD_CTRL(NO_PAD_CTRL),	/* POWER */
 	MX6_PAD_ENET1_CRS__GPIO2_IO_1 | MUX_PAD_CTRL(NO_PAD_CTRL),	/* RESET */
@@ -818,9 +819,9 @@ int board_init(void)
 	 */
 	imx_iomux_v3_setup_pad(wdog_b_pad);
 
-	/* Active high for ncp692 */
-	gpio_request(IMX_GPIO_NR(4, 16), "ncp692_en");
-	gpio_direction_output(IMX_GPIO_NR(4, 16), 1);
+#if defined(CONFIG_DM_REGULATOR)
+	regulators_enable_boot_on(false);
+#endif
 
 #ifdef CONFIG_SYS_I2C
 	setup_i2c(0, CONFIG_SYS_I2C_SPEED, 0x7f, &i2c_pad_info1);
@@ -837,7 +838,7 @@ int board_init(void)
 	board_qspi_init();
 #endif
 
-#ifdef CONFIG_PCIE_IMX
+#if defined(CONFIG_PCIE_IMX) && !defined(CONFIG_DM_PCI)
 	setup_pcie();
 #endif
 
