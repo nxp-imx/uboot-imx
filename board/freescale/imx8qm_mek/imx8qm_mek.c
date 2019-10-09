56bbbae97225bb5231f71d2ea4912b7fcfd6ed06
Franck LENORMAND <franck.lenormand@nxp.com>
2019-10-09 10:27:43 +0200

    SSI-87: imx8: Configure SNVS
    Add a module to configure the tamper and secure violation of
    the SNVS using the SCU API.
    
    The module also adds some commands:
     - snvs_cfg: Configure the SNVS HP and LP registers
     - snvs_dgo_cfg: Configure the SNVS DGO bloc if present (8QXP)
     - tamper_pin_cfg: Change the configuration of the tamper pins
     - snvs_clear_status: Allow to write to LPSR and LPTDSR to clear status bits
    
    Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>
    Reviewed-by: Silvano Di Ninno <silvano.dininno@nxp.com>
    (cherry picked from commit b84be4fa1454ee0cd718be329d630b55aff34273)

--- board/freescale/imx8qm_mek/imx8qm_mek.c
+++ board/freescale/imx8qm_mek/imx8qm_mek.c
@@ -15,6 +15,7 @@
 #include <asm/arch/clock.h>
 #include <asm/arch/sci/sci.h>
 #include <asm/arch/imx8-pins.h>
+#include <asm/arch/snvs_security_sc.h>
 #include <usb.h>
 #include <asm/arch/iomux.h>
 #include <asm/arch/sys_proto.h>
@@ -332,6 +333,15 @@ int board_init(void)
 
 #if defined(CONFIG_USB) && defined(CONFIG_USB_TCPC)
 	setup_typec();
+#endif
+
+#ifdef CONFIG_SNVS_SEC_SC_AUTO
+	{
+		int ret = snvs_security_sc_init();
+
+		if (ret)
+			return ret;
+	}
 #endif
 
 	return 0;
