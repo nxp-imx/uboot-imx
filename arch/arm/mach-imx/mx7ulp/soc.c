d0c99654c2ee9869aab4f1f7d81a4946d44025fe
Ye Li <ye.li@nxp.com>
2019-10-23 22:40:18 -0700

    MLK-22830 mx7ulp: Fix build break of mx7ulp_evk_m4boot_defconfig
    Get build error when using mx7ulp_evk_m4boot_defconfig
    
      arch/arm/lib/built-in.o: In function `setup_serial_tag':
      /home/leyoen/Workspace/uboot-imx/arch/arm/lib/bootm.c:191:
      undefined reference to `get_board_serial'
    
    The boot cause is CONFIG_SERIAL_TAG is defined in header file
    without depending on fastboot, but the function definition uses
    CONFIG_FSL_FASTBOOT. So remove the wrong macro from function
    definition to fix the issue
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/mach-imx/mx7ulp/soc.c
+++ arch/arm/mach-imx/mx7ulp/soc.c
@@ -421,7 +421,6 @@ bool is_usb_boot(void)
 	return get_boot_device() == USB_BOOT;
 }
 
-#ifdef CONFIG_FSL_FASTBOOT
 #ifdef CONFIG_SERIAL_TAG
 void get_board_serial(struct tag_serialnr *serialnr)
 {
@@ -434,4 +433,3 @@ void get_board_serial(struct tag_serialnr *serialnr)
 	serialnr->high = (fuse->cfg2 & 0xFFFF) + ((fuse->cfg3 & 0xFFFF) << 16);
 }
 #endif /*CONFIG_SERIAL_TAG*/
-#endif /*CONFIG_FSL_FASTBOOT*/
