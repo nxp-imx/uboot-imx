5c2cc02edf0d134485494048dba5d561133cca80
Ye Li <ye.li@nxp.com>
2019-11-19 23:00:07 -0800

    MLK-23964-23 imx7ulp_evk: Enable the rm68200 mipi panel for splash screen
    Update DTS for LCDIF, MIPI DSI and panel relevant nodes.
    Enable the video drivers and splash screen in board codse
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 52d4c7c3e98d573f1c3e4e67e02dd4b08be6bee7)
    (cherry picked from commit 6a4a992f81ffd75f0c9ec78703009000d8045a5c)
    (cherry picked from commit d963dfd9fca5e28db01b6faa03c0d8f8821ed69e)
    (cherry picked from commit 2036e8dbe9edee25d132e47cb828f32fd79e7968)

--- include/configs/mx7ulp_evk.h
+++ include/configs/mx7ulp_evk.h
@@ -41,6 +41,7 @@
 	"script=boot.scr\0" \
 	"image=zImage\0" \
 	"console=ttyLP0\0" \
+	"splashimage=0x78000000\0" \
 	"fdt_high=0xffffffff\0" \
 	"initrd_high=0xffffffff\0" \
 	"fdt_file=" CONFIG_DEFAULT_FDT_FILE ".dtb\0" \
