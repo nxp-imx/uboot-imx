nxp-imx/uboot-imx

commit 79474268a4d7ec473435181b85176b266549f278

MLK-22997: add nandfit_part env for uuu
Add the nandfit_part environment to notify uuu to burn flash.bin to
nandfit partition for iMX8QM/QX/MQ/MM platforms

Signed-off-by: Han Xu <han.xu@nxp.com>

include/configs/imx_env.h
@@ -18,6 +18,14 @@
     #define FASTBOOT_CMD "echo \"Run fastboot ...\"; fastboot 0; "
 #endif
 
+/* define the nandfit partiton environment for uuu */
+#if defined(CONFIG_IMX8MM) || defined(CONFIG_IMX8MQ) || \
+	defined(CONFIG_IMX8QM) || defined(CONFIG_IMX8QXP)
+#define MFG_NAND_FIT_PARTITION "nandfit_part=yes"
+#else
+#define MFG_NAND_FIT_PARTITION ""
+#endif
+
 #define CONFIG_MFG_ENV_SETTINGS_DEFAULT \
 	"mfgtool_args=setenv bootargs console=${console},${baudrate} " \
 		"rdinit=/linuxrc " \
@@ -34,5 +42,7 @@
         "else " \
 		FASTBOOT_CMD  \
         "fi;\0" \
+	MFG_NAND_FIT_PARTITION \
+	"\0" \
 
 #endif

commit 61da9c0022fa62806764e58451b736709a5a548a

MLK-23016 configs: fix CONFIG_MFG_ENV_SETTINGS_DEFAULT macro
Remove end of string char of CONFIG_MFG_ENV_SETTINGS_DEFAULT.
This corrupted uboot env on imx6/7:

\## Error: "findfdt" not defined
\## Error: "findtee" not defined
MMC Device 0 not found
no mmc device at slot 0
MMC Device 0 not found
no mmc device at slot 0
\## Error: "netboot" not defined

fixes: 79474268a4d7ec473435181b85176b266549f278
("MLK-22997: add nandfit_part env for uuu")

Signed-off-by: Clement Faure <clement.faure@nxp.com>
Reviewed-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Han Xu <han.xu@nxp.com>

include/configs/imx_env.h
@@ -21,7 +21,7 @@
 /* define the nandfit partiton environment for uuu */
 #if defined(CONFIG_IMX8MM) || defined(CONFIG_IMX8MQ) || \
 	defined(CONFIG_IMX8QM) || defined(CONFIG_IMX8QXP)
-#define MFG_NAND_FIT_PARTITION "nandfit_part=yes"
+#define MFG_NAND_FIT_PARTITION "nandfit_part=yes\0"
 #else
 #define MFG_NAND_FIT_PARTITION ""
 #endif
@@ -43,6 +43,5 @@
 		FASTBOOT_CMD  \
         "fi;\0" \
 	MFG_NAND_FIT_PARTITION \
-	"\0" \
 
 #endif