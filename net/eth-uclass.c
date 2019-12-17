b7cbfa5f86e3be87e831786744f81c8cbda2f2e7
Ye Li <ye.li@nxp.com>
2019-12-17 00:51:44 -0800

    MLK-23574-28 net: Add eth phy generic driver for shared MDIO
    For dual ethernet controllers, the HW design may connect ETH phys to
    one MDIO ports. So two different ethernet drivers have to share MDIO bus.
    Since two ethernet drivers are independent, we can't ensure their probe order.
    
    To resolve this problem, introduce an eth phy generic driver and uclass.
    
    After eth-uclass binds, we search the mdio node and binds the phy node with
    the eth-phy-generic driver.
    
    When one eth driver get its phy device, the parent of phy device will probe prior than
    phy device. So this ensure the eth driver ownes the MDIO bus will be probed
    before using its MDIO.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- net/eth-uclass.c
+++ net/eth-uclass.c
@@ -15,6 +15,7 @@
 #include <dm/uclass-internal.h>
 #include <net/pcap.h>
 #include "eth_internal.h"
+#include <eth_phy.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -462,6 +463,10 @@ static int eth_post_bind(struct udevice *dev)
 		       dev->name);
 		return -EINVAL;
 	}
+
+#ifdef CONFIG_DM_ETH_PHY
+	eth_phy_binds_nodes(dev);
+#endif
 
 	return 0;
 }
