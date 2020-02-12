333273dd07eed245fda6947795b9b007f10b6c42
Sherry Sun <sherry.sun@nxp.com>
2020-02-12 14:04:57 +0800

    MLK-23307-2 imx8mp: Add three reserved-memory dts nodes if inline ECC is enabled
    If inline ECC is enabled on imx8mp, then the ECC region should be
    reserved. Since the limit of the ddr address map when use inline ecc
    and 6G memory, ECC region have to be divided into three parts. So these
    three parts ECC region should be set as reserved-memory with no-map.
    
    Signed-off-by: Sherry Sun <sherry.sun@nxp.com>
    (cherry picked from commit 0ea77fc9364e46e897cfa554d93895595aaf2a20)
    (cherry picked from commit db36f389921293abbd2824e95867650ae290bad5)
    (cherry picked from commit 95c9950c65251dc2748519660598920c3048a24c)
    (cherry picked from commit a6fe82733f9c4c7b8b3f5ca552e4e9ccd7db1555)

--- arch/arm/include/asm/mach-imx/sys_proto.h
+++ arch/arm/include/asm/mach-imx/sys_proto.h
@@ -273,4 +273,7 @@ void enable_ca7_smp(void);
 
 enum boot_device get_boot_device(void);
 
+int add_res_mem_dt_node(void *fdt, const char *name, phys_addr_t pa,
+			size_t size);
+int add_dt_path_subnode(void *fdt, const char *path, const char *subnode);
 #endif
