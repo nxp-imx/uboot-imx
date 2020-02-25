5b184052ceff17a74523a4097990d320e5103466
faqiang.zhu <faqiang.zhu@nxp.com>
2020-02-25 10:18:24 +0800

    MA-16457-1 avoid to try to access system partition
    To enable dynamic partition feature, system partition will be a logic
    parition in "super" partition, uboot can't access system partition
    anymore.
    
    In i.MX Android use case, only vbmeta partition is used to verify other
    imags. boot and system are not used. so there is no need to access
    system partition to get avb device info, remove system partition from
    avb lib.
    
    Now, standard Android will boot with ramdisk in boot.img, there is no
    need to provide root info to kernel for standard Android. so only
    Android Auto will provide this info.
    
    Change-Id: I99a43eb8f7aa1dc635e3937c93266f881c9b3655
    Signed-off-by: faqiang.zhu <faqiang.zhu@nxp.com>
    (cherry picked from commit 3a2418a1cc097cd956347fc12b0b4e0566652bfd)

--- drivers/fastboot/fb_fsl/fb_fsl_boot.c
+++ drivers/fastboot/fb_fsl/fb_fsl_boot.c
@@ -218,7 +218,7 @@ U_BOOT_CMD(
 );
 #endif
 
-#ifdef CONFIG_SYSTEM_RAMDISK_SUPPORT
+#if defined CONFIG_SYSTEM_RAMDISK_SUPPORT && defined CONFIG_ANDROID_AUTO_SUPPORT
 /* Setup booargs for taking the system parition as ramdisk */
 static void fastboot_setup_system_boot_args(const char *slot, bool append_root)
 {
@@ -249,12 +249,6 @@ static void fastboot_setup_system_boot_args(const char *slot, bool append_root)
 					ptentry->partition_index);
 		}
 		strcat(bootargs_3rd, "rootwait");
-
-		/* for standard android, recovery ramdisk will be used anyway, to
-		 * boot up Android, "androidboot.force_normal_boot=1" is needed */
-#ifndef CONFIG_ANDROID_AUTO_SUPPORT
-			strcat(bootargs_3rd, " androidboot.force_normal_boot=1");
-#endif
 
 		env_set("bootargs_3rd", bootargs_3rd);
 	} else {
@@ -416,6 +410,7 @@ int vbh_calculate(uint8_t *vbh, AvbSlotVerifyData *avb_out_data)
 		ret = -1;
 		goto fail;
 	}
+
 	/* Calculate VBH2 */
 	if (sha256_concatenation(hash_buf, vbh, image_hash)) {
 		ret = -1;
@@ -678,9 +673,14 @@ int do_boota(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]) {
 		strcat(bootargs_sec, avb_out_data->cmdline);
 #else
 		strcat(bootargs_sec, strstr(avb_out_data->cmdline, "androidboot"));
+		/* for standard android, recovery ramdisk will be used anyway, to
+		 * boot up Android, "androidboot.force_normal_boot=1" is needed */
+		if(!is_recovery_mode) {
+			strcat(bootargs_sec, " androidboot.force_normal_boot=1");
+		}
 #endif
 		env_set("bootargs_sec", bootargs_sec);
-#ifdef CONFIG_SYSTEM_RAMDISK_SUPPORT
+#if defined CONFIG_SYSTEM_RAMDISK_SUPPORT && defined CONFIG_ANDROID_AUTO_SUPPORT
 		if(!is_recovery_mode) {
 			if(avb_out_data->cmdline != NULL && strstr(avb_out_data->cmdline, "root="))
 				fastboot_setup_system_boot_args(avb_out_data->ab_suffix, false);
