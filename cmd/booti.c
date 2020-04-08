f9bc9d634322c59309a8581c7f3c425661bc4e7f
Ji Luo <ji.luo@nxp.com>
2020-04-08 12:34:55 +0800

    MA-16877 Skip kernel hab authentication for android
    Android use AVB to verify the kernel, hab authentication is
    not necessary for boot image.
    
    For imx8m, don't authenticate the kernel image when AVB
    (CONFIG_AVB_SUPPORT) is enabled. For imx8q, as android uses
    different 'CONFIG_EXTRA_ENV_SETTINGS' and 'CONFIG_BOOTCOMMAND'
    with linux bsp, so it won't try to do kernel hab authentication.
    by default.
    
    Test: boot imx8mp with "CONFIG_SECURE_BOOT" and imx8qxp with
          'CONFIG_AHAB_BOOT'.
    
    Change-Id: I1b2087ce7d8f9795422a053b6b68a694c86f0b3d
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- cmd/booti.c
+++ cmd/booti.c
@@ -41,7 +41,7 @@ static int booti_start(cmd_tbl_t *cmdtp, int flag, int argc,
 	if (ret != 0)
 		return 1;
 
-#ifdef CONFIG_SECURE_BOOT
+#if defined(CONFIG_SECURE_BOOT) && !defined(CONFIG_AVB_SUPPORT)
 	extern int authenticate_image(
 		uint32_t ddr_start, uint32_t raw_image_size);
 	if (authenticate_image(ld, image_size) != 0) {
