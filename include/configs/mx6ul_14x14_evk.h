109860f30f2a9e48e8995d29faa1b00a6100f498
Ye Li <ye.li@nxp.com>
2020-04-15 02:26:47 -0700

    MLK-18155-2 mx6ulevk: Update board level codes
    Add functions below to align with v2020.04
    1. Switch from SPL to Non-SPL
    2. Add plugin and DCD for DDR initialization
    3. Update two ethernet controllers support at runtime
    4. Add GPMI NAND support
    5. Add LDO bypass settings
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 5daeae6f49fefdf6eb54f5a9ae37f21483883f83)
    (cherry picked from commit 755fb5c111a3aeb55d16790336af14fa2678c4bf)
    (cherry picked from commit 2071ae10025d45f617210bf4b867bd2fd650a7c6)

--- include/configs/mx6ul_14x14_evk.h
+++ include/configs/mx6ul_14x14_evk.h
@@ -15,6 +15,12 @@
 
 #define is_mx6ul_9x9_evk()	IS_ENABLED(CONFIG_TARGET_MX6UL_9X9_EVK)
 
+#ifdef CONFIG_TARGET_MX6UL_9X9_EVK
+#define BOOTARGS_CMA_SIZE   "cma=96M "
+#else
+#define BOOTARGS_CMA_SIZE   ""
+#endif
+
 #define CFG_MXC_UART_BASE		UART1_BASE
 
 /* MMC Configs */
@@ -30,7 +36,46 @@
 
 #endif
 
+#ifdef CONFIG_NAND_BOOT
+#define MFG_NAND_PARTITION "mtdparts=gpmi-nand:64m(boot),16m(kernel),16m(dtb),1m(misc),-(rootfs) "
+#else
+#define MFG_NAND_PARTITION ""
+#endif
+
+#define CFG_MFG_ENV_SETTINGS \
+	"mfgtool_args=setenv bootargs console=${console},${baudrate} " \
+		BOOTARGS_CMA_SIZE \
+		"rdinit=/linuxrc " \
+		"g_mass_storage.stall=0 g_mass_storage.removable=1 " \
+		"g_mass_storage.file=/fat g_mass_storage.ro=1 " \
+		"g_mass_storage.idVendor=0x066F g_mass_storage.idProduct=0x37FF "\
+		"g_mass_storage.iSerialNumber=\"\" "\
+		MFG_NAND_PARTITION \
+		"clk_ignore_unused "\
+		"\0" \
+	"initrd_addr=0x83800000\0" \
+	"initrd_high=0xffffffff\0" \
+	"bootcmd_mfg=run mfgtool_args;bootz ${loadaddr} ${initrd_addr} ${fdt_addr};\0" \
+
+#if defined(CONFIG_NAND_BOOT)
 #define CFG_EXTRA_ENV_SETTINGS \
+	CFG_MFG_ENV_SETTINGS \
+	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"videomode=video=ctfb:x:480,y:272,depth:24,pclk:108695,le:8,ri:4,up:2,lo:4,hs:41,vs:10,sync:0,vmode:0\0" \
+	"fdt_addr=0x83000000\0" \
+	"fdt_high=0xffffffff\0"	  \
+	"console=ttymxc0\0" \
+	"bootargs=console=ttymxc0,115200 ubi.mtd=4 "  \
+		"root=ubi0:rootfs rootfstype=ubifs "		     \
+		BOOTARGS_CMA_SIZE \
+		"mtdparts=gpmi-nand:64m(boot),16m(kernel),16m(dtb),1m(misc),-(rootfs)\0"\
+	"bootcmd=nand read ${loadaddr} 0x4000000 0xc00000;"\
+		"nand read ${fdt_addr} 0x5000000 0x100000;"\
+		"bootz ${loadaddr} - ${fdt_addr}\0"
+
+#else
+#define CFG_EXTRA_ENV_SETTINGS \
+	CFG_MFG_ENV_SETTINGS \
 	"script=boot.scr\0" \
 	"image=zImage\0" \
 	"console=ttymxc0\0" \
@@ -47,6 +92,7 @@
 	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
 	"mmcautodetect=yes\0" \
 	"mmcargs=setenv bootargs console=${console},${baudrate} " \
+		BOOTARGS_CMA_SIZE \
 		"root=${mmcroot}\0" \
 	"loadbootscript=" \
 		"fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
@@ -70,6 +116,7 @@
 			"bootz; " \
 		"fi;\0" \
 	"netargs=setenv bootargs console=${console},${baudrate} " \
+		BOOTARGS_CMA_SIZE \
 		"root=/dev/nfs " \
 	"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
 		"netboot=echo Booting from net ...; " \
@@ -103,6 +150,8 @@
 					"echo WARNING: Could not determine dtb to use; fi; " \
 			"fi;\0" \
 
+#endif
+
 /* Miscellaneous configurable options */
 
 /* Physical Memory Map */
@@ -114,22 +163,13 @@
 
 /* environment organization */
 
+/* NAND stuff */
+#define CFG_SYS_NAND_BASE		0x40000000
+
 /* USB Configs */
 #ifdef CONFIG_CMD_USB
 #define CFG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
 #define CFG_MXC_USB_FLAGS   0
 #endif
-
-#ifdef CONFIG_CMD_NET
-#define CFG_FEC_ENET_DEV		1
-
-#if (CFG_FEC_ENET_DEV == 0)
-#define CFG_FEC_MXC_PHYADDR          0x2
-#elif (CFG_FEC_ENET_DEV == 1)
-#define CFG_FEC_MXC_PHYADDR		0x1
-#endif
-#endif
-
-#define MXS_LCDIF_BASE MX6UL_LCDIF1_BASE_ADDR
 
 #endif
