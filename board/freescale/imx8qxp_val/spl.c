c251cbece893b9206301218ecc81242e2b5b2c0b
Ye Li <ye.li@nxp.com>
2020-04-30 00:46:41 -0700

    MLK-23574-46 imx8qxp: Add iMX8QXP/iMX8DX validation board support
    Add the validation board support for iMX8QXP/iMX8DX platforms.
        - iMX8QXP 21x21 DDR3 board
        - iMX8QXP 21x21 LPDDR4 board
        - iMX8QXP 17x17 LPDDR4 board
        - iMX8DX 17x17 DDR3 board
    
    Ready functions:  SD/eMMC, flexspi, ENET, USB, i2c and NAND
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

---
+++ board/freescale/imx8qxp_val/spl.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <spl.h>
+#include <dm/uclass.h>
+#include <dm/device.h>
+#include <dm/uclass-internal.h>
+#include <dm/device-internal.h>
+#include <dm/lists.h>
+#include <bootm.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+void spl_board_init(void)
+{
+	struct udevice *dev;
+
+	uclass_find_first_device(UCLASS_MISC, &dev);
+
+	for (; dev; uclass_find_next_device(&dev)) {
+		if (device_probe(dev))
+			continue;
+	}
+
+	board_early_init_f();
+
+	timer_init();
+
+	preloader_console_init();
+
+	puts("Normal Boot\n");
+}
+
+void spl_board_prepare_for_boot(void)
+{
+	board_quiesce_devices();
+}
+
+#ifdef CONFIG_SPL_LOAD_FIT
+int board_fit_config_name_match(const char *name)
+{
+	/* Just empty function now - can't decide what to choose */
+	debug("%s: %s\n", __func__, name);
+
+	return 0;
+}
+#endif
+
+void board_init_f(ulong dummy)
+{
+	/* Clear the BSS. */
+	memset(__bss_start, 0, __bss_end - __bss_start);
+
+	arch_cpu_init();
+
+	board_init_r(NULL, 0);
+}
