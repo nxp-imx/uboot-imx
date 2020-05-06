nxp-imx/uboot-imx

commit b81e528dd232b8c2f4d10e89c6bf20c65263b14e

TEE-520 core: imx: Set TZASC region 0 to non-sec
As it is done in ATF for i.MX 8 SoC.
Enable and set region 0 attribute to be allow secure
and non-secure transaction.

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Reviewed-by: Ye Li <ye.li@nxp.com>

arch/arm/mach-imx/misc.c
@@ -107,11 +107,25 @@ void board_lmb_reserve(struct lmb *lmb)
 	}
 }
 
+static void configure_tzc380(void)
+{
+	struct iomuxc *iomux = (struct iomuxc *)IOMUXC_BASE_ADDR;
+#if defined (IP2APB_TZASC1_BASE_ADDR)
+	if (iomux->gpr[9] & 0x1)
+		writel(0xf0000000, IP2APB_TZASC1_BASE_ADDR + 0x108);
+#endif
+#if defined (IP2APB_TZASC2_BASE_ADDR)
+	if (iomux->gpr[9] & 0x2)
+		writel(0xf0000000, IP2APB_TZASC2_BASE_ADDR + 0x108);
+#endif
+}
+
 void imx_sec_init(void)
 {
 #if defined(CONFIG_SPL_BUILD) || !defined(CONFIG_SPL)
 	caam_open();
 #endif
+       configure_tzc380();
 }
 
 static void set_dt_val(void *data, uint32_t cell_size, uint64_t val)

arch/arm/include/asm/arch-mx7/imx-regs.h
@@ -143,7 +143,7 @@
 #define ELCDIF1_IPS_BASE_ADDR           (AIPS2_OFF_BASE_ADDR+0x130000)
 #define MIPI_CSI2_IPS_BASE_ADDR         (AIPS2_OFF_BASE_ADDR+0x150000)
 #define MIPI_DSI_IPS_BASE_ADDR          (AIPS2_OFF_BASE_ADDR+0x160000)
-#define IP2APB_TZASC1_IPS_BASE_ADDR     (AIPS2_OFF_BASE_ADDR+0x180000)
+#define IP2APB_TZASC1_BASE_ADDR     	(AIPS2_OFF_BASE_ADDR+0x180000)
 #define DDRPHY_IPS_BASE_ADDR            (AIPS2_OFF_BASE_ADDR+0x190000)
 #define DDRC_IPS_BASE_ADDR              (AIPS2_OFF_BASE_ADDR+0x1A0000)
 #define IP2APB_PERFMON1_IPS_BASE_ADDR   (AIPS2_OFF_BASE_ADDR+0x1C0000)

arch/arm/mach-imx/mx6/soc.c
@@ -654,19 +654,6 @@ int arch_cpu_init(void)
 			0x3, MX6UL_SNVS_LP_BASE_ADDR);
 	}
 
-	if (is_mx6ull() || is_mx6ul()) {
-		struct iomuxc *iomux = (struct iomuxc *)IOMUXC_BASE_ADDR;
-		if (iomux->gpr[9] & 0x1) {
-			/* When trust zone is enabled,
-			 * set Region 0 attribute to allow secure and non-secure read/write permission
-			 * Because PL301 hard code to non-secure for some masters on m_3/4/5 ports.
-			 * Like LCDIF, PXP, CSI can't work with secure memory.
-			 */
-
-			writel(0xf0000000, IP2APB_TZASC1_BASE_ADDR + 0x108);
-		}
-	}
-
 	/* Set perclk to source from OSC 24MHz */
 	if (is_mx6sl())
 		set_preclk_from_osc();

commit ab7cf304d782f3ab88d37d81ed17a7f388ad4a11

TEE-520 core: imx: fix compilation issue for imx8 and imxrt
Fixes: b81e528dd232 ("TEE-520 core: imx: Set TZASC region 0 to non-sec")
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

arch/arm/mach-imx/misc.c
@@ -109,8 +109,8 @@ void board_lmb_reserve(struct lmb *lmb)
 
 static void configure_tzc380(void)
 {
-	struct iomuxc *iomux = (struct iomuxc *)IOMUXC_BASE_ADDR;
 #if defined (IP2APB_TZASC1_BASE_ADDR)
+	struct iomuxc *iomux = (struct iomuxc *)IOMUXC_BASE_ADDR;
 	if (iomux->gpr[9] & 0x1)
 		writel(0xf0000000, IP2APB_TZASC1_BASE_ADDR + 0x108);
 #endif