6901ffb66e516a3e52d2120522dc1be18de5bafa
Ye Li <ye.li@nxp.com>
2020-05-09 10:50:28 -0700

    MLK-23964-18 imx8mn_evk: Enable video splash screen on DDR4 and LPDDR4 EVK
    Update board codes and DTS files to add display relevant nodes and
    configurations.
    Support two video links with MIPI DSI:
    1. MIPI DSI to HDMI convertor, this is default
    2. RM67191 panel
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- include/configs/imx8mn_evk.h
+++ include/configs/imx8mn_evk.h
@@ -108,6 +108,7 @@
 	JAILHOUSE_ENV \
 	"script=boot.scr\0" \
 	"image=Image\0" \
+	"splashimage=0x50000000\0" \
 	"console=ttymxc1,115200\0" \
 	"fdt_addr=0x43000000\0"			\
 	"fdt_high=0xffffffffffffffff\0"		\
@@ -260,5 +261,18 @@
 
 #define CONFIG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
 #define CONFIG_USB_MAX_CONTROLLER_COUNT         2
+
+#ifdef CONFIG_DM_VIDEO
+#define CONFIG_VIDEO_MXS
+#define CONFIG_VIDEO_LOGO
+#define CONFIG_SPLASH_SCREEN
+#define CONFIG_SPLASH_SCREEN_ALIGN
+#define CONFIG_CMD_BMP
+#define CONFIG_BMP_16BPP
+#define CONFIG_BMP_24BPP
+#define CONFIG_BMP_32BPP
+#define CONFIG_VIDEO_BMP_RLE8
+#define CONFIG_VIDEO_BMP_LOGO
+#endif
 
 #endif
