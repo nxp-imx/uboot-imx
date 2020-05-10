1a93d13ac21ecda0d3a971fea15c613a22293b6e
Ye Li <ye.li@nxp.com>
2020-05-10 06:50:28 -0700

    MLK-23964-25 imx6sll_evk: Enable splash screen
    Enable the splash screen basing on video link
    Update the DTS to fix the bpp in lcdif node and add alias
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 93a53c60a73dab539fdf98542a367e83820df775)
    (cherry picked from commit d10851677db691b5769acd66d9e67fa987dbe655)
    (cherry picked from commit a7c604aba8c65db61c30aa39cb6fe2a1f7029462)
    (cherry picked from commit 4eeb2d8218898424e8be08f3cbc03ba928d572b2)

--- include/configs/mx6sllevk.h
+++ include/configs/mx6sllevk.h
@@ -36,6 +36,7 @@
 	"tee_file=uTee-6sllevk\0" \
 	"boot_fdt=try\0" \
 	"ip_dyn=yes\0" \
+	"splashimage=0x8c000000\0" \
 	"mmcdev="__stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
 	"mmcpart=1\0" \
 	"mmcroot=/dev/mmcblk0p2 rootwait rw\0" \
