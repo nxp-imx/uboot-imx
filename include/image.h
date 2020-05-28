ba8b2c1e6741ea1dcc063f51b136be25a4eef76d
Ji Luo <ji.luo@nxp.com>
2020-05-28 11:06:37 +0800

    MA-17260 Add vendor boot and boot header v3 support
    GKI(Generic Kernel Image) would require the boot header v3 and vendor
    boot support, all device specific info are moved to vendor_boot partition
    ,the boot header v3 will not be compatible with earlier version(0/1/2).
    
    This commit adds support for boot header v3 and vendor boot, it would
    concatenate the generic ramdisk and vendor ramdisk to generate the
    final ramdisk passed to kernel.
    
    Test: boots with or without boot header v3 and vendor boot support.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: Ib3298ae46bfc728aa4a34909d372eff6cc86ca70
    (cherry picked from commit c3854f270a19e7d57b996e6074d692ab9bc88c32)

--- include/image.h
+++ include/image.h
@@ -1451,9 +1451,13 @@ int fit_image_cipher_get_algo(const void *fit, int noffset, char **algo);
 struct cipher_algo *image_get_cipher_algo(const char *full_name);
 
 struct andr_img_hdr;
+struct boot_img_hdr_v3;
+struct vendor_boot_img_hdr_v3;
 int android_image_check_header(const struct andr_img_hdr *hdr);
+int android_image_check_header_v3(const struct boot_img_hdr_v3 *hdr, const struct vendor_boot_img_hdr_v3 *vendor_hdr);
 int android_image_get_kernel(const struct andr_img_hdr *hdr, int verify,
 			     ulong *os_data, ulong *os_len);
+int android_image_get_kernel_v3(const struct boot_img_hdr_v3 *hdr, const struct vendor_boot_img_hdr_v3 *vendor_hdr);
 int android_image_get_ramdisk(const struct andr_img_hdr *hdr,
 			      ulong *rd_data, ulong *rd_len);
 int android_image_get_second(const struct andr_img_hdr *hdr,
