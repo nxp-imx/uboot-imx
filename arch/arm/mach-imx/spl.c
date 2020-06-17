3a9cea328057ba484f8884d32a5e0a6ed5b2b24d
Ji Luo <ji.luo@nxp.com>
2020-06-17 15:44:05 +0800

    MA-17382 spl: Add weak function at the end of image load
    Add a weak function after the image load in case we have extra
    steps to complete, like check the rpmb keyblob for trusty enabled
    platform.
    
    Test: Boots with trusty.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: Ie8181fcb32d1640e39fdcc3cebb2dbf50cbfdbab
    (cherry picked from commit b16fc52cf9a9ea4d63bced50b949d0bc2328ddea)
    (cherry picked from commit 3e40454cdac6b7b63cdff36ab48f86e4b98502be)
    (cherry picked from commit f3fb8740f9aea5a024a2b4579903e78affcda9e1)

--- arch/arm/mach-imx/spl.c
+++ arch/arm/mach-imx/spl.c
@@ -20,6 +20,7 @@
 #include <asm/mach-imx/boot_mode.h>
 #include <g_dnl.h>
 #include <linux/libfdt.h>
+#include <mmc.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -388,3 +389,13 @@ void *spl_load_simple_fit_fix_load(const void *fit)
 
 	return (void *)new;
 }
+
+#ifdef CONFIG_IMX_TRUSTY_OS
+int check_rpmb_blob(struct mmc *mmc);
+
+int mmc_image_load_late(struct mmc *mmc)
+{
+	/* Check the rpmb key blob for trusty enabled platfrom. */
+	return check_rpmb_blob(mmc);
+}
+#endif
