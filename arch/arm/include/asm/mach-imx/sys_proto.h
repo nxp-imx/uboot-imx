40ffcb9cf66ca7a66764c9b27ea317b6f00d3848
Ye Li <ye.li@nxp.com>
2020-06-29 19:58:45 -0700

    MLK-25504-6 imx: Support container image in ROM API download
    Current ROM API only supports FIT image. Add the container image
    support, so that on iMX8ULP we can use ROM API to download container
    image.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>

--- arch/arm/include/asm/mach-imx/sys_proto.h
+++ arch/arm/include/asm/mach-imx/sys_proto.h
@@ -146,7 +146,7 @@ struct rproc_att {
 	u32 size; /* size of reg range */
 };
 
-#ifdef CONFIG_IMX8M
+#if defined(CONFIG_IMX8M) || defined(CONFIG_IMX8ULP)
 struct rom_api {
 	u16 ver;
 	u16 tag;
@@ -177,6 +177,9 @@ enum boot_dev_type_e {
 #define ROM_API_OKAY		0xF0
 
 extern struct rom_api *g_rom_api;
+
+ulong spl_romapi_raw_seekable_read(u32 offset, u32 size,
+                              void *buf);
 #endif
 
 /* For i.MX ULP */
