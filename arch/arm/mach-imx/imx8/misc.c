27899e098e287bc661fa421d287a0c78aaae4621
Ye Li <ye.li@nxp.com>
2020-07-20 01:55:43 -0700

    MLK-24426-2 iMX8DXL: Print the V2X FW build info
    Print the version and commit of V2X firmware in u-boot log when
    SCFW has supported this new API.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>
    (cherry picked from commit 912635be68baa1f418ab82833e73e7e7ce03b8c4)

--- arch/arm/mach-imx/imx8/misc.c
+++ arch/arm/mach-imx/imx8/misc.c
@@ -39,6 +39,10 @@ int sc_pm_setup_uart(sc_rsrc_t uart_rsrc, sc_pm_clock_rate_t clk_rate)
 #define FSL_SIP_BUILDINFO_GET_COMMITHASH	0x00
 extern uint32_t _end_ofs;
 
+#define V2X_PROD_VER(X)      (((X) >> 16) & 0x7FFF)
+#define V2X_MAJOR_VER(X)     (((X) >> 4) & 0xFFF)
+#define V2X_MINOR_VER(X)     ((X) & 0xF)
+
 static void set_buildinfo_to_env(uint32_t scfw, uint32_t secofw, char *mkimage, char *atf)
 {
 	if (!mkimage || !atf)
@@ -50,12 +54,19 @@ static void set_buildinfo_to_env(uint32_t scfw, uint32_t secofw, char *mkimage, 
 	env_set_hex("commit_secofw", (ulong)secofw);
 }
 
+static void set_v2x_buildinfo_to_env(u32 v2x_build, u32 v2x_commit)
+{
+	env_set_hex("commit_v2x", (ulong)v2x_commit);
+	env_set_hex("version_v2x", (ulong)v2x_build);
+}
+
 void build_info(void)
 {
 	u32 seco_build = 0, seco_commit = 0;
 	u32 sc_build = 0, sc_commit = 0;
 	char *mkimage_commit, *temp;
 	ulong atf_commit = 0;
+	u32 v2x_build = 0, v2x_commit = 0;
 
 	/* Get SCFW build and commit id */
 	sc_misc_build_info(-1, &sc_build, &sc_commit);
@@ -70,6 +81,17 @@ void build_info(void)
 		debug("SECO FW does not support build info\n");
 		/* Display 0 when the build info is not supported */
 		seco_commit = 0;
+	}
+
+	if (is_imx8dxl()) {
+		int ret;
+		ret = sc_seco_v2x_build_info(-1, &v2x_build, &v2x_commit);
+		if (ret) {
+			debug("Failed to get V2X FW build info\n");
+			/* Display 0 when the build info is not supported */
+			v2x_build = 0;
+			v2x_commit = 0;
+		}
 	}
 
 	/* Get imx-mkimage commit id.
@@ -96,8 +118,15 @@ void build_info(void)
 	/* Set all to env */
 	set_buildinfo_to_env(sc_commit, seco_commit, mkimage_commit, (char *)&atf_commit);
 
-	printf("\n BuildInfo: \n  - SCFW %08x, SECO-FW %08x, IMX-MKIMAGE %s, ATF %s\n  - %s \n\n",
+	printf("\n BuildInfo: \n  - SCFW %08x, SECO-FW %08x, IMX-MKIMAGE %s, ATF %s\n  - %s \n",
 		sc_commit, seco_commit, mkimage_commit, (char *)&atf_commit, U_BOOT_VERSION);
+
+	if (is_imx8dxl() && v2x_build != 0 && v2x_commit != 0) {
+		set_v2x_buildinfo_to_env(v2x_build, v2x_commit);
+		printf("  - V2X-FW %08x version %u.%u.%u\n", v2x_commit,
+			V2X_PROD_VER(v2x_build), V2X_MAJOR_VER(v2x_build), V2X_MINOR_VER(v2x_build));
+	}
+	printf("\n");
 }
 
 #if !defined(CONFIG_SPL_BUILD) && defined(CONFIG_PSCI_BOARD_REBOOT)
