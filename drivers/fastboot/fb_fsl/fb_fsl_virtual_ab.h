3ba11d20069f9d01b25cf8c7b3e9c47af3467cde
Ji Luo <ji.luo@nxp.com>
2020-07-22 19:03:32 +0800

    MA-17541-1 Support virtual A/B update
    A 'misc_virtual_ab_message' struct will be stored at the 32kB offset
    in misc partition, which will be used to record the virtual A/B update
    status.
    
    Bootloader should take care of this status, some operations must be
    restricted. This commit will:
        1. Restrict erase/flash operations to "misc", "userdata" or
           "metadata" partitions if the merge status are "SNAPSHOTTED" or
           "MERGING".
        2. Restrict slot switch if the merge status is "MERGING".
        3. Output a warning in slot switch if the merge status is "SNAPSHOTTED".
        4. Set the merge status as "CANCELLED" if image flash happen.
    
    Test: 1. fastboot erase/flash "userdata", "misc", "metadata" after virtual
            A/B update
          2. slot switch after virtual A/B update
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: I33f0041c5e76913d3970d943cad52353e0ac5f2d
    (cherry picked from commit 30df087bfc5e31413473f85dfefaa7176bc394a8)
    (cherry picked from commit 7fd03bcc8f1fc094362c11d71cf740ac9b5724c1)
    (cherry picked from commit 97495e9189afc4a26e6e03fd8ac7389f445c590e)

---
+++ drivers/fastboot/fb_fsl/fb_fsl_virtual_ab.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright 2020 NXP
+ */
+
+typedef enum {
+	VIRTUAL_AB_NONE = 0,
+	VIRTUAL_AB_UNKNOWN,
+	VIRTUAL_AB_SNAPSHOTTED,
+	VIRTUAL_AB_MERGING,
+	VIRTUAL_AB_CANCELLED,
+} Virtual_AB_Status;
+
+bool partition_is_protected_during_merge(char *part);
+bool virtual_ab_update_is_merging(void);
+bool virtual_ab_update_is_snapshoted(void);
+int virtual_ab_cancel_update(void);
