7e74dff704acba6390906e016c8a973d1ea5bd2f
Ji Luo <ji.luo@nxp.com>
2020-07-30 21:15:55 +0800

    MA-17570 Show unlock warning logo before AVB sevice is closed
    The unlock warning logo show will query the device lock status
    via AVB tipc (if trusty is enabled). Show the unlock warning
    logo before AVB tipc channel is closed, this will fix below
    warning:
      avb.c:90: ERROR avb_do_tipc: AVB TIPC client not initialized
    
    Test: boots with trusty.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: I9fa675b72193d7d618ddf2d0d813b97a3073bfea

--- drivers/fastboot/fb_fsl/fb_fsl_boot.c
+++ drivers/fastboot/fb_fsl/fb_fsl_boot.c
@@ -869,6 +869,21 @@ int do_boota(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]) {
 		boot_args[2] = NULL;
 #endif
 
+	/* Show orange warning for unlocked device, press power button to skip. */
+#ifdef CONFIG_AVB_WARNING_LOGO
+	if (fastboot_get_lock_stat() == FASTBOOT_UNLOCK) {
+		int count = 0;
+
+		printf("Device is unlocked, press power key to skip warning logo... \n");
+		if (display_unlock_warning())
+			printf("can't show unlock warning.\n");
+		while ( (count < 10 * CONFIG_AVB_WARNING_TIME_LAST) && !is_power_key_pressed()) {
+			mdelay(100);
+			count++;
+		}
+	}
+#endif
+
 	/* Trusty related operations */
 #ifdef CONFIG_IMX_TRUSTY_OS
 	/* Trusty keymaster needs some parameters before it work */
@@ -886,21 +901,6 @@ int do_boota(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[]) {
 	hwcrypto_lock_boot_state();
 	/* put ql-tipc to release resource for Linux */
 	trusty_ipc_shutdown();
-#endif
-
-	/* Show orange warning for unlocked device, press power button to skip. */
-#ifdef CONFIG_AVB_WARNING_LOGO
-	if (fastboot_get_lock_stat() == FASTBOOT_UNLOCK) {
-		int count = 0;
-
-		printf("Device is unlocked, press power key to skip warning logo... \n");
-		if (display_unlock_warning())
-			printf("can't show unlock warning.\n");
-		while ( (count < 10 * CONFIG_AVB_WARNING_TIME_LAST) && !is_power_key_pressed()) {
-			mdelay(100);
-			count++;
-		}
-	}
 #endif
 
 	/* Free AVB data */
