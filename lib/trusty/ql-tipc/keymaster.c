688ab2803c70dbbaf71ea16cf0866e832935c360
Ji Luo <ji.luo@nxp.com>
2020-08-03 22:44:11 +0800

    MA-20507-4 trusty: Fix extra argument to printf format specifier
    Fix Coverity Issue 10473659 and 10473672. Pass correct parameter
    to fix the extra argument issue.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: I72e30250827ad0c48e079a7048bdb40773c17a96
    (cherry picked from commit ed061b4a21a24d8deb36561e0bfe2343e2ce45c8)

--- lib/trusty/ql-tipc/keymaster.c
+++ lib/trusty/ql-tipc/keymaster.c
@@ -247,7 +247,7 @@ static int km_get_version(int32_t *version)
 
     rc = km_send_request(KM_GET_VERSION, NULL, 0);
     if (rc < 0) {
-        trusty_error("failed to send km version request", rc);
+        trusty_error("failed (%d) to send km version request", rc);
         return rc;
     }
 
@@ -524,7 +524,7 @@ int trusty_get_mppubk(uint8_t *mppubk, uint32_t *size)
 
     rc = km_send_request(KM_GET_MPPUBK, NULL, 0);
     if (rc < 0) {
-        trusty_error("failed to send km mppubk request\n", rc);
+        trusty_error("%s: failed (%d) to send km mppubk request\n", __func__, rc);
         return rc;
     }
 
