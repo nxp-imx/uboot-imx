nxp-imx/uboot-imx

commit 7c6a45e9efa348205f8f5fab63e29d69aeadd762

PCI: layerscape: Add the PCIe EP mode support for lx2160a-v2
Add the PCIe EP mode support for lx2160a-v2 platform.

Signed-off-by: Xiaowei Bao <xiaowei.bao@nxp.com>
Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

drivers/pci/pcie_layerscape.h
@@ -100,7 +100,7 @@
 #define PCIE_SRIOV_VFBAR0	0x19C
 #define PCIE_MISC_CONTROL_1_OFF	0x8BC
 
-#define PCIE_MASK_OFFSET(flag, pf) ((flag) ? 0 : (0x1000 + 0x20000 * (pf)))
+#define PCIE_MASK_OFFSET(flag, pf, off) ((flag) ? 0 : (0x1000 + (off) * (pf)))
 
 /* LUT registers */
 #define PCIE_LUT_UDR(n)		(0x800 + (n) * 8)
@@ -139,6 +139,12 @@
 #define LS1021_PEXMSCPORTSR(pex_idx)	(0x94 + (pex_idx) * 4)
 #define LS1021_LTSSM_STATE_SHIFT	20
 
+/* LX2160a PF1 offset */
+#define LX2160_PCIE_PF1_OFFSET	0x8000
+
+/* layerscape PF1 offset */
+#define LS_PCIE_PF1_OFFSET	0x20000
+
 struct ls_pcie {
 	int idx;
 	struct list_head list;
@@ -157,6 +163,7 @@ struct ls_pcie {
 	int next_lut_index;
 	uint sriov_flag;
 	uint cfg2_flag;
+	uint pf1_offset;
 	int stream_id_cur;
 	int mode;
 };

drivers/pci/pcie_layerscape.c
@@ -470,7 +470,8 @@ static void ls_pcie_setup_ep(struct ls_pcie *pcie)
 			writel(0, pcie->dbi + PCIE_MISC_CONTROL_1_OFF);
 
 			bar_base = pcie->dbi +
-				   PCIE_MASK_OFFSET(pcie->cfg2_flag, pf);
+				   PCIE_MASK_OFFSET(pcie->cfg2_flag, pf,
+						    pcie->pf1_offset);
 
 			if (pcie->cfg2_flag) {
 				ctrl_writel(pcie,
@@ -581,6 +582,11 @@ static int ls_pcie_probe(struct udevice *dev)
 		pcie->cfg_res.end = pcie->cfg_res.start + cfg_size;
 		pcie->ctrl = pcie->lut + 0x40000;
 	}
+
+	if (svr == SVR_LX2160A)
+		pcie->pf1_offset = LX2160_PCIE_PF1_OFFSET;
+	else
+		pcie->pf1_offset = LS_PCIE_PF1_OFFSET;
 
 	if (svr == SVR_LS2080A || svr == SVR_LS2085A)
 		pcie->cfg2_flag = 1;

commit 036827ebdb753c603bcd6747249530baf7344d47

PCI: layerscape: Fix build errors on LS1021A platforms
Fix the following build errors:
drivers/pci/pcie_layerscape.c: In function ‘ls_pcie_probe’:
drivers/pci/pcie_layerscape.c:586:13: error: ‘SVR_LX2160A’ undeclared (first use in this function)
  586 |  if (svr == SVR_LX2160A)
      |             ^~~~~~~~~~~
drivers/pci/pcie_layerscape.c:586:13: note: each undeclared identifier is reported only once for each function it appears in
drivers/pci/pcie_layerscape.c:591:13: error: ‘SVR_LS2080A’ undeclared (first use in this function); did you mean ‘SVR_LS2088A’?
  591 |  if (svr == SVR_LS2080A || svr == SVR_LS2085A)
      |             ^~~~~~~~~~~
      |             SVR_LS2088A
drivers/pci/pcie_layerscape.c:591:35: error: ‘SVR_LS2085A’ undeclared (first use in this function); did you mean ‘SVR_LS2088A’?
  591 |  if (svr == SVR_LS2080A || svr == SVR_LS2085A)
      |                                   ^~~~~~~~~~~
      |                                   SVR_LS2088A

Fixes: 7c6a45e9efa3 ("PCI: layerscape: Add the PCIe EP mode support for lx2160a-v2")
Fixes: 98a564f35ef8 ("PCI: layerscape: Add the configuration for PF1 and VFs")
Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

drivers/pci/pcie_layerscape.h
@@ -124,12 +124,15 @@
 #define SVR_LS102XA		0
 #define SVR_VAR_PER_SHIFT	8
 #define SVR_LS102XA_MASK	0x700
+#define SVR_LS2080A		0x870110
+#define SVR_LS2085A		0x870100
 #define SVR_LS2088A		0x870900
 #define SVR_LS2084A		0x870910
 #define SVR_LS2048A		0x870920
 #define SVR_LS2044A		0x870930
 #define SVR_LS2081A		0x870918
 #define SVR_LS2041A		0x870914
+#define SVR_LX2160A		0x873600
 
 /* LS1021a PCIE space */
 #define LS1021_PCIE_SPACE_OFFSET	0x4000000000ULL