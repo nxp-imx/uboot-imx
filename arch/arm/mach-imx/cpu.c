05af9ac08052c92d011908726534e227db3143c4
Ye Li <ye.li@nxp.com>
2020-09-22 20:51:41 -0700

    MLK-25003-1 iMX8MN: Add support for 11x11 UltraLite part number
    There are 3 part numbers for 11x11 i.MX8MNano with different core number
    configuration: UltraLite Quad/Dual/Solo
    
    Comparing with i.MX8MN Lite parts, they have MIPI DSI disabled. So
    checking the MIPI DSI disable fuse to recognize these parts.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/mach-imx/cpu.c
+++ arch/arm/mach-imx/cpu.c
@@ -132,6 +132,12 @@ const char *get_imx_type(u32 imxtype)
 		return "8MNano DualLite";/* Dual-core Lite version of the imx8mn */
 	case MXC_CPU_IMX8MNSL:
 		return "8MNano SoloLite";/* Single-core Lite version of the imx8mn */
+	case MXC_CPU_IMX8MNUQ:
+		return "8MNano UltraLite Quad";/* Quad-core UltraLite version of the imx8mn */
+	case MXC_CPU_IMX8MNUD:
+		return "8MNano UltraLite Dual";/* Dual-core UltraLite version of the imx8mn */
+	case MXC_CPU_IMX8MNUS:
+		return "8MNano UltraLite Solo";/* Single-core UltraLite version of the imx8mn */
 	case MXC_CPU_IMX8MM:
 		return "8MMQ";	/* Quad-core version of the imx8mm */
 	case MXC_CPU_IMX8MML:
