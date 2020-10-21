835ceea1b5c730ffd7b51d1280234d9b0269198e
Ji Luo <ji.luo@nxp.com>
2020-10-21 10:52:21 +0800

    MA-18087-2 Support snapshot-update-status variable
    Add support for 'fastboot getvar snapshot-update-status', it
    returns the snapshot update states.
    
    Test: run 'fastboot getvar snapshot-update-status'
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Change-Id: Ifd46410994b11c327373a35eb86d121f321e39ae
    (cherry picked from commit 28c6cbec7fe9e613620e5d80b7b43d1ca6bdd9c6)
    (cherry picked from commit dece52d012e8c6e06f96f1ad34b47f5da6514d2e)

--- drivers/fastboot/fb_fsl/fb_fsl_virtual_ab.h
+++ drivers/fastboot/fb_fsl/fb_fsl_virtual_ab.h
@@ -3,6 +3,9 @@
  * Copyright 2020 NXP
  */
 
+#ifndef __FB_FSL_VIRTUAL_AB_H__
+#define __FB_FSL_VIRTUAL_AB_H__
+
 typedef enum {
 	VIRTUAL_AB_NONE = 0,
 	VIRTUAL_AB_UNKNOWN,
@@ -16,3 +19,5 @@ bool virtual_ab_update_is_merging(void);
 bool virtual_ab_update_is_snapshoted(void);
 bool virtual_ab_slot_match(void);
 int virtual_ab_cancel_update(void);
+
+#endif
