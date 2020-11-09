0600a61b1c21bca06b4bbdae28448944bb0a09b3
Ye Li <ye.li@nxp.com>
2020-11-09 00:52:53 -0800

    LFU-4 imx8qm_mek: Select schedutil cpufreq governor as default
    Add "cpufreq.default_governor=SCHEDUTIL" to iMX8QM MEK bootargs, so that
    v5.10 kernel can uses schedutil cpufreq governor as default.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Anson Huang <Anson.Huang@nxp.com>

--- include/configs/imx8qm_mek.h
+++ include/configs/imx8qm_mek.h
@@ -178,7 +178,8 @@
 	"mmcpart=" __stringify(CONFIG_SYS_MMC_IMG_LOAD_PART) "\0" \
 	"mmcroot=" CONFIG_MMCROOT " rootwait rw\0" \
 	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs console=${console},${baudrate} earlycon root=${mmcroot}\0 " \
+	"mmcargs=setenv bootargs console=${console},${baudrate} earlycon root=${mmcroot} " \
+		"cpufreq.default_governor=SCHEDUTIL\0" \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
@@ -215,7 +216,8 @@
 		"fi;\0" \
 	"netargs=setenv bootargs console=${console},${baudrate} earlycon " \
 		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
+		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp " \
+		"cpufreq.default_governor=SCHEDUTIL\0" \
 	"netboot=echo Booting from net ...; " \
 		"run netargs;  " \
 		"if test ${ip_dyn} = yes; then " \
