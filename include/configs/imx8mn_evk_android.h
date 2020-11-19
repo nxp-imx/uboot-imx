d65fed26a397aaee3c7de83d845bd84c59a8cdf2
Ji Luo <ji.luo@nxp.com>
2020-11-19 16:23:51 +0800

    MA-18299 Fix imx8mn flash panic
    TEE memory region are moved to 0x56000000 ~ 0x58000000 to
    support i.MX 8MN DDR3L board:
    
        TEE-619 imx: 8mn: add support for i.MX 8MN DDR3L EVK
    
        DD3L EVK board only has 512MB of DDR.
        move OP-TEE mapping for all the 8MN boards.
    
        Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
    
    Adjust the fastboot buffer region to avoid conflicts.
    
    Test: Super partition flash on imx8mn.
    
    Change-Id: I56bd0194f24f9f267f436b4b6d762948d96bf815
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- include/configs/imx8mn_evk_android.h
+++ include/configs/imx8mn_evk_android.h
@@ -15,7 +15,7 @@
 
 #ifdef CONFIG_SYS_MALLOC_LEN
 #undef CONFIG_SYS_MALLOC_LEN
-#define CONFIG_SYS_MALLOC_LEN           (64 * SZ_1M)
+#define CONFIG_SYS_MALLOC_LEN           (32 * SZ_1M)
 #endif
 
 #undef CONFIG_EXTRA_ENV_SETTINGS
