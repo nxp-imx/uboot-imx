15e4a9ec8b94842358a1443ca242339d04a10938
Ye Li <ye.li@nxp.com>
2021-03-09 01:30:34 -0800

    LF-3483-1 imx8mp_evk: Allow to use BSS section in board_init_f
    The SPL framework will clean up the BSS section after board_init_f,
    since we have explicitily clean BSS in board_init_f, so directly
    call to board_init_r to bypass framework
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit a9e0b361c1d83a434d7a52e9b40f418bc3043807)

--- board/freescale/imx8mp_evk/spl.c
+++ board/freescale/imx8mp_evk/spl.c
@@ -116,7 +116,6 @@ int board_fit_config_name_match(const char *name)
 }
 #endif
 
-/* Do not use BSS area in this phase */
 void board_init_f(ulong dummy)
 {
 	int ret;
@@ -144,4 +143,6 @@ void board_init_f(ulong dummy)
 
 	/* DDR initialization */
 	spl_dram_init();
+
+	board_init_r(NULL, 0);
 }
