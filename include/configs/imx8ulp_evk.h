e0af80f599514f0c1897f7ec9606c93cfb55fe3a
Ji Luo <ji.luo@nxp.com>
2021-04-21 14:49:38 +0800

    MA-19091 android: add android support for 8ulp
    Add configs to support android on imx8ulp.
    
    Test: Boots on imx8ulp.
    
    Change-Id: I3fdc977b5e88b310b835e75f6eafd7b0a9e84821
    Signed-off-by: Ji Luo <ji.luo@nxp.com>

--- include/configs/imx8ulp_evk.h
+++ include/configs/imx8ulp_evk.h
@@ -177,4 +177,9 @@
 /* USB Configs */
 #define CONFIG_USB_MAX_CONTROLLER_COUNT 2
 #define CONFIG_MXC_USB_PORTSC  (PORT_PTS_UTMI | PORT_PTS_PTW)
+
+#ifdef CONFIG_ANDROID_SUPPORT
+#include "imx8ulp_evk_android.h"
+#endif
+
 #endif
