f8cba1b10417d59187ab80cb7dd5d7d4e17c7ae9
Peng Fan <peng.fan@nxp.com>
2021-07-13 14:35:58 +0800

    LF-3892-5 imx8mn_evk: update defconfig and code for SR-IR
    Update defconfig and code for SR-IR
    
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- board/freescale/imx8mn_evk/imx8mn_evk.c
+++ board/freescale/imx8mn_evk/imx8mn_evk.c
@@ -304,6 +304,9 @@ int board_init(void)
 
 #ifdef CONFIG_USB_TCPC
 	setup_typec();
+
+	/* Enable Power by default for SR-IR usage */
+	imx8m_usb_power(0, true);
 #endif
 
 	if (IS_ENABLED(CONFIG_FEC_MXC))
