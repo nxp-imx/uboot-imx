dc3adcc2588da1a405e471f259d14f22957b2565
Gaurav Jain <gaurav.jain@nxp.com>
2021-07-26 18:50:01 +0530

    LFU-192-4 crypto/fsl: Add support for HW accelerated AES ECB decryption
    added function and caam descriptor for aes ecb decrypt.
    Output of aes-256-ecb operation is a black key.
    
    Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>
    Reviewed-by: Horia Geantă <horia.geanta@nxp.com>

--- include/fsl_sec.h
+++ include/fsl_sec.h
@@ -419,6 +419,8 @@ int derive_blob_kek(u8 *bkek_buf, u8 *key_mod, u32 key_sz);
 
 int hwrng_generate(u8 *dst, u32 len);
 
+int aesecb_decrypt(u8 *key, u32 key_len, u8 *src, u8 *dst, u32 len);
+
 int tag_black_obj(u8 *black_obj, size_t black_obj_len, size_t key_len, size_t black_max_len);
 #endif
 
