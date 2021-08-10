222f12642c62f70558b07456348e638d5723fcb6
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2021-08-10 14:28:45 +0800

    configs: Layerscape: Add 'fdt_addr' env with DRAM address
    To work around distro boot issue caused by the EFI boot process
    change (refer to LFU-189), add a 'fdt_addr' env pointing to
    a DRAM address, in which there isn't valid DTB, to let bootefi
    get invalid DTB and then skip EFI boot.
    
    This is a workaround patch, remove it when the EFI boot feature
    has been supported or the U-Boot DTB has been modified to
    compatibile with Linux.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

--- include/configs/ls1088ardb.h
+++ include/configs/ls1088ardb.h
@@ -256,6 +256,7 @@
 	"kernel_addr_r=0x81000000\0"		\
 	"kernelheader_size=0x40000\0"		\
 	"fdt_addr_r=0x90000000\0"		\
+	"fdt_addr=0x90000000\0"                 \
 	"load_addr=0xa0000000\0"		\
 	"kernel_size=0x2800000\0"		\
 	"kernel_size_sd=0x14000\0"		\
@@ -324,6 +325,7 @@
 	"kernel_addr_r=0x81000000\0"		\
 	"kernelheader_size=0x40000\0"		\
 	"fdt_addr_r=0x90000000\0"		\
+	"fdt_addr=0x90000000\0"                 \
 	"load_addr=0xa0000000\0"		\
 	"kernel_size=0x2800000\0"		\
 	"kernel_size_sd=0x14000\0"		\
