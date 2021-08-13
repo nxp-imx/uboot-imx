f32456715d7b32643ff831f18cb664b455204d84
Ji Luo <ji.luo@nxp.com>
2021-08-13 16:02:41 +0800

    MA-19331 imx8m: skip JR probe for android in uboot
    As JR0 is reserved to secure world for HAB (secure boot),
    commit 'ed618ce6a9 (LFU-219)' switch to use JR1 in uboot.
    But this will cause conflict with Android as JR1 is
    assigned to secure world for Trusty OS.
    
    Since android doesn't use CAAM in uboot so we just skip the
    JR probe in uboot to avoid conflicts.
    
    Test: Boots on imx8mp.
    
    Signed-off-by: Ji Luo <ji.luo@nxp.com>
    Reviewed-by: Gaurav Jain <gaurav.jain@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 8fdf9c32919e3a4ea671d47c19819321c3c09c9b)
    
    Change-Id: Ic79a54f90ecf8ed55140fc45b731716664180fd5

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -1775,6 +1775,7 @@ void reset_cpu(void)
 #if defined(CONFIG_ARCH_MISC_INIT)
 int arch_misc_init(void)
 {
+#ifndef CONFIG_ANDROID_SUPPORT
 	if (IS_ENABLED(CONFIG_FSL_CAAM)) {
 		struct udevice *dev;
 		int ret;
@@ -1783,6 +1784,7 @@ int arch_misc_init(void)
 		if (ret)
 			printf("Failed to initialize caam_jr: %d\n", ret);
 	}
+#endif
 
 	return 0;
 }
