030b02d3fd02e9cdf66bad6a2440348bf267120e
Ye Li <ye.li@nxp.com>
2021-10-21 15:10:04 +0800

    LFU-119 imx8qm/qxp: Fix Unchecked return value
    Fix coverity issue 10045169, add return value check to tcpc_init
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- board/freescale/imx8qm_mek/imx8qm_mek.c
+++ board/freescale/imx8qm_mek/imx8qm_mek.c
@@ -335,7 +335,11 @@ static void setup_typec(void)
 	/* Enable SS MUX */
 	dm_gpio_set_dir_flags(&typec_en_desc, GPIOD_IS_OUT | GPIOD_IS_OUT_ACTIVE);
 
-	tcpc_init(&port, port_config, &ss_mux_select);
+	ret = tcpc_init(&port, port_config, &ss_mux_select);
+	if (ret) {
+		printf("%s: tcpc init failed, err=%d\n", __func__, ret);
+		return;
+	}
 }
 #endif
 
