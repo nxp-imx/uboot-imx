ed0ed595417d436bee213aad1bd1702df55a1d6a
Frank Li <Frank.Li@nxp.com>
2022-01-19 15:45:51 -0600

    MLK-25820 fat_write: fix wrong short name check when no ext
    if use fat_write write filename qnx-lfs, fat_write think it is
    long filename, short name change to QNX-LFS~1, which cause qnx
    boot failure.
    
    Signed-off-by: Frank Li <Frank.Li@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 927a9bcc8bece2e4e5522f605784174ec3857b3a)

--- fs/fat/fat_write.c
+++ fs/fat/fat_write.c
@@ -136,7 +136,7 @@ static int set_name(fat_itr *itr, const char *filename, char *shortname)
 
 	/* If filename and short name are the same, quit. */
 	sprintf(buf, "%.*s.%.3s", period_location, dirent.name, dirent.ext);
-	if (!strcmp(buf, filename)) {
+	if (!strncmp(buf, filename, strlen(filename))) {
 		ret = 1;
 		goto out;
 	}
