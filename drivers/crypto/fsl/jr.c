nxp-imx/uboot-imx

commit 1097bb9155b9689eb4a84179c1683cff724f8397

LFU-287 i.MX6SX: Update entropy delay range(min-max)
Issue: RNG Hardware error observed as below.
1. caam_jr 2102000.jr: 20003c5b: CCB: desc idx 60: RNG: Hardware error
   hwrng: no data available

2. HAB failure generated for RNG Hardware error.
{{--------- HAB Event 2 -----------------
event data:
0xdb 0x00 0x24 0x42 0x33 0x30 0xc0 0x1d
0x00 0x08 0x00 0x02 0x20 0x00 0x3c 0x5b(20003c5b)
0x55 0x55 0x00 0x0f 0x00 0x00 0x00 0x00
0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00
0x00 0x00 0x02 0x05
STS = HAB_FAILURE (0x33)
RSN = HAB_ENG_FAIL (0x30)
CTX = HAB_CTX_COMMAND (0xC0)
ENG = HAB_ENG_CAAM (0x1D)}}
{{}}

Root cause:
CAAM TRNG initialization fail with a low entropy delay(3200)

Fix:
self test are run to determine entropy delay.
Based on room temperature, default volage,
(10000-11200) is a good entropy delay for i.MX6SX.

Signed-off-by: Frank Zhang <frank.zhang@nxp.com>
Reviewed-by: Ye Li <ye.li@nxp.com>

include/fsl_sec.h
@@ -58,8 +58,13 @@ struct rng4tst {
 	u32 rtmctl;		/* misc. control register */
 	u32 rtscmisc;		/* statistical check misc. register */
 	u32 rtpkrrng;		/* poker range register */
+#ifdef CONFIG_MX6SX
+#define RTSDCTL_ENT_DLY_MIN	10000
+#define RTSDCTL_ENT_DLY_MAX	11200
+#else
 #define RTSDCTL_ENT_DLY_MIN	3200
 #define RTSDCTL_ENT_DLY_MAX	12800
+#endif
 	union {
 		u32 rtpkrmax;	/* PRGM=1: poker max. limit register */
 		u32 rtpkrsq;	/* PRGM=0: poker square calc. result register */

commit d942c221b7fb735cbbe664861cb4e6fa2ae7aeb7

LFU-287 i.MX6SX: crypto/fsl: fix entropy delay value
RNG Hardware error is reported due to incorrect entropy delay

rng self test are run to determine the correct ent_dly.
test is executed with different voltage and temperature to identify the
worst case value for ent_dly. after adding a margin value(1000),
ent_dly should be at least 12000.

Fixes: 1097bb9155 (LFU-287 i.MX6SX: Update entropy delay range(min-max))
Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>
Reviewed-by: Varun Sethi <v.sethi@nxp.com>
Reviewed-by: Vabhav Sharma <vabhav.sharma@nxp.com>

drivers/crypto/fsl/jr.c
@@ -740,7 +740,7 @@ static void kick_trng(int ent_delay, ccsr_sec_t *sec)
 
 static int rng_init(uint8_t sec_idx, ccsr_sec_t *sec)
 {
-	int ret, gen_sk, ent_delay = RTSDCTL_ENT_DLY_MIN;
+	int ret, gen_sk, ent_delay = RTSDCTL_ENT_DLY;
 	struct rng4tst __iomem *rng =
 			(struct rng4tst __iomem *)&sec->rng;
 	u32 inst_handles;
@@ -769,6 +769,15 @@ static int rng_init(uint8_t sec_idx, ccsr_sec_t *sec)
 		 * the RNG.
 		 */
 		ret = instantiate_rng(sec_idx, sec, gen_sk);
+		/*
+		 * entropy delay is calculated via self-test method.
+		 * self-test are run across different volatge, temp.
+		 * if worst case value for ent_dly is identified,
+		 * loop can be skipped for that platform.
+		 */
+		if (IS_ENABLED(CONFIG_MX6SX))
+			break;
+
 	} while ((ret == -1) && (ent_delay < RTSDCTL_ENT_DLY_MAX));
 	if (ret) {
 		printf("SEC%u:  Failed to instantiate RNG\n", sec_idx);

include/fsl_sec.h
@@ -59,12 +59,11 @@ struct rng4tst {
 	u32 rtscmisc;		/* statistical check misc. register */
 	u32 rtpkrrng;		/* poker range register */
 #ifdef CONFIG_MX6SX
-#define RTSDCTL_ENT_DLY_MIN	10000
-#define RTSDCTL_ENT_DLY_MAX	11200
+#define RTSDCTL_ENT_DLY		12000
 #else
-#define RTSDCTL_ENT_DLY_MIN	3200
-#define RTSDCTL_ENT_DLY_MAX	12800
+#define RTSDCTL_ENT_DLY		3200
 #endif
+#define RTSDCTL_ENT_DLY_MAX	12800
 	union {
 		u32 rtpkrmax;	/* PRGM=1: poker max. limit register */
 		u32 rtpkrsq;	/* PRGM=0: poker square calc. result register */