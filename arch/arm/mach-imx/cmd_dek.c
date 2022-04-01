6ea31052ff262031dd04a8e31f133fd12708dcdd
Ye Li <ye.li@nxp.com>
2022-04-01 15:11:55 +0800

    LFU-293 imx: cmd_dek: Fix Uninitialized pointer read
    Fix coverity Issue: 21143558 Uninitialized pointer read.
    When tee_shm_register returns failure, the shm_input pointer is
    invalid, should not free it. Same issue also exists on registering
    shm_output.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- arch/arm/mach-imx/cmd_dek.c
+++ arch/arm/mach-imx/cmd_dek.c
@@ -101,6 +101,7 @@ static int blob_encap_dek(uint32_t src_addr, uint32_t dst_addr, uint32_t len)
 			       0x0, &shm_output);
 	if (ret < 0) {
 		printf("Cannot register output shared memory 0x%X\n", ret);
+		tee_shm_free(shm_input);
 		goto error;
 	}
 
@@ -122,11 +123,11 @@ static int blob_encap_dek(uint32_t src_addr, uint32_t dst_addr, uint32_t len)
 	if (ret < 0)
 		printf("Cannot generate Blob with PTA DEK Blob 0x%X\n", ret);
 
-error:
 	/* Free shared memory */
 	tee_shm_free(shm_input);
 	tee_shm_free(shm_output);
 
+error:
 	/* Close session */
 	ret = tee_close_session(dev, arg.session);
 	if (ret < 0)
