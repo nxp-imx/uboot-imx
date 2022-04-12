fa705c54a55fcc65d61467751a67097d50257a74
Ye Li <ye.li@nxp.com>
2022-04-12 09:47:02 +0800

    LFU-314 caam: fsl_rsa: Fix dcache issue in the driver
    Since the keys and output decrypted data are processed by CAAM master,
    we need to flush and invalidate the dcache for them, otherwise CAAM JR
    returns failure with key error.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Gaurav Jain <gaurav.jain@nxp.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>

--- drivers/crypto/fsl/fsl_rsa.c
+++ drivers/crypto/fsl/fsl_rsa.c
@@ -36,11 +36,19 @@ int fsl_mod_exp(struct udevice *dev, const uint8_t *sig, uint32_t sig_len,
 
 	inline_cnstr_jobdesc_pkha_rsaexp(desc, &pkin, out, sig_len);
 
+	flush_dcache_range((ulong)sig, (ulong)(sig + sig_len));
+	flush_dcache_range((ulong)prop->modulus, (ulong)(prop->modulus) + keylen);
+	flush_dcache_range((ulong)prop->public_exponent, (ulong)(prop->public_exponent) + prop->exp_len);
+	flush_dcache_range((ulong)desc, (ulong)desc + (sizeof(uint32_t) * MAX_CAAM_DESCSIZE));
+	flush_dcache_range((ulong)out, (ulong)out + sig_len);
+
 	ret = run_descriptor_jr(desc);
 	if (ret) {
 		debug("%s: RSA failed to verify: %d\n", __func__, ret);
 		return -EFAULT;
 	}
+
+	invalidate_dcache_range((ulong)out, (ulong)sig_len);
 
 	return 0;
 }
