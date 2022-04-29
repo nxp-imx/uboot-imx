62a81128c07c3efc123039f796ffc43b4eacbf04
Nitin Garg <nitin.garg@nxp.com>
2022-04-29 10:54:20 -0500

    MLK-25878: imx8: scu: Increase sendmsg timeout for SCFW MU
    Similar to receivemsg, sendmsg function should also
    use 1 sec timeout for MU communication with SCFW.
    
    Signed-off-by: Nitin Garg <nitin.garg@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- drivers/misc/imx8/scu.c
+++ drivers/misc/imx8/scu.c
@@ -54,7 +54,7 @@ static int mu_hal_sendmsg(struct mu_type *base, u32 reg_index, u32 msg)
 	assert(reg_index < MU_TR_COUNT);
 
 	/* Wait TX register to be empty. */
-	ret = readl_poll_timeout(&base->sr, val, val & mask, 10000);
+	ret = readl_poll_timeout(&base->sr, val, val & mask, 1000000);
 	if (ret < 0) {
 		printf("%s timeout\n", __func__);
 		return -ETIMEDOUT;
