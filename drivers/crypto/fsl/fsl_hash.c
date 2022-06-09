nxp-imx/uboot-imx

commit 50333280be0b94b9228c89a2bc6b4903907f35e7

LFU-340 crypto/fsl: fsl_hash: Fix crash in flush dcache
wrong end address passed to flush_dcache_range.
modified the flush_dache logic for scatter list elements.

Fixes: 6921d2811b (LFU-320 crypto/fsl: fsl_hash: Fix dcache issue in caam_hash_finish)
Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>

drivers/crypto/fsl/fsl_hash.c
@@ -131,25 +131,31 @@ static int caam_hash_update(void *hash_ctx, const void *buf,
 static int caam_hash_finish(void *hash_ctx, void *dest_buf,
 			    int size, enum caam_hash_algos caam_algo)
 {
-	uint32_t len = 0;
+	uint32_t len = 0, sg_entry_len;
 	struct sha_ctx *ctx = hash_ctx;
 	int i = 0, ret = 0;
+	ulong addr;
 
 	if (size < driver_hash[caam_algo].digestsize) {
 		return -EINVAL;
 	}
 
-	for (i = 0; i < ctx->sg_num; i++)
-		len += (sec_in32(&ctx->sg_tbl[i].len_flag) &
-			SG_ENTRY_LENGTH_MASK);
-
+	flush_dcache_range((ulong)ctx->sg_tbl,
+			   (ulong)(ctx->sg_tbl) + (ctx->sg_num * sizeof(struct sg_entry)));
+	for (i = 0; i < ctx->sg_num; i++) {
+		sg_entry_len = (sec_in32(&ctx->sg_tbl[i].len_flag) &
+				SG_ENTRY_LENGTH_MASK);
+		len += sg_entry_len;
+		addr = ctx->sg_tbl[i].addr_hi;
+		addr = (addr << 32) | ctx->sg_tbl[i].addr_lo;
+		flush_dcache_range((ulong)addr, (ulong)addr + sg_entry_len);
+	}
 	inline_cnstr_jobdesc_hash(ctx->sha_desc, (uint8_t *)ctx->sg_tbl, len,
 				  ctx->hash,
 				  driver_hash[caam_algo].alg_type,
 				  driver_hash[caam_algo].digestsize,
 				  1);
 
-	flush_dcache_range((ulong)ctx->sg_tbl, (ulong)(ctx->sg_tbl) + len);
 	flush_dcache_range((ulong)ctx->sha_desc,
 			   (ulong)(ctx->sha_desc) + (sizeof(uint32_t) * MAX_CAAM_DESCSIZE));
 	flush_dcache_range((ulong)ctx->hash,

commit 11bb74ae88b8a8a3ec55daa46c8aec04052973c4

LFU-346 crypto/fsl: fsl_hash: read correct addr
Fixes: 50333280be (LFU-340 crypto/fsl: fsl_hash: Fix crash in flush dcache)
Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>

drivers/crypto/fsl/fsl_hash.c
@@ -146,8 +146,8 @@ static int caam_hash_finish(void *hash_ctx, void *dest_buf,
 		sg_entry_len = (sec_in32(&ctx->sg_tbl[i].len_flag) &
 				SG_ENTRY_LENGTH_MASK);
 		len += sg_entry_len;
-		addr = ctx->sg_tbl[i].addr_hi;
-		addr = (addr << 32) | ctx->sg_tbl[i].addr_lo;
+		addr = sec_in32(&ctx->sg_tbl[i].addr_hi);
+		addr = (addr << 32) | sec_in32(&ctx->sg_tbl[i].addr_lo);
 		flush_dcache_range((ulong)addr, (ulong)addr + sg_entry_len);
 	}
 	inline_cnstr_jobdesc_hash(ctx->sha_desc, (uint8_t *)ctx->sg_tbl, len,