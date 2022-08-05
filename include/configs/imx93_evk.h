f95538e2ccd3751f4eea4f509bd53afeeb7c39a1
Alice Guo <alice.guo@nxp.com>
2022-08-05 15:48:04 +0800

    MLK-26005 configs: imx93: modify mem=
    Ethosu occupies 0x10000000 DRAM from 0xc0000000 so that increase mem= by
    256MB to include it.
    
    Signed-off-by: Alice Guo <alice.guo@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- include/configs/imx93_evk.h
+++ include/configs/imx93_evk.h
@@ -33,11 +33,11 @@
 
 #define JAILHOUSE_ENV \
 	"jh_mmcboot=setenv fdtfile imx93-11x11-evk-root.dtb; " \
-		    "setenv jh_clk clk_ignore_unused mem=1GB kvm-arm.mode=nvhe; " \
+		    "setenv jh_clk clk_ignore_unused mem=1280MB kvm-arm.mode=nvhe; " \
 		    "if run loadimage; then run mmcboot;" \
 		    "else run jh_netboot; fi; \0" \
 	"jh_netboot=setenv fdtfile imx93-11x11-evk-root.dtb; " \
-		    "setenv jh_clk clk_ignore_unused mem=1GB kvm-arm.mode=nvhe; run netboot; \0 "
+		    "setenv jh_clk clk_ignore_unused mem=1280MB kvm-arm.mode=nvhe; run netboot; \0 "
 
 #define CFG_MFG_ENV_SETTINGS \
 	CFG_MFG_ENV_SETTINGS_DEFAULT \
