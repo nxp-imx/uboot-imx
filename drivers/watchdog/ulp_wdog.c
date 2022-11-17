6c3239e1482324c86b21d99e8a3e8d7e825cf90b
Alice Guo <alice.guo@nxp.com>
2022-11-17 16:36:18 +0800

    MLK-26061 watchdog: ulp_wdog: only compile the code of DM driver when CONFIG_WDT=y
    DM watchdog timer driver and non-DM driver exist in ulp_wdog.c at the
    same time. Add a compilation restriction that only compile the DM driver
    part when CONFIG_WDT=y.
    
    Signed-off-by: Alice Guo <alice.guo@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- drivers/watchdog/ulp_wdog.c
+++ drivers/watchdog/ulp_wdog.c
@@ -163,6 +163,7 @@ void reset_cpu(void)
 	while (1);
 }
 
+#if CONFIG_IS_ENABLED(WDT)
 static int ulp_wdt_start(struct udevice *dev, u64 timeout_ms, ulong flags)
 {
 	struct ulp_wdt_priv *priv = dev_get_priv(dev);
@@ -221,3 +222,4 @@ U_BOOT_DRIVER(ulp_wdt) = {
 	.probe		= ulp_wdt_probe,
 	.ops	= &ulp_wdt_ops,
 };
+#endif
