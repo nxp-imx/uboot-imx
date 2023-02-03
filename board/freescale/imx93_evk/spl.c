1b7b16ebc939de329288c4ccf27a05c4e0c5544a
Ye Li <ye.li@nxp.com>
2023-02-03 11:23:13 +0800

    LFU-461 imx93_evk: Change VDD_SOC to 0.8v for low drive mode
    According to latest PE/TE report, the voltages of VDD_SOC for
    LD and ND mode need add 50mv margin, so LD voltage is 0.75v->0.8v,
    ND voltage is 0.8v->0.85v.
    Update this voltage setting for CONFIG_IMX9_LOW_DRIVE_MODE.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- board/freescale/imx93_evk/spl.c
+++ board/freescale/imx93_evk/spl.c
@@ -77,10 +77,10 @@ int power_init_board(void)
 	pmic_reg_write(dev, PCA9450_BUCK1CTRL, 0x59);
 
 	if (IS_ENABLED(CONFIG_IMX9_LOW_DRIVE_MODE)){
-		/* 0.75v for Low drive mode
+		/* 0.8v for Low drive mode
 		 */
-		pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS0, 0x0c);
-		pmic_reg_write(dev, PCA9450_BUCK3OUT_DVS0, 0x0c);
+		pmic_reg_write(dev, PCA9450_BUCK1OUT_DVS0, 0x10);
+		pmic_reg_write(dev, PCA9450_BUCK3OUT_DVS0, 0x10);
 	} else {
 		/* 0.9v for Over drive mode
 		 */
