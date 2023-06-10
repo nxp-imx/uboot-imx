f6bdce81aa581d797390dd0043fc49feeb573f3c
Peng Fan <peng.fan@nxp.com>
2023-06-10 11:31:42 +0800

    LFU-604-33 usb: gadget: ci_udc: support i.MX95
    i.MX95 has a USB2 port that same as i.MX93, so reuse what i.MX93
    enabled.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Ye Li <ye.li@nxp.com>

--- drivers/usb/gadget/ci_udc.c
+++ drivers/usb/gadget/ci_udc.c
@@ -1051,7 +1051,7 @@ static int ci_udc_otg_phy_mode2(void *__iomem phy_base)
 			return USB_INIT_DEVICE;
 		else
 			return USB_INIT_HOST;
-	} else if (is_mx7() || is_imx8mm() || is_imx8mn() || is_imx93()) {
+	} else if (is_mx7() || is_imx8mm() || is_imx8mn() || is_imx93() || is_imx95()) {
 		phy_status = (void __iomem *)(phy_base +
 					      USBNC_PHY_STATUS_OFFSET);
 		val = readl(phy_status);
@@ -1376,7 +1376,7 @@ static int ci_udc_otg_phy_mode(struct udevice *dev)
 			return USB_INIT_DEVICE;
 		else
 			return USB_INIT_HOST;
-	} else if (is_mx7() || is_imx8mm() || is_imx8mn() || is_imx93()) {
+	} else if (is_mx7() || is_imx8mm() || is_imx8mn() || is_imx93() || is_imx95()) {
 		phy_status = (void __iomem *)(phy_base +
 					      USBNC_PHY_STATUS_OFFSET);
 		val = readl(phy_status);
