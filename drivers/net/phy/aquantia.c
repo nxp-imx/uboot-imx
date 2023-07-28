0ef833a67159e84f1ba10b5f99a603a8552e106c
Camelia Groza <camelia.groza@nxp.com>
2023-07-28 13:05:08 +0300

    drivers: net: aquantia: add driver for AQR115 PHYs
    AQR115 is similar to AQR113C, supporting speeds up to 2.5Gbps.
    
    Signed-off-by: Camelia Groza <camelia.groza@nxp.com>

--- drivers/net/phy/aquantia.c
+++ drivers/net/phy/aquantia.c
@@ -693,6 +693,20 @@ struct phy_driver aqr113c_driver = {
 	.data = AQUANTIA_GEN3,
 };
 
+struct phy_driver aqr115_driver = {
+	.name = "Aquantia AQR115",
+	.uid = 0x31c31c63,
+	.mask = 0xfffffff0,
+	.features = PHY_GBIT_FEATURES | SUPPORTED_2500baseX_Full,
+	.mmds = (MDIO_MMD_PMAPMD | MDIO_MMD_PCS|
+		 MDIO_MMD_PHYXS | MDIO_MMD_AN |
+		 MDIO_MMD_VEND1),
+	.config = &aquantia_config,
+	.startup = &aquantia_startup,
+	.shutdown = &gen10g_shutdown,
+	.data = AQUANTIA_GEN3,
+};
+
 struct phy_driver aqr405_driver = {
 	.name = "Aquantia AQR405",
 	.uid = 0x3a1b4b2,
@@ -730,6 +744,7 @@ int phy_aquantia_init(void)
 	phy_register(&aqr107_driver);
 	phy_register(&aqr112_driver);
 	phy_register(&aqr113c_driver);
+	phy_register(&aqr115_driver);
 	phy_register(&aqr405_driver);
 	phy_register(&aqr412_driver);
 
