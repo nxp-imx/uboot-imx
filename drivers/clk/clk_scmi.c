f941a4834e69f2ff19bb477bd23a1954f274d4d8
Ye Li <ye.li@nxp.com>
2023-10-24 21:34:01 +0800

    LFU-604-30 clk: clk_scmi: Fix assigned clock issue
    When assigned clock rate or parent in dts, the clk dev is not probed.
    so its priv data is not valid. Have to use its parent's priv data for
    scmi channel, but its parent device may change when device_reparent.
    
    Also for each child clk device, should not to get scmi channel again.
    They should use one channel which is got by scmi clk device.
    
    So fix the issue by changing priv data to plat data which is created
    at binding device not probing device. Then assign the scmi channel from
    clk parent device to every child device afer clk register.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- drivers/clk/clk_scmi.c
+++ drivers/clk/clk_scmi.c
@@ -14,16 +14,16 @@
 #include <linux/clk-provider.h>
 
 /**
- * struct scmi_clk_priv - Private data for SCMI clocks
+ * struct scmi_clk_plat - Platform data for SCMI clocks
  * @channel: Reference to the SCMI channel to use
  */
-struct scmi_clk_priv {
+struct scmi_clk_plat {
 	struct scmi_channel *channel;
 };
 
 static int scmi_clk_get_num_clock(struct udevice *dev, size_t *num_clocks)
 {
-	struct scmi_clk_priv *priv = dev_get_priv(dev);
+	struct scmi_clk_plat *plat = dev_get_plat(dev);
 	struct scmi_clk_protocol_attr_out out;
 	struct scmi_msg msg = {
 		.protocol_id = SCMI_PROTOCOL_ID_CLOCK,
@@ -33,7 +33,7 @@ static int scmi_clk_get_num_clock(struct udevice *dev, size_t *num_clocks)
 	};
 	int ret;
 
-	ret = devm_scmi_process_msg(dev, priv->channel, &msg);
+	ret = devm_scmi_process_msg(dev, plat->channel, &msg);
 	if (ret)
 		return ret;
 
@@ -44,7 +44,7 @@ static int scmi_clk_get_num_clock(struct udevice *dev, size_t *num_clocks)
 
 static int scmi_clk_get_attibute(struct udevice *dev, int clkid, char **name)
 {
-	struct scmi_clk_priv *priv = dev_get_priv(dev);
+	struct scmi_clk_plat *plat = dev_get_plat(dev);
 	struct scmi_clk_attribute_in in = {
 		.clock_id = clkid,
 	};
@@ -59,7 +59,7 @@ static int scmi_clk_get_attibute(struct udevice *dev, int clkid, char **name)
 	};
 	int ret;
 
-	ret = devm_scmi_process_msg(dev, priv->channel, &msg);
+	ret = devm_scmi_process_msg(dev, plat->channel, &msg);
 	if (ret)
 		return ret;
 
@@ -70,7 +70,7 @@ static int scmi_clk_get_attibute(struct udevice *dev, int clkid, char **name)
 
 static int scmi_clk_gate(struct clk *clk, int enable)
 {
-	struct scmi_clk_priv *priv = dev_get_priv(clk->dev);
+	struct scmi_clk_plat *plat = dev_get_plat(clk->dev);
 	struct scmi_clk_state_in in = {
 		.clock_id = clk->id,
 		.attributes = enable,
@@ -81,7 +81,7 @@ static int scmi_clk_gate(struct clk *clk, int enable)
 					  in, out);
 	int ret;
 
-	ret = devm_scmi_process_msg(clk->dev, priv->channel, &msg);
+	ret = devm_scmi_process_msg(clk->dev, plat->channel, &msg);
 	if (ret)
 		return ret;
 
@@ -106,7 +106,7 @@ static int scmi_clk_disable(struct clk *clk)
 
 static ulong scmi_clk_get_rate(struct clk *clk)
 {
-	struct scmi_clk_priv *priv = dev_get_priv(clk->dev);
+	struct scmi_clk_plat *plat = dev_get_plat(clk->dev);
 	struct scmi_clk_rate_get_in in = {
 		.clock_id = clk->id,
 	};
@@ -116,7 +116,7 @@ static ulong scmi_clk_get_rate(struct clk *clk)
 					  in, out);
 	int ret;
 
-	ret = devm_scmi_process_msg(clk->dev, priv->channel, &msg);
+	ret = devm_scmi_process_msg(clk->dev, plat->channel, &msg);
 	if (ret < 0)
 		return ret;
 
@@ -129,7 +129,7 @@ static ulong scmi_clk_get_rate(struct clk *clk)
 
 static ulong __scmi_clk_set_rate(struct clk *clk, ulong rate)
 {
-	struct scmi_clk_priv *priv = dev_get_priv(clk->dev);
+	struct scmi_clk_plat *plat = dev_get_plat(clk->dev);
 	struct scmi_clk_rate_set_in in = {
 		.clock_id = clk->id,
 		.flags = SCMI_CLK_RATE_ROUND_CLOSEST,
@@ -142,7 +142,7 @@ static ulong __scmi_clk_set_rate(struct clk *clk, ulong rate)
 					  in, out);
 	int ret;
 
-	ret = devm_scmi_process_msg(clk->dev, priv->channel, &msg);
+	ret = devm_scmi_process_msg(clk->dev, plat->channel, &msg);
 	if (ret < 0)
 		return ret;
 
@@ -166,21 +166,24 @@ static ulong scmi_clk_set_rate(struct clk *clk, ulong rate)
 
 static int scmi_clk_probe(struct udevice *dev)
 {
-	struct scmi_clk_priv *priv = dev_get_priv(dev);
+	struct scmi_clk_plat *plat = dev_get_plat(dev);
+	struct scmi_clk_plat *child_plat;
 	struct clk *clk;
 	size_t num_clocks, i;
 	int ret;
 
-	ret = devm_scmi_of_get_channel(dev, &priv->channel);
-	if (ret)
+	if (!CONFIG_IS_ENABLED(CLK_CCF)) {
+		ret = devm_scmi_of_get_channel(dev, &plat->channel);
 		return ret;
-
-	if (!CONFIG_IS_ENABLED(CLK_CCF))
-		return 0;
+	}
 
 	/* register CCF children: CLK UCLASS, no probed again */
 	if (device_get_uclass_id(dev->parent) == UCLASS_CLK)
 		return 0;
+
+	ret = devm_scmi_of_get_channel(dev, &plat->channel);
+	if (ret)
+		return ret;
 
 	ret = scmi_clk_get_num_clock(dev, &num_clocks);
 	if (ret)
@@ -203,6 +206,10 @@ static int scmi_clk_probe(struct udevice *dev)
 				return ret;
 			}
 
+			/* Assign the SCMI channel of parent to each child plat */
+			child_plat = dev_get_plat(clk->dev);
+			child_plat->channel = plat->channel;
+
 			clk_dm(i, clk);
 		}
 	}
@@ -212,7 +219,7 @@ static int scmi_clk_probe(struct udevice *dev)
 
 static int scmi_clk_set_parent(struct clk *clk, struct clk *parent)
 {
-	struct scmi_clk_priv *priv = dev_get_priv(clk->dev);
+	struct scmi_clk_plat *plat = dev_get_plat(clk->dev);
 	struct scmi_clk_parent_set_in in = {
 		.clock_id = clk->id,
 		.parent_clk = parent->id,
@@ -223,7 +230,7 @@ static int scmi_clk_set_parent(struct clk *clk, struct clk *parent)
 					  in, out);
 	int ret;
 
-	ret = devm_scmi_process_msg(clk->dev, priv->channel, &msg);
+	ret = devm_scmi_process_msg(clk->dev, plat->channel, &msg);
 	if (ret < 0)
 		return ret;
 
@@ -243,6 +250,6 @@ U_BOOT_DRIVER(scmi_clock) = {
 	.id = UCLASS_CLK,
 	.ops = &scmi_clk_ops,
 	.probe = scmi_clk_probe,
-	.priv_auto = sizeof(struct scmi_clk_priv *),
+	.plat_auto = sizeof(struct scmi_clk_plat *),
 	.flags = DM_FLAG_PRE_RELOC,
 };
