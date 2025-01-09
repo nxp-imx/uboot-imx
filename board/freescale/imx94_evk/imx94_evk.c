b1c407db8f26f077e1ab55323dd4b502e901ec2a
Ye Li <ye.li@nxp.com>
2025-01-09 18:26:25 +0800

    LFU-830-21 imx94_evk: Add temporary workaround for XSPI MTO setting
    Since GMID uses master ID, it only assigned to CPU0. So kernel driver
    may fail to set MTO when SMP enabled.
    So use temporary workaround to set it in u-boot. May need to move to
    other place in future.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

--- board/freescale/imx94_evk/imx94_evk.c
+++ board/freescale/imx94_evk/imx94_evk.c
@@ -278,6 +278,18 @@ void netc_init(void)
 	pci_init();
 }
 
+static void xspi_nor_setup(void)
+{
+	/* Set MTO to max */
+	imx_clk_scmi_enable(IMX94_CLK_XSPI1, true);
+	imx_clk_scmi_enable(IMX94_CLK_XSPI2, true);
+
+	writel(0xffffffff, 0x42b90928);
+	writel(0xffffffff, 0x42be0928);
+
+	return;
+}
+
 #if CONFIG_IS_ENABLED(NET)
 int board_phy_config(struct phy_device *phydev)
 {
@@ -303,6 +315,8 @@ int board_init(void)
 #endif
 
 	netc_init();
+
+	xspi_nor_setup();
 
 	power_on_m7("mx94alt");
 
