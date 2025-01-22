f1e22b713a744bf3a273aba65c7631259335911b
Ye Li <ye.li@nxp.com>
2025-01-22 15:47:55 +0800

    LFU-830-3 arm: dts: imx94: Add iMX94 dtsi and header files
    Add i.MX94 dtsi file and relavent header files for clock, power and
    pinfunc.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>

---
+++ include/dt-bindings/clock/nxp,imx94-clock.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR MIT */
+/*
+ * Copyright 2025 NXP
+ */
+
+#ifndef __DT_BINDINGS_CLOCK_IMX94_H
+#define __DT_BINDINGS_CLOCK_IMX94_H
+
+#define IMX94_CLK_DISPMIX_CLK_SEL	0
+#define IMX94_CLK_DISPMIX_END		1
+
+#define IMX94_CLK_DISPMIX_LVDS_CLK_GATE	0
+#define IMX94_CLK_DISPMIX_LVDS_CLK_END		1
+
+#endif /* __DT_BINDINGS_CLOCK_IMX94_H */
+
