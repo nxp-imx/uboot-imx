tq-systems/u-boot-tqmaxx

commit 7919847247e68822bd16f0cadc47f6860abe91ed

LF-9055 imx8m: Fix iMX8MP QuadLite kernel boot hang
VPU g1/g2/vc8000e are all disabled on the i.MX8MP Quadlite.
On A1, the pgc of vpumix can't be accessed. So need to disable the
pgc of vpumix/vpu_g1/vpu_g2/vpu_vc8000e from kernel dts. Otherwise
when pd device of vpu_g1/vpu_g2/vpu_vc8000e are added in kernel,
the pgc_vpumix will be power up then cause kernel hang.

Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

arch/arm/mach-imx/imx8m/soc.c
@@ -1103,6 +1103,10 @@ int disable_vpu_nodes(void *blob)
 		"/soc@0/video-codec@38310000",
 		"/soc@0/blk-ctrl@38330000",
 		"/soc@0/blk-ctl@38330000",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@19",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@20",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@21",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@22"
 	};
 
 	if (is_imx8mq())

nxp-imx/uboot-imx

commit 4651d51d38f39f1f6b647a1eafcde5b5a3a41f7f

arm: mach-imx: imx8m: Fix disabled VPU node names
Since kernel upstream commit 2f8405fb077b ("arm64: dts: imx8mp: Fix pgc vpu locations")
the pgc vpu nodes got renamed.

This has not been reflected in the NXP
commit: 7919847247e6 ("LF-9055 imx8m: Fix iMX8MP QuadLite kernel boot hang")
Where the power-domains are disabled before kernel boot.

Update the node names.

Fixes: 7919847247e6 ("LF-9055 imx8m: Fix iMX8MP QuadLite kernel boot hang")
Signed-off-by: Teresa Remmet <t.remmet@phytec.de>

arch/arm/mach-imx/imx8m/soc.c
@@ -1118,10 +1118,10 @@ int disable_vpu_nodes(void *blob)
 		"/soc@0/video-codec@38310000",
 		"/soc@0/blk-ctrl@38330000",
 		"/soc@0/blk-ctl@38330000",
-		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@19",
-		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@20",
-		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@21",
-		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@22"
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@8",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@11",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@12",
+		"/soc@0/bus@30000000/gpc@303a0000/pgc/power-domain@13"
 	};
 
 	if (is_imx8mq())