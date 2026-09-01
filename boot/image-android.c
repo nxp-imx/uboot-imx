nxp-imx/uboot-imx

commit bb7724717907f35931146e37694e380354cf0795

MA-25319-2 pass ddr size to android userspace for all platforms
The "ro.board.api_level" was set to "202604" on android 17 for
all platforms, so pass the ddr size property for all platforms.

Change-Id: I39b5d10fed6d8e9c1cc231f87b933d00cb9e1341
Signed-off-by: Ji Luo <ji.luo@nxp.com>

boot/image-android.c
@@ -560,7 +560,6 @@ static int append_androidboot_args(char *args, uint32_t *len, void *fdt_addr)
 	}
 #endif
 
-#ifdef CONFIG_IMX9
 	/* append total DDR size into bootconfig for android userspace */
 	phys_size_t ddr_bytes = 0;
 	if (board_phys_sdram_size(&ddr_bytes) == 0) {
@@ -568,7 +567,6 @@ static int append_androidboot_args(char *args, uint32_t *len, void *fdt_addr)
 		sprintf(args_buf, " androidboot.ddr_size=%uMB", ddr_mb);
 		strncat(args, args_buf, *len - strlen(args));
 	}
-#endif
 
 #ifdef CONFIG_APPEND_BOOTARGS
 	/* Add 'append_bootconfig' environment variable to hold some paramemters

commit d29a6c061738c14d08529392d03f95a8d693b4fe

LFU-1047 image: android: Fix build break on iMX6/7
The board_phys_sdram_size is not defined on legacy iMX platforms
like iMX6 and iMX7. Since fastboot is enabled to support UUU, so
the image-android is also built and cause the issue.

Fix it by restricting calling to iMX ARMv8 platforms only.

Fixes: bb7724717907 ("MA-25319-2 pass ddr size to android userspace for all platforms")
Signed-off-by: Ye Li <ye.li@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

boot/image-android.c
@@ -560,6 +560,7 @@ static int append_androidboot_args(char *args, uint32_t *len, void *fdt_addr)
 	}
 #endif
 
+#if IS_ENABLED(CONFIG_MACH_IMX) && IS_ENABLED(CONFIG_ARM64)
 	/* append total DDR size into bootconfig for android userspace */
 	phys_size_t ddr_bytes = 0;
 	if (board_phys_sdram_size(&ddr_bytes) == 0) {
@@ -567,6 +568,7 @@ static int append_androidboot_args(char *args, uint32_t *len, void *fdt_addr)
 		sprintf(args_buf, " androidboot.ddr_size=%uMB", ddr_mb);
 		strncat(args, args_buf, *len - strlen(args));
 	}
+#endif
 
 #ifdef CONFIG_APPEND_BOOTARGS
 	/* Add 'append_bootconfig' environment variable to hold some paramemters